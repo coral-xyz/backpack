df577a760f7efc717b1aff14738f141700b3f142
wentokay <wentokay@users.noreply.github.com>
2022-05-01 16:44:25 -0400

    remove create-react-app's extra files

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
 import * as background from "./background/client";
 
 async function main() {
@@ -27,8 +26,3 @@ function render() {
 }
 
 main();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
