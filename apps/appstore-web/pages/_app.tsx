4887ca45a814de4b706b1ef4882c965ef85a2a8e
Italo Casas <me@italoacasas.com>
2022-05-13 14:55:53 -0400

    small improvements

--- apps/appstore-web/pages/_app.tsx
+++ apps/appstore-web/pages/_app.tsx
@@ -2,11 +2,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import PlausibleProvider from 'next-plausible';
 import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
 import { ContextProvider } from '../context/ContextProvider';
 import Nav from '../components/nav';
-
-require('@solana/wallet-adapter-react-ui/styles.css');
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -17,13 +14,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/*// TODO: domain*/}
       <PlausibleProvider domain="domain" trackOutboundLinks={true}>
         <ContextProvider>
-          <SessionProvider>
-            <Nav />
+          <Nav />
 
-            <div className="min-h-screen bg-gray-800 px-5 py-10">
-              <Component {...pageProps} />
-            </div>
-          </SessionProvider>
+          <div className="min-h-screen bg-gray-800 px-5 py-10">
+            <Component {...pageProps} />
+          </div>
         </ContextProvider>
       </PlausibleProvider>
     </>
