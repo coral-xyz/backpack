da2ac70726543435b09f405f2541a465078d6bd4
Italo Casas <me@italoacasas.com>
2022-05-13 16:45:45 -0400

    adding types

--- apps/appstore-web/next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: 'domain',
-  generateRobotsTxt: true
-};
--- apps/appstore-web/pages/_app.tsx
+++ apps/appstore-web/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import PlausibleProvider from 'next-plausible';
 import Head from 'next/head';
-// import { ContextProvider } from '../context/ContextProvider';
 import Nav from '../components/nav';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -13,13 +12,11 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       {/*// TODO: domain*/}
       <PlausibleProvider domain="domain" trackOutboundLinks={true}>
-        {/* <ContextProvider> */}
         <Nav />
 
         <div className="min-h-screen bg-gray-800 px-5 py-10">
           <Component {...pageProps} />
         </div>
-        {/* </ContextProvider> */}
       </PlausibleProvider>
     </>
   );
