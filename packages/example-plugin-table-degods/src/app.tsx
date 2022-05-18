a8eb12f6c2be9c2df359ae9f6e6341d34896625b
Armani Ferrante <armaniferrante@gmail.com>
2022-05-18 13:07:31 -0400

    cache requests inside plugins (#54)

--- packages/example-plugin-table-degods/src/app.tsx
+++ packages/example-plugin-table-degods/src/app.tsx
@@ -1,8 +1,9 @@
 import React, { useMemo, useState, useEffect } from "react";
 import { PublicKey } from "@solana/web3.js";
 import { Program } from "@project-serum/anchor";
-import * as anchor from "@project-serum/anchor";
 import {
+  useNavigation,
+  Text,
   BalancesTable,
   BalancesTableHead,
   BalancesTableContent,
@@ -19,7 +20,8 @@ export function App() {
 }
 
 function DegodsTable() {
-  const [tokenAccounts, setTokenAccounts] = useState<any>([]);
+  const nav = useNavigation();
+  const [tokenAccounts, setTokenAccounts] = useState<any>(null);
   const gemFarm = useMemo(() => {
     return new Program<GemFarm>(IDL_GEM_FARM, PID_GEM_FARM, window.anchor);
   }, []);
@@ -29,68 +31,107 @@ function DegodsTable() {
 
   useEffect(() => {
     (async () => {
-      const farmers = await gemFarm.account.farmer.all([
-        // Farm pubkey.
-        {
-          memcmp: {
-            bytes: FARM.toString(),
-            offset: 8,
-          },
-        },
-        // Farmer authority
-        {
-          memcmp: {
-            bytes: window.anchor.publicKey.toString(),
-            offset: 8 + 32,
-          },
-        },
-      ]);
-      if (farmers.length === 0) {
-        return;
-      }
-      const farmer = farmers[0];
-      const receipts = await gemBank.account.gemDepositReceipt.all([
-        {
-          memcmp: {
-            bytes: farmer.account.vault.toString(),
-            offset: 8,
-          },
-        },
-      ]);
-
-      if (receipts.length === 0) {
-        return;
-      }
-
-      const vault = await gemBank.account.vault.fetch(farmer.account.vault);
-      const tokenAccounts = await customSplTokenAccounts(
-        window.anchor.connection,
-        vault.authority
-      );
-
-      setTokenAccounts(tokenAccounts.nftMetadata.map((m) => m[1]));
+      const ta = await fetchTokenAccounts(gemFarm, gemBank);
+      setTokenAccounts(ta);
     })();
   }, [gemFarm, gemBank]);
 
   return (
     <BalancesTable>
       <BalancesTableHead title={"Staked Degods"} iconUrl={DEGODS_ICON_DATA} />
-      <BalancesTableContent>
-        {tokenAccounts.map((t) => {
-          return (
-            <BalancesTableRow>
-              <BalancesTableCell
-                title={t.tokenMetaUriData.name}
-                icon={t.tokenMetaUriData.image}
-                subtitle={t.tokenMetaUriData.collection.family}
-              />
-            </BalancesTableRow>
-          );
-        })}
-      </BalancesTableContent>
+      {tokenAccounts === null ? (
+        <BalancesTableContent></BalancesTableContent>
+      ) : tokenAccounts.length === 0 ? (
+        <BalancesTableContent>
+          <BalancesTableRow onClick={() => nav.push(<StakeDetail />)}>
+            <BalancesTableCell
+              title={"Stake your Degods"}
+              icon={
+                "https://uploads-ssl.webflow.com/61f2155bfe47bd05cae702bb/61f21670d6560ecc93050888_New%20Logo.png"
+              }
+              subtitle={"Earn $DUST now"}
+              usdValue={0}
+            />
+          </BalancesTableRow>
+        </BalancesTableContent>
+      ) : (
+        <BalancesTableContent>
+          {tokenAccounts.map((t) => {
+            return (
+              <BalancesTableRow onClick={() => nav.push(<StakeDetail />)}>
+                <BalancesTableCell
+                  title={t.tokenMetaUriData.name}
+                  icon={t.tokenMetaUriData.image}
+                  subtitle={t.tokenMetaUriData.collection.family}
+                />
+              </BalancesTableRow>
+            );
+          })}
+        </BalancesTableContent>
+      )}
       <BalancesTableFooter></BalancesTableFooter>
     </BalancesTable>
   );
+}
+
+function StakeDetail() {
+  return <Text>Stake Detail Here</Text>;
+}
+
+async function fetchTokenAccounts(
+  gemFarm: Program<GemFarm>,
+  gemBank: Program<GemBank>
+): Promise<any> {
+  //
+  // If we have a cached response, then use it.
+  //
+  const resp = CACHE.get(window.anchor.publicKey.toString());
+  if (resp) {
+    return resp;
+  }
+
+  const farmers = await gemFarm.account.farmer.all([
+    // Farm pubkey.
+    {
+      memcmp: {
+        bytes: FARM.toString(),
+        offset: 8,
+      },
+    },
+    // Farmer authority
+    {
+      memcmp: {
+        bytes: window.anchor.publicKey.toString(),
+        offset: 8 + 32,
+      },
+    },
+  ]);
+  if (farmers.length === 0) {
+    return [];
+  }
+  const farmer = farmers[0];
+  const receipts = await gemBank.account.gemDepositReceipt.all([
+    {
+      memcmp: {
+        bytes: farmer.account.vault.toString(),
+        offset: 8,
+      },
+    },
+  ]);
+
+  if (receipts.length === 0) {
+    return [];
+  }
+
+  const vault = await gemBank.account.vault.fetch(farmer.account.vault);
+  const tokenAccounts = await customSplTokenAccounts(
+    window.anchor.connection,
+    vault.authority
+  );
+
+  const newResp = tokenAccounts.nftMetadata.map((m) => m[1]);
+  CACHE.set(window.anchor.publicKey.toString(), newResp);
+  return newResp;
 }
 
 const DEGODS_ICON_DATA =
@@ -102,3 +143,8 @@ const PID_GEM_BANK = new PublicKey(
   "6VJpeYFy87Wuv4KvwqD5gyFBTkohqZTqs6LgbCJ8tDBA"
 );
 const FARM = new PublicKey("G9nFryoG6Cn2BexRquWa2AKTwcJfumWoDNLUwWkhXcij");
+
+//
+// Caches requests.
+//
+const CACHE = new Map<string, any>();
