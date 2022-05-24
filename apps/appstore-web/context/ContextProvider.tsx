8715757c83a42f25a9157a228aedbbbf6c49c9b6
Italo Casas <me@italoacasas.com>
2022-05-24 18:11:12 +0200

    wallet adapter

--- apps/appstore-web/context/ContextProvider.tsx
+++ apps/appstore-web/context/ContextProvider.tsx
@@ -1,7 +1,7 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, AnchorWalletAdapter } from '@italoacasas/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useMemo } from 'react';
 import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
