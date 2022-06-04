8677b8bc6fe91fc04702eb53401ab33d52beaf83
Taylor123 <cjohns28@terpmail.umd.edu>
2022-06-04 14:00:01 -0400

    WIP

--- examples/plugins/table-psyfi/src/App.tsx
+++ examples/plugins/table-psyfi/src/App.tsx
@@ -1,10 +1,22 @@
-import { BalancesTable, BalancesTableHead } from "@200ms/anchor-ui";
+import AnchorUi, {
+  BalancesTable,
+  BalancesTableContent,
+  BalancesTableHead,
+} from "@200ms/anchor-ui";
+// import { fetchAllVaults } from "./utils";
 
-export const App = () => (
-  <BalancesTable>
-    <BalancesTableHead
-      title="PsyFinance Vaults"
-      iconUrl="https://uploads-ssl.webflow.com/6158e3591ba06d14de4fd0df/61f900784e63439a5a052fed_PsyOptions.svg"
-    />
-  </BalancesTable>
-);
+// AnchorUi.events.on('connect', () => {
+//   fetchAllVaults()
+// });
+
+export const App = () => {
+  return (
+    <BalancesTable>
+      <BalancesTableHead
+        title="PsyFinance Vaults"
+        iconUrl="https://uploads-ssl.webflow.com/6158e3591ba06d14de4fd0df/61f900784e63439a5a052fed_PsyOptions.svg"
+      />
+      <BalancesTableContent />
+    </BalancesTable>
+  );
+};
---
+++ examples/plugins/table-psyfi/src/utils.ts
@@ -0,0 +1,14 @@
+// import { instructions, PsyFiIdl, pdas } from "psyfi-euros-test";
+
+// /**
+//  * Fetch all vaults owned by the psyfi program.
+//  */
+// export const fetchAllVaults = async () => {
+//   const mainnetUrl = "https://us-central1-psyfi-api.cloudfunctions.net/vaults";
+//   try {
+//     const resp = await fetch(mainnetUrl);
+//     console.log(resp);
+//   } catch (err) {
+//     console.warn(err);
+//   }
+// };
