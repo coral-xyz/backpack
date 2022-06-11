c9fb15798983090f1801c6e2aa872fb6b6913d77
Armani Ferrante <armaniferrante@gmail.com>
2022-06-11 15:03:57 -0400

    stash

--- packages/extension/src/background/index.ts
+++ packages/extension/src/background/index.ts
@@ -1,11 +1,3 @@
 import { start } from "@200ms/background";
 
-//
-// Only run in the context of a service worker.
-//
-console.log("armani here testing!", navigator, navigator.serviceWorker);
-//if (navigator.serviceWorker === undefined) {
 const _handle = start();
-//}
-
-//import {} from "@200ms/common";
