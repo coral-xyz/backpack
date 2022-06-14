fe028b80999b913f9b851b491e761b589b0316b5
wentokay <wentokay@users.noreply.github.com>
2022-06-14 06:26:25 -0700

    remove old TODO comment

--- packages/anchor-ui-renderer/src/Renderer.tsx
+++ packages/anchor-ui-renderer/src/Renderer.tsx
@@ -26,8 +26,6 @@ function RootRenderer() {
   //
   useEffect(() => {
     plugin.onRenderRoot((c: Array<Element>) => {
-      // TODO: c will often be children of a different iframe when
-      //       hosting them all from the same domain.
       setChildren([...c]);
     });
   }, [plugin, setChildren]);
