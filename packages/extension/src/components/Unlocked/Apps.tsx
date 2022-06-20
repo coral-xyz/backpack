d04f4060949beb6ab53da7e389fc09a9f66aa5ca
wentokay <wentokay@users.noreply.github.com>
2022-06-20 10:42:05 -0400

    feat(css): squircle icons

--- packages/extension/src/components/Unlocked/Apps.tsx
+++ packages/extension/src/components/Unlocked/Apps.tsx
@@ -4,21 +4,30 @@ import { Plugin, PluginRenderer } from "@coral-xyz/anchor-ui-renderer";
 import { usePlugins, useTablePlugins, useNavigation } from "@coral-xyz/recoil";
 import type { SearchParamsFor } from "@coral-xyz/recoil";
 import { NAV_COMPONENT_PLUGINS } from "@coral-xyz/common";
+import { getSvgPath } from "figma-squircle";
 
-const ICON_WIDTH = "64px";
+const ICON_WIDTH = 64;
+
+const squircleIconPath = getSvgPath({
+  width: ICON_WIDTH,
+  height: ICON_WIDTH,
+  cornerRadius: 15,
+  cornerSmoothing: 0.8,
+});
 
 const useStyles = makeStyles((theme: any) => ({
   pluginIconRoot: {
-    minWidth: "64px",
+    minWidth: ICON_WIDTH,
     marginLeft: "auto",
     marginRight: "auto",
   },
   pluginIconButton: {
     width: ICON_WIDTH,
     height: ICON_WIDTH,
-    borderRadius: "10px",
-    backgroundColor: theme.custom.colors.nav,
+    overflow: "hidden",
+    clipPath: `path('${squircleIconPath}')`,
     padding: 0,
+    backgroundColor: theme.custom.colors.nav,
   },
   pluginTitle: {
     fontWeight: 500,
@@ -95,7 +104,6 @@ function PluginIcon({ plugin }: { plugin: Plugin }) {
         <img
           src={plugin.iconUrl}
           style={{
-            borderRadius: "8px",
             width: ICON_WIDTH,
             height: ICON_WIDTH,
           }}
