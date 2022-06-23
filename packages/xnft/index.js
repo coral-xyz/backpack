36d63900caa4b8fc03e857f3a35c5ae3cb7a652c
wentokay <wentokay@users.noreply.github.com>
2022-06-22 23:02:03 -0400

    feat: basic xnft cli

---
+++ packages/xnft/index.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+// using JS for now because there are race-condition issues
+// with compiling typescript before running in the monorepo
+
+const { Parcel } = require("@parcel/core");
+const { program } = require("commander");
+
+const options = {
+  entries: "./src/index.tsx",
+  defaultConfig: "@parcel/config-default",
+  defaultTargetOptions: {
+    engines: {
+      browsers: ["last 1 Chrome version"],
+    },
+  },
+  targets: {
+    modern: {
+      distDir: "dist",
+    },
+  },
+};
+
+program.command("build").action(async () => {
+  // https://parceljs.org/features/parcel-api/#building
+  const bundler = new Parcel({
+    ...options,
+    mode: "production",
+    sourceMap: false,
+    optimize: true,
+  });
+
+  try {
+    const { buildTime } = await bundler.run();
+    console.debug(`✨ built in ${buildTime}ms!`);
+  } catch (err) {
+    console.error(err.diagnostics);
+  }
+});
+
+program
+  .command("watch")
+  .option("-p, --port <port>", "Port to serve from", 1234)
+  .action(async ({ port }) => {
+    console.debug(`👀 watching on http://localhost:${port}`);
+    // https://parceljs.org/features/parcel-api/#watching
+    const bundler = new Parcel({
+      ...options,
+      mode: "development",
+      sourceMap: true,
+      optimize: false,
+      serveOptions: {
+        port,
+      },
+      hmrOptions: {
+        port,
+      },
+    });
+    await bundler.watch();
+  });
+
+program.parse();
