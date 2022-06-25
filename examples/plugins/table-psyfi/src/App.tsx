6c1a7cd7a72ab2235050927fcf80be2233b42c68
Taylor123 <cjohns28@terpmail.umd.edu>
2022-06-25 15:19:52 -0400

    clean up after updates

--- examples/plugins/table-psyfi/src/App.tsx
+++ examples/plugins/table-psyfi/src/App.tsx
@@ -2,21 +2,41 @@ import AnchorUi, {
   BalancesTable,
   BalancesTableContent,
   BalancesTableHead,
-} from "@200ms/anchor-ui";
-// import { fetchAllVaults } from "./utils";
-
-// AnchorUi.events.on('connect', () => {
-//   fetchAllVaults()
-// });
+  BalancesTableCell,
+  BalancesTableRow,
+} from "@coral-xyz/anchor-ui";
+import { useEffect, useState } from "react";
+import { Vault } from "./types";
+import { fetchAllVaults } from "./utils";
 
 export const App = () => {
+  const [vaults, setVaults] = useState<Record<string, Vault>>({});
+  useEffect(() => {
+    (async () => {
+      const resp = await fetchAllVaults();
+      if (resp) {
+        setVaults(resp.vaults);
+      }
+    })();
+  }, []);
+
+  // TODO get SPL token values for vault
   return (
     <BalancesTable>
       <BalancesTableHead
         title="PsyFinance Vaults"
         iconUrl="https://uploads-ssl.webflow.com/6158e3591ba06d14de4fd0df/61f900784e63439a5a052fed_PsyOptions.svg"
       />
-      <BalancesTableContent />
+      <BalancesTableContent>
+        {Object.keys(vaults).map((key) => {
+          const vault = vaults[key];
+          return (
+            <BalancesTableRow key={key}>
+              <BalancesTableCell title={vault.name} />
+            </BalancesTableRow>
+          );
+        })}
+      </BalancesTableContent>
     </BalancesTable>
   );
 };
--- examples/plugins/table-psyfi/src/utils.ts
+++ examples/plugins/table-psyfi/src/utils.ts
@@ -1,14 +1,18 @@
-// import { instructions, PsyFiIdl, pdas } from "psyfi-euros-test";
+import { instructions, PsyFiIdl, pdas } from "psyfi-euros-test";
+import { VaultsResponse } from "./types";
 
-// /**
-//  * Fetch all vaults owned by the psyfi program.
-//  */
-// export const fetchAllVaults = async () => {
-//   const mainnetUrl = "https://us-central1-psyfi-api.cloudfunctions.net/vaults";
-//   try {
-//     const resp = await fetch(mainnetUrl);
-//     console.log(resp);
-//   } catch (err) {
-//     console.warn(err);
-//   }
-// };
+/**
+ * Fetch all vaults owned by the psyfi program.
+ */
+export const fetchAllVaults: () => Promise<
+  VaultsResponse | undefined
+> = async () => {
+  const mainnetUrl = "https://us-central1-psyfi-api.cloudfunctions.net/vaults";
+  try {
+    const resp = await fetch(mainnetUrl);
+    const json = await resp.json();
+    return json;
+  } catch (err) {
+    console.warn(err);
+  }
+};
