a37a862bb4e64af2165ca3695f21c5d5d040d7ce
wentokay <wentokay@users.noreply.github.com>
2022-06-30 19:00:19 -0400

    handle sending & receiving messages with fake chrome

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -5,7 +5,7 @@ import { useKeyringStoreState } from "@coral-xyz/recoil";
 
 export default function App() {
   // XXX: uncommenting the line below will currently break the app
-  // const keyringStoreState = useKeyringStoreState();
+  const keyringStoreState = useKeyringStoreState();
   return (
     <View style={styles.container}>
       <Text>{Keypair.generate().publicKey.toString()}</Text>
