431b52b4e7973877cd5a5443eb85232943db89e4
Tom Linton <tlinton@fastmail.fm>
2022-07-03 11:03:09 +1200

    Remove routing from options

--- packages/extension/src/options/Options.tsx
+++ packages/extension/src/options/Options.tsx
@@ -1,12 +1,7 @@
 import { RecoilRoot } from "recoil";
-import {
-  setupClient,
-  QUERY_CONNECT_HARDWARE,
-  QUERY_ONBOARDING,
-} from "@coral-xyz/common";
+import { setupClient } from "@coral-xyz/common";
 import { WithSuspense } from "../app/Router";
 
-import { ConnectHardware } from "../components/Settings/ConnectHardware";
 import { Onboarding } from "../components/Onboarding";
 import "../app/App.css";
 import { WithTheme } from "../app/theme";
@@ -15,45 +10,18 @@ import { WithTheme } from "../app/theme";
 // Options provides the "expanded" extension app flows. Namely,
 //
 // - Onboarding
-// - Connect to hardware
-//
+
 function Options() {
   setupClient();
   return (
     <RecoilRoot>
-      <_Options />
+      <WithTheme>
+        <WithSuspense>
+          <Onboarding />
+        </WithSuspense>
+      </WithTheme>
     </RecoilRoot>
   );
-}
-
-function _Options() {
-  return (
-    <WithTheme>
-      <WithSuspense>
-        <Router />
-      </WithSuspense>
-    </WithTheme>
-  );
-}
-
-function Router() {
-  //
-  // Extract the url query parameters for routing dispatch.
-  //
-  const search =
-    window.location.search.length > 0
-      ? window.location.search.substring(1)
-      : "";
-  const query = search.split("&")[0];
-
-  switch (query) {
-    case QUERY_CONNECT_HARDWARE:
-      return <ConnectHardware />;
-    case QUERY_ONBOARDING:
-      return <Onboarding />;
-    default:
-      throw new Error("invalid query param");
-  }
 }
 
 export default Options;
