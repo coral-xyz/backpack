b0b66e1b2f830c32891e9238b90e5329f7214856
Tom Linton <tlinton@fastmail.fm>
2022-07-03 14:59:05 +1200

    Add connect hardware success message

--- packages/extension/src/components/Settings/ConnectHardware/ConnectHardwareSuccess.tsx
+++ packages/extension/src/components/Settings/ConnectHardware/ConnectHardwareSuccess.tsx
@@ -1,1 +1,43 @@
-export function ConnectHardwareSuccess() {}
+import { Box } from "@mui/material";
+import { Header, PrimaryButton, SubtextParagraph } from "../../common";
+import { SuccessIcon } from "../../Icon";
+
+export function ConnectHardwareSuccess({ onNext }: { onNext: () => void }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box
+        sx={{
+          marginTop: "16px",
+          marginLeft: "24px",
+          marginRight: "24px",
+        }}
+      >
+        <Box sx={{ display: "block", textAlign: "center", mb: "12px" }}>
+          <SuccessIcon />
+        </Box>
+        <Header text="Hardware wallet connected" />
+        <SubtextParagraph>
+          You can now access your hardware wallet with Backpack.
+        </SubtextParagraph>
+      </Box>
+      <Box
+        sx={{
+          marginLeft: "16px",
+          marginRight: "16px",
+          marginBottom: "16px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <PrimaryButton label="View wallet" onClick={onNext} />
+      </Box>
+    </Box>
+  );
+}
