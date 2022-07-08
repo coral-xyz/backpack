c6534c1b9d5f9818664e737bb16084f79a98ac67
Armani Ferrante <armaniferrante@gmail.com>
2022-07-08 14:32:22 -0400

    stash

--- backend/workers/prices/src/index.ts
+++ backend/workers/prices/src/index.ts
@@ -8,16 +8,27 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    if (request.url.endsWith("/cron")) {
-      return handleCron(request, env, ctx);
-    } else {
-      const j = await env.PRICES.get("prices", { type: "json" });
-      return jsonResponse(j);
+    const store = new Store(env);
+    const _coinPrices = await store.get("prices");
+    try {
+      const coinPrices = await fetchPrices();
+    } catch (err) {
+      console.log("err", err);
     }
+    //		await store.set("prices", coinPrices);
+    return jsonResponse(_coinPrices);
+  },
+
+  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+    await ctx.waitUntil(handleCron(event, env, ctx));
   },
 };
 
-async function handleCron(request: Request, env: Env, ctx: ExecutionContext) {
+async function handleCron(
+  request: ScheduledEvent,
+  env: Env,
+  ctx: ExecutionContext
+) {
   const prices = await fetchPrices();
   const result = {
     //    updatedAt: scheduledData,
@@ -31,6 +42,18 @@ function handleRequest(request: Request, env: Env, ctx: ExecutionContext) {
   return {};
 }
 
+class Store {
+  constructor(private env: Env) {}
+
+  async get(key: string) {
+    return JSON.parse(await this.env.PRICES.get(key));
+  }
+
+  async set(key: string, value: any) {
+    await this.env.PRICES.put(key, JSON.stringify(value));
+  }
+}
+
 function jsonResponse(jsonObj: any, status = 200) {
   return new Response(
     JSON.stringify(jsonObj, {
@@ -42,31 +65,33 @@ function jsonResponse(jsonObj: any, status = 200) {
   );
 }
 
-const COINS = {
-  solana: {
-    symbol: "SOL",
-    name: "Solana",
-  },
-  ethereum: {
-    symbol: "ETH",
-    name: "Ethereum",
-  },
-  serum: {
-    symbol: "SRM",
-    name: "Serum",
-  },
-  "ftx-token": {
-    symbol: "FTT",
-    name: "FTX",
-  },
-};
-
 async function fetchPrices() {
+  const COINS = {
+    solana: {
+      symbol: "SOL",
+      name: "Solana",
+    },
+    ethereum: {
+      symbol: "ETH",
+      name: "Ethereum",
+    },
+    serum: {
+      symbol: "SRM",
+      name: "Serum",
+    },
+    "ftx-token": {
+      symbol: "FTT",
+      name: "FTX",
+    },
+  };
+
   const coinPrices = {};
+  console.log("here", coinPrices);
   const ids = Object.keys(COINS);
   for (let k = 0; k < ids.length; k += 1) {
     const id = ids[k];
     const data = await coingeckoApi(`coins/${id}/market_chart`);
+    console.log("data here", data);
     coinPrices[id] = {
       ...COINS[id],
       ...data,
@@ -76,9 +101,13 @@ async function fetchPrices() {
 }
 
 async function coingeckoApi(resource: string) {
-  return await (
-    await fetch(
-      `https://api.coingecko.com/api/v3/${resource}?vs_currency=usd&days=1`
-    )
-  ).json();
+  try {
+    const url = `https://api.coingecko.com/api/v3/${resource}?vs_currency=usd&days=1`;
+    console.log("url", url);
+    const resp = await fetch(url);
+    return await resp.json();
+  } catch (err) {
+    console.log("err", err);
+    throw err;
+  }
 }
