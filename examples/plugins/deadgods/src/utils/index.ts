1b979ba985124894c7486c0aad9ba33ee8ec8ae6
Armani Ferrante <armaniferrante@gmail.com>
2022-07-11 23:13:45 -0400

    stash

--- examples/plugins/deadgods/src/utils/index.ts
+++ examples/plugins/deadgods/src/utils/index.ts
@@ -58,7 +58,6 @@ export function useEstimatedRewards() {
           true
         );
         setEstimatedRewards(rewards.toFixed(4));
-        /*
         const interval = setInterval(() => {
           const newRewards = getEstimatedRewards(
             farmer.rewardA,
@@ -69,7 +68,6 @@ export function useEstimatedRewards() {
           setEstimatedRewards(newRewards.toFixed(4));
         }, 1000);
         return () => clearInterval(interval);
-*/
       } catch (err) {
         console.error(err);
       }
