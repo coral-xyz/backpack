f23d7fa39c973bc6b61df774a5143005369e6d59
Armani Ferrante <armaniferrante@gmail.com>
2022-07-20 22:04:18 -0400

    remove degods widget

--- examples/plugins/deadgods/src/index.tsx
+++ examples/plugins/deadgods/src/index.tsx
@@ -1,15 +1,8 @@
 import ReactXnft, { AnchorDom } from "react-xnft";
 import { App } from "./app";
-import { Widget } from "./widget";
 
 ReactXnft.render(
   <AnchorDom>
     <App />
-  </AnchorDom>
-);
-
-ReactXnft.renderWidget(
-  <AnchorDom>
-    <Widget />
   </AnchorDom>
 );
