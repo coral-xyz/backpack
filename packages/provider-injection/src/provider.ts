a856acf6f1310e668d249188087c1fc337834964
Tom Linton <tlinton@fastmail.fm>
2022-07-24 13:21:01 +1200

    remove unneeded throw

--- packages/provider-injection/src/provider.ts
+++ packages/provider-injection/src/provider.ts
@@ -132,9 +132,7 @@ export class ProviderInjection extends EventEmitter implements Provider {
       method: RPC_METHOD_CONNECT,
       params: [onlyIfTrustedMaybe],
     });
-    if (!result) {
-      throw new Error("user did not approve connection");
-    }
+
     this._connect(result.publicKey, result.connectionUrl);
   }
 
