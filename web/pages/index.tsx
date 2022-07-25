899bc2620d17c00cdd8467c95d5a99a673ee3a39
Matthew Callens <callensmatt@gmail.com>
2022-07-25 09:06:54 -0400

    migrate the landing and about next app (#384)

---
+++ web/pages/index.tsx
@@ -0,0 +1,21 @@
+import dynamic from 'next/dynamic';
+
+const Hero = dynamic(() => import('../components/Hero'));
+const PlaceholderApps = dynamic(() => import('../components/PlaceholderApps'));
+const SecondaryCta = dynamic(() => import('../components/SecondaryCta'));
+// const Posts = dynamic(() => import('../components/posts'));
+const Newsletter = dynamic(() => import('../components/Newsletter'));
+const Partners = dynamic(() => import('../components/Partners'));
+
+export default function Home() {
+  return (
+    <div className="mb-20 flex flex-col gap-10">
+      <Hero />
+      <PlaceholderApps />
+      <SecondaryCta publishDisable={true} />
+      {/*<Posts />*/}
+      <Partners />
+      <Newsletter />
+    </div>
+  );
+}
