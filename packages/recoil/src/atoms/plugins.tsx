a1179636f27c55c0dac4fc7875a560386798afd7
Armani Ferrante <armaniferrante@gmail.com>
2022-07-25 23:43:10 -0400

    stash

--- packages/recoil/src/atoms/plugins.tsx
+++ packages/recoil/src/atoms/plugins.tsx
@@ -8,11 +8,11 @@ const OPEN_ORDERS_ICON_URL =
 
 const HELLO_WORLD_PLUGIN_URL =
   "https://embed.xnfts.dev/5r1jwBmveWJaJVtkFCUFcboqv4sfYheaoEoBicAiJmEJ";
-const ANCHOR_TABLE_PLUGIN_URL = pluginURL("table-anchor");
-const PSYFI_TABLE_PLUGIN_URL = pluginURL("table-psyfi");
+const ANCHOR_TABLE_PLUGIN_URL = pluginURL("xnfts/table-anchor");
+const PSYFI_TABLE_PLUGIN_URL = pluginURL("xnfts/table-psyfi");
 
-const MANGO_TABLE_PLUGIN_URL = pluginURL("mango");
-const PRICES_PLUGIN_URL = pluginURL("prices");
+const MANGO_TABLE_PLUGIN_URL = pluginURL("xnfts/mango");
+const PRICES_PLUGIN_URL = pluginURL("xnfts/prices");
 const SIMULATOR_URL = `http://localhost:${SIMULATOR_PORT}`;
 
 //
