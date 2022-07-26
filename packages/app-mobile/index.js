c583a61895fb1584f86d9c183bef8910b1c8e414
wentokay <101902546+wentokay@users.noreply.github.com>
2022-07-26 16:44:45 +0100

    store background serviceworker scripts in github pages (#388)
    store packages/background html and serviceworker output at https://coral-xyz.github.io/backpack/background-scripts/COMMIT_SHA

--- packages/app-mobile/index.js
+++ packages/app-mobile/index.js
@@ -7,7 +7,7 @@ import {
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
 import { useRef, Suspense } from "react";
-import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import "react-native-get-random-values";
 import { WebView } from "react-native-webview";
 import { RecoilRoot } from "recoil/native/recoil";
@@ -42,19 +42,13 @@ function Background() {
         cacheMode="LOAD_CACHE_ELSE_NETWORK"
         ref={ref}
         source={{
-          uri:
-            Platform.OS === "android"
-              ? // temporary hack as android can't access localhost. Using
-                // `adb -s emulator-5554 reverse tcp:9333 tcp:9333` is not
-                // reliable. ngrok & localtunnel don't work with dev server.
-                "https://fc9e097a.backpack.pages.dev"
-              : // serviceworkers must be used with SSL (unless its localhost)
-                // & expo iOS apps can only load from localhost in dev mode
-                // because of restrictions imposed by WKAppBoundDomains.
-                // Need to find a workaround to be able to test on iOS devices
-                // in dev, possibly using a custom plugin that'd allow access
-                // to a non-localhost URL that is specific for each build.
-                "http://localhost:9333",
+          uri: process.env.EAS_BUILD_GIT_COMMIT_HASH
+            ? [
+                "https://coral-xyz.github.io/backpack/background-scripts",
+                process.env.EAS_BUILD_GIT_COMMIT_HASH,
+                "service-worker-loader.html",
+              ].join("/")
+            : "http://localhost:9333",
         }}
         onMessage={(event) => {
           const msg = JSON.parse(event.nativeEvent.data);
