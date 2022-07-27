2a08eb80630d2ddeb95d9fb62a4ec662b8405b8e
Tom Linton <tlinton@fastmail.fm>
2022-07-27 21:14:19 +1200

    Stash more logic in recoil

--- packages/recoil/src/atoms/jupiter.tsx
+++ packages/recoil/src/atoms/jupiter.tsx
@@ -1,13 +1,27 @@
 import { atom, selector, selectorFamily } from "recoil";
-import * as atoms from ".";
+import { Transaction } from "@solana/web3.js";
+import { TokenInfo } from "@solana/spl-token-registry";
 import { Blockchain } from "@coral-xyz/common";
+import * as atoms from ".";
 
-const ROUTE_MAP_URL = "https://quote-api.jup.ag/v1/indexed-route-map";
+const JUPITER_BASE_URL = "https://quote-api.jup.ag/v1/";
+
+type JupiterRoute = {
+  amount: number;
+  inAmount: number;
+  otherAmountThreshold: number;
+  outAmount: number;
+  outAmountWithSlippage: number;
+  priceImpactPct: number;
+  swapMode: string;
+};
 
 export const jupiterRouteMap = selector({
   key: "jupiterRouteMap",
   get: async ({}) => {
-    const response = await (await fetch(ROUTE_MAP_URL)).json();
+    const response = await (
+      await fetch(`${JUPITER_BASE_URL}/indexed-route-map`)
+    ).json();
     const getMint = (index: number) => response["mintKeys"][index];
     // Replace indices with mint addresses
     return Object.keys(response["indexedRouteMap"]).reduce((acc, key) => {
@@ -49,9 +63,95 @@ export const swapTokenList = selectorFamily({
         return get(walletJupiterTokens);
       } else {
         const routeMap = get(jupiterRouteMap);
-        return routeMap[mint].map((mint: string) =>
-          get(atoms.solanaMintRegistryMap({ mint }))
-        );
+        return routeMap[mint].map((mint: string) => {
+          const tokenRegistry = get(atoms.splTokenRegistry)!;
+          const tokenMetadata = tokenRegistry.get(mint) ?? ({} as TokenInfo);
+          const { name, symbol, logoURI } = tokenMetadata;
+          return { name, symbol, logoURI };
+        });
       }
     },
 });
+
+/**
+export const jupiterRoutes = selectorFamily({
+  key: "jupiterRoutes",
+  get:
+    ({
+      inputMint,
+      outputMint,
+      amount,
+      slippage,
+      feeBps,
+    }: {
+      inputMint: string;
+      outputMint: string;
+      amount: string;
+      slippage: string;
+      feeBps: string;
+    }) =>
+    async (): Promise<JupiterRoute[]> => {
+      const params = {
+        inputMint,
+        outputMint,
+        amount,
+        slippage,
+        feeBps,
+      };
+      const queryString = new URLSearchParams(params).toString();
+      // TODO: simulate this query failing
+      const { data } = await (
+        await fetch(`${JUPITER_BASE_URL}quote?${queryString}`)
+      ).json();
+      return data;
+    },
+});
+
+export const jupiterTransactions = selector({
+  key: "jupiterTransactions",
+  get: async ({ get }) => {
+    const routes = get(jupiterRoutes);
+    if (routes.length > 0) {
+      const body = {
+        route: routes[0],
+        userPublicKey: get(atoms.activeWallet),
+      };
+      return await (
+        await fetch(`${JUPITER_BASE_URL}swap`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        })
+      ).json();
+    } else {
+      return [];
+    }
+  },
+});
+
+export const jupiterTransactionFee = selector({
+  key: "jupiterTransactionFee",
+  get: async ({ get }) => {
+    const { connection } = get(atoms.anchorContext);
+    const transactions = get(jupiterTransactions);
+    let fee = 0;
+    for (const serializedTransaction of Object.values(transactions)) {
+      const transaction = Transaction.from(
+        Buffer.from(serializedTransaction, "base64")
+      );
+      // Under the hood this just calls connection.getFeeForMessage with
+      // the message, it's a convenience method
+      try {
+        fee += await transaction.getEstimatedFee(connection);
+      } catch {
+        // TODO errors here for connection unavailable intermittently, why?
+        // TODO should we provide no estimate instead? 5000 lamports seems ballpark
+        fee += 5000;
+      }
+    }
+    return fee;
+  },
+});
+**/
