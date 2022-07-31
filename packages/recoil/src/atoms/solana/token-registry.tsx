b31b3af0514059ae0d49a322f0bd30b11c31432a
Tom Linton <tlinton@fastmail.fm>
2022-07-31 19:06:55 +1200

    Rewrite SOL to wSOL for wrapped SOL

--- packages/recoil/src/atoms/solana/token-registry.tsx
+++ packages/recoil/src/atoms/solana/token-registry.tsx
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 import { TokenListProvider, TokenInfo } from "@solana/spl-token-registry";
-import { SOL_NATIVE_MINT } from "@coral-xyz/common";
+import { WSOL_MINT, SOL_NATIVE_MINT } from "@coral-xyz/common";
 
 const SOL_LOGO_URI =
   "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/solana/info/logo.png";
@@ -16,7 +16,11 @@ export const splTokenRegistry = atom<Map<string, TokenInfo> | null>({
             .filterByClusterSlug("mainnet-beta") // TODO: get network atom.
             .getList();
           const tokenMap = tokenList.reduce((map, item) => {
-            map.set(item.address, item);
+            if (item.address === WSOL_MINT) {
+              map.set(item.address, { ...item, symbol: "wSOL" });
+            } else {
+              map.set(item.address, item);
+            }
             return map;
           }, new Map());
           tokenMap.set(SOL_NATIVE_MINT, {
