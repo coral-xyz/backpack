2efe727b7f82fbe2c921a4da4c5dc313fc5a06b0
Armani Ferrante <armaniferrante@gmail.com>
2022-08-04 12:49:17 -0400

    xnft-cli: dev command on fresh repo

--- packages/xnft-cli/index.js
+++ packages/xnft-cli/index.js
@@ -67,6 +67,14 @@ program.command("dev").action(async () => {
     sourceMap: true,
     optimize: false,
   });
+
+  if (!fs.existsSync("dist/index.js")) {
+    if (!fs.existsSync("dist")) {
+      fs.mkdirSync("dist");
+    }
+    fs.writeFileSync("dist/index.js", "");
+  }
+
   let js = fs.readFileSync("dist/index.js", { encoding: "utf-8" });
   await bundler.watch(() => {
     console.log("build changed");
