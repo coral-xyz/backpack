bc17c96ada9b1d3640ba21db6bc948c2db083279
Armani Ferrante <armaniferrante@gmail.com>
2022-08-05 09:42:23 -0400

    react-xnft: fix list types

--- packages/react-xnft/src/sdk/List.tsx
+++ packages/react-xnft/src/sdk/List.tsx
@@ -1,13 +1,20 @@
 import { View } from "../elements";
 import { useTheme } from "../Context";
 
-export function List({ children, style }) {
+// Note: this component currently explects `<ListItem>` children.
+export const List: React.FC<{
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}> = ({ children, style }) => {
   const theme = useTheme();
+  // @ts-ignore
   const isArray = children && children.length !== undefined;
   const childrenArray = isArray ? children : [children];
   const newChildrenArray: Array<React.ReactNode> = [];
+  // @ts-ignore
   childrenArray.forEach((c, idx) => {
     newChildrenArray.push(c);
+    // @ts-ignore
     if (idx !== childrenArray.length - 1) {
       newChildrenArray.push(<Divider />);
     }
@@ -23,9 +30,12 @@ export function List({ children, style }) {
       {newChildrenArray}
     </View>
   );
-}
+};
 
-export function ListItem({ children, style }) {
+export const ListItem: React.FC<{
+  children: Array<React.ReactNode> | React.ReactNode;
+  style?: React.CSSProperties;
+}> = ({ children, style }) => {
   return (
     <View
       style={{
@@ -37,7 +47,7 @@ export function ListItem({ children, style }) {
       {children}
     </View>
   );
-}
+};
 
 function Divider() {
   const theme = useTheme();
