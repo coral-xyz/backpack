7c32d43fcfdf749b685f2f2168563bfc1e051f66
Jordan Sexton <jordan@jordansexton.com>
2022-08-06 18:22:34 -0500

    add window.navigator.wallets interface declaration

--- packages/react-xnft/types.d.ts
+++ packages/react-xnft/types.d.ts
@@ -2,10 +2,15 @@ import type {
   ProviderInjection,
   ProviderUiInjection,
 } from "@coral-xyz/provider-injection";
+import type { NavigatorWallets, WalletAccount } from "@solana/wallet-standard";
 
 declare global {
   interface Window {
     backpack: ProviderInjection;
     xnft: ProviderUiInjection;
+  }
+
+  interface Navigator {
+    wallets?: NavigatorWallets<WalletAccount>;
   }
 }
