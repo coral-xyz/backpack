084da5431ebfefac83dd7d8f9f67b4638e5cb52b
Jordan Sexton <jordan@jordansexton.com>
2022-08-07 12:30:37 -0500

    Make Backpack the first Standard Wallet  (#578)
    * add compressed png icon
    
    * add base64 string of icon
    
    * add @solana/wallet-standard dependency
    
    * add window.navigator.wallets interface declaration
    
    * add Wallet Standard wrapper for window.backpack
    
    * register Backpack as a Standard Wallet
    
    * pass context arg to addEventListener

--- packages/provider-injection/src/index.ts
+++ packages/provider-injection/src/index.ts
@@ -1,5 +1,6 @@
 import { getLogger } from "@coral-xyz/common";
 import { ProviderInjection } from "./provider";
+import { BackpackSolanaWallet } from "./provider-standard";
 import { ProviderXnftInjection } from "./provider-xnft";
 
 const logger = getLogger("provider-injection");
@@ -14,6 +15,13 @@ function main() {
 function initProvider() {
   window.backpack = new ProviderInjection();
   window.xnft = new ProviderXnftInjection();
+
+  window.navigator.wallets = window.navigator.wallets || [];
+  window.navigator.wallets.push({
+    method: "register",
+    wallets: [new BackpackSolanaWallet()],
+    callback() {},
+  });
 }
 
 main();
