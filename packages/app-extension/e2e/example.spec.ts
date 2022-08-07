14535037029e58a90af8191de0e86e3dbc2dfc41
wentokay <wentokay@users.noreply.github.com>
2022-08-07 10:38:15 -0400

    add playwright e2e test

---
+++ packages/app-extension/e2e/example.spec.ts
@@ -0,0 +1,58 @@
+// import { expect, test } from "@playwright/test";
+
+import { chromium, test as base } from "@playwright/test";
+import path from "path";
+
+const extensionPath = path.join(__dirname, "../build"); // make sure this is correct
+
+const test = base.extend({
+  context: async ({ browserName }, use) => {
+    // const browserTypes = { chromium, webkit };
+    const browserTypes = { chromium };
+    const launchOptions = {
+      devtools: false,
+      headless: false,
+      args: [`--disable-extensions-except=${extensionPath}`],
+      viewport: {
+        width: 400,
+        height: 700,
+      },
+    };
+    const context = await browserTypes[browserName].launchPersistentContext(
+      "",
+      launchOptions
+    );
+    await use(context);
+    await context.close();
+  },
+});
+
+const EXTENSION_ID = "jclcndnaoggcdljhefioenkobihhahge";
+
+test.describe("Setup", () => {
+  test("our extension loads", async ({ page, context }) => {
+    const VALID_PASSWORD = "password1234";
+
+    await page.goto(
+      `chrome-extension://${EXTENSION_ID}/options.html?onboarding=true`
+    );
+    await page.locator("text=Create a new Wallet").click();
+
+    await page.locator('input[name="password"]').fill(VALID_PASSWORD);
+    await page
+      .locator('input[name="password-confirmation"]')
+      .fill(VALID_PASSWORD);
+    await page.locator("text=Next").click();
+
+    await page.locator('input[type="checkbox"]').click();
+    await page.locator("text=Next").click();
+
+    await page.locator("text=Finish").click();
+
+    const setup = await context.newPage();
+
+    await setup.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+    await setup.locator('input[type="password"]').fill(VALID_PASSWORD);
+    await setup.locator("text=Unlock").click();
+  });
+});
