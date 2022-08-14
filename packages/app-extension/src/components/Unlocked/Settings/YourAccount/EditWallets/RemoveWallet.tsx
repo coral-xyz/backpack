711b9d1f6d119463a5819ac742e38aa254ad2c1b
Armani Ferrante <armaniferrante@gmail.com>
2022-08-14 19:14:39 -0400

    stash

--- packages/app-extension/src/components/Unlocked/Settings/YourAccount/EditWallets/RemoveWallet.tsx
+++ packages/app-extension/src/components/Unlocked/Settings/YourAccount/EditWallets/RemoveWallet.tsx
@@ -4,7 +4,8 @@ import { UI_RPC_METHOD_KEYRING_KEY_DELETE } from "@coral-xyz/common";
 import { useCustomTheme } from "@coral-xyz/themes";
 import { useBackgroundClient } from "@coral-xyz/recoil";
 import { useNavStack } from "../../../../common/Layout/NavStack";
-import { SuccessIcon, WarningIcon } from "../../../../common/Icon";
+import { WithMiniDrawer } from "../../../../common/Layout/Drawer";
+import { CheckIcon, WarningIcon } from "../../../../common/Icon";
 import { SecondaryButton, PrimaryButton } from "../../../../common";
 
 export const RemoveWallet: React.FC<{
@@ -15,7 +16,7 @@ export const RemoveWallet: React.FC<{
   const theme = useCustomTheme();
   const nav = useNavStack();
   const background = useBackgroundClient();
-  const [showSuccess, setShowSuccess] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(true);
 
   useEffect(() => {
     nav.setTitle("Remove Wallet");
@@ -25,129 +26,159 @@ export const RemoveWallet: React.FC<{
     publicKey.slice(0, 4) + "..." + publicKey.slice(publicKey.length - 4);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        height: "100%",
-        paddingBottom: "16px",
-      }}
-    >
-      <div
-        style={{
-          marginLeft: "36px",
-          marginRight: "36px",
-          marginTop: "45px",
-          textAlign: "center",
-        }}
-      >
-        {showSuccess ? (
-          <SuccessIcon />
-        ) : (
-          <WarningIcon fill={theme.custom.colors.negative} />
-        )}
-        <Typography
-          style={{
-            fontWeight: 500,
-            fontSize: "24px",
-            lineHeight: "32px",
-            textAlign: "center",
-            marginTop: "30px",
-          }}
-        >
-          {showSuccess
-            ? "Wallet removed"
-            : `Are you sure you want to remove ${pubkeyStr}?`}
-        </Typography>
-        <Typography
-          style={{
-            textAlign: "center",
-            color: theme.custom.colors.secondary,
-            fontSize: "16px",
-            lineHeight: "24px",
-            fontWeight: 500,
-            marginTop: "8px",
-          }}
-        >
-          {showSuccess ? (
-            <>
-              {type === "derived" ? (
-                <>
-                  This wallet is no longer available in Backpack, but can be
-                  reaccessed by importing your secret recovery phrase in a new
-                  Backpack.
-                </>
-              ) : type === "ledger" ? (
-                <>
-                  This wallet is no longer availble in Backpack, but can be
-                  reaccessed by connecting your ledger again.
-                </>
-              ) : (
-                <>This wallet is no longer available in Backpack.</>
-              )}
-            </>
-          ) : (
-            <>
-              {type === "derived" ? (
-                <>
-                  Removing from Backpack will not delete the wallet’s contents.
-                  It will still be available by importing your secret recovery
-                  phrase in a new Backpack.
-                </>
-              ) : type === "ledger" ? (
-                <>
-                  Removing from Backpack will not delete the wallet’s contents.
-                  It will still be available by connecting your ledger.
-                </>
-              ) : (
-                <>
-                  Removing from Backpack will delete the wallet’s keypair. Make
-                  sure you have exported and saved the private key before
-                  removing.
-                </>
-              )}
-            </>
-          )}
-        </Typography>
-      </div>
+    <>
       <div
         style={{
           display: "flex",
-          marginLeft: "16px",
-          marginRight: "16px",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%",
+          paddingBottom: "16px",
         }}
       >
-        {showSuccess ? (
+        <div
+          style={{
+            marginLeft: "36px",
+            marginRight: "36px",
+            marginTop: "45px",
+            textAlign: "center",
+          }}
+        >
+          <WarningIcon fill={theme.custom.colors.negative} />
+          <Typography
+            style={{
+              fontWeight: 500,
+              fontSize: "24px",
+              lineHeight: "32px",
+              textAlign: "center",
+              marginTop: "30px",
+            }}
+          >
+            {`Are you sure you want to remove ${pubkeyStr}?`}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "center",
+              color: theme.custom.colors.secondary,
+              fontSize: "16px",
+              lineHeight: "24px",
+              fontWeight: 500,
+              marginTop: "8px",
+            }}
+          >
+            {type === "derived" ? (
+              <>
+                Removing from Backpack will not delete the wallet’s contents. It
+                will still be available by importing your secret recovery phrase
+                in a new Backpack.
+              </>
+            ) : type === "ledger" ? (
+              <>
+                Removing from Backpack will not delete the wallet’s contents. It
+                will still be available by connecting your ledger.
+              </>
+            ) : (
+              <>
+                Removing from Backpack will delete the wallet’s keypair. Make
+                sure you have exported and saved the private key before
+                removing.
+              </>
+            )}
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            marginLeft: "16px",
+            marginRight: "16px",
+          }}
+        >
+          <SecondaryButton
+            label={"Cancel"}
+            style={{ marginRight: "8px" }}
+            onClick={() => nav.pop()}
+          />
           <PrimaryButton
-            label={"Done"}
+            label={"Remove"}
+            style={{ backgroundColor: theme.custom.colors.negative }}
             onClick={() => {
-              nav.pop();
+              (async () => {
+                await background.request({
+                  method: UI_RPC_METHOD_KEYRING_KEY_DELETE,
+                  params: [publicKey],
+                });
+                setShowSuccess(true);
+              })();
             }}
           />
-        ) : (
-          <>
-            <SecondaryButton
-              label={"Cancel"}
-              style={{ marginRight: "8px" }}
-              onClick={() => nav.pop()}
-            />
-            <PrimaryButton
-              label={"Remove"}
-              style={{ backgroundColor: theme.custom.colors.negative }}
-              onClick={() => {
-                (async () => {
-                  await background.request({
-                    method: UI_RPC_METHOD_KEYRING_KEY_DELETE,
-                    params: [publicKey],
-                  });
-                  setShowSuccess(true);
-                })();
-              }}
-            />
-          </>
-        )}
+        </div>
       </div>
-    </div>
+      <WithMiniDrawer
+        openDrawer={showSuccess}
+        setOpenDrawer={setShowSuccess}
+        backdropProps={{
+          style: {
+            opacity: 0.8,
+            background: "#18181b",
+          },
+        }}
+        onClose={() => {
+          setShowSuccess(false);
+          nav.pop(2);
+        }}
+      >
+        <div
+          style={{
+            borderTopLeftRadius: "12px",
+            borderTopRightRadius: "12px",
+            height: "100%",
+            background: theme.custom.colors.background,
+          }}
+        >
+          <div
+            style={{
+              height: "100%",
+              borderTopLeftRadius: "12px",
+              borderTopRightRadius: "12px",
+              background: theme.custom.colors.drawerGradient,
+              paddingTop: "32px",
+              paddingBottom: "16px",
+              paddingLeft: "16px",
+              paddingRight: "16px",
+            }}
+          >
+            <div
+              style={{
+                marginLeft: "36px",
+                marginRight: "36px",
+                textAlign: "center",
+              }}
+            >
+              <CheckIcon />
+            </div>
+            <div>
+              <Typography
+                style={{
+                  fontWeight: 500,
+                  fontSize: "24px",
+                  lineHeight: "32px",
+                  textAlign: "center",
+                  marginTop: "16px",
+                  marginBottom: "32px",
+                }}
+              >
+                Wallet removed
+              </Typography>
+              <PrimaryButton
+                label={"Done"}
+                onClick={() => {
+                  nav.pop(2);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </WithMiniDrawer>
+    </>
   );
 };
