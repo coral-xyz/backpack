97bcdc30163339c1aff2c2a688b9ec0d1e5fa154
Armani Ferrante <armaniferrante@gmail.com>
2022-08-16 00:14:27 -0400

    stash

--- packages/react-xnft/src/hooks.tsx
+++ packages/react-xnft/src/hooks.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Connection, PublicKey } from "@solana/web3.js";
 
 export function usePublicKey(): PublicKey {
-  console.log("using publicKey here", window.xnft.publicKey.toString());
   const [publicKey, setPublicKey] = useState(window.xnft.publicKey);
   useEffect(() => {
     window.xnft.on("publicKeyUpdate", () => {
