4724de630d42449e1f2bdf99bd7cb1baefdf955a
Jordan Sexton <jordan@jordansexton.com>
2022-08-15 19:40:14 -0500

    externalize standard wallet package in development (#630)

--- packages/provider-injection/src/index.ts
+++ packages/provider-injection/src/index.ts
@@ -1,6 +1,6 @@
 import { getLogger } from "@coral-xyz/common";
+import { register } from "@wallet-standard/wallets-backpack";
 import { ProviderSolanaInjection } from "./provider-solana";
-import { BackpackSolanaWallet } from "./provider-standard";
 import { ProviderXnftInjection } from "./provider-xnft";
 
 const logger = getLogger("provider-injection");
@@ -16,12 +16,7 @@ function initProvider() {
   window.backpack = new ProviderSolanaInjection();
   window.xnft = new ProviderXnftInjection();
 
-  window.navigator.wallets = window.navigator.wallets || [];
-  window.navigator.wallets.push({
-    method: "register",
-    wallets: [new BackpackSolanaWallet()],
-    callback() {},
-  });
+  register();
 }
 
 main();
--- packages/react-xnft/types.d.ts
+++ packages/react-xnft/types.d.ts
@@ -2,18 +2,10 @@ import type {
   ProviderInjection,
   ProviderUiInjection,
 } from "@coral-xyz/provider-injection";
-import type {
-  NavigatorWallets,
-  WalletAccount,
-} from "@wallet-standard/standard";
 
 declare global {
   interface Window {
     backpack: ProviderInjection;
     xnft: ProviderUiInjection;
-  }
-
-  interface Navigator {
-    wallets?: NavigatorWallets<WalletAccount>;
   }
 }
