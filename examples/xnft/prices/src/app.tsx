2ed525f963099d4347311867813d139b73e8fde4
Armani Ferrante <armaniferrante@gmail.com>
2022-08-15 23:55:55 -0400

    stash

--- examples/xnft/prices/src/app.tsx
+++ examples/xnft/prices/src/app.tsx
@@ -1,13 +1,24 @@
-import ReactXnft, { Text, View } from "react-xnft";
-
-ReactXnft.events.on("connect", () => {
-  // no-op
-});
+import { useState, useEffect } from "react";
+import ReactXnft, { usePublicKey, Text, View } from "react-xnft";
 
 export function App() {
+  const pk = useHere();
+  console.log("WTF", pk.toString());
   return (
     <View>
-      <Text>Hello, World!</Text>
+      <Text>Hello, World! {pk.toString()}</Text>
     </View>
   );
+}
+
+function useHere() {
+  //	console.log("initial render here", window.xnft.publicKey);
+  const [publicKey, setPublicKey] = useState(window.xnft.publicKey);
+  useEffect(() => {
+    window.xnft.on("publicKeyUpdate", () => {
+      console.log("updated here", window.xnft.pubicKey);
+      setPublicKey(window.xnft.publicKey);
+    });
+  }, [setPublicKey]);
+  return publicKey;
 }
