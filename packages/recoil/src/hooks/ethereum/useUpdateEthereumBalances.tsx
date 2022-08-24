ae919bec7fc2e44b80a91b1d6b5de968dac9292a
Tom Linton <tlinton@fastmail.fm>
2022-08-24 20:37:56 +1200

    convert balance updates to map

--- packages/recoil/src/hooks/ethereum/useUpdateEthereumBalances.tsx
+++ packages/recoil/src/hooks/ethereum/useUpdateEthereumBalances.tsx
@@ -18,7 +18,7 @@ export const useUpdateEthereumBalances = () =>
             connectionUrl,
             publicKey,
           }),
-          balances
+          new Map(Object.entries(balances))
         );
       }
   );
