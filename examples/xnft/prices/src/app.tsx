48f09d2d6559d963b07e8443d78af2074f7c2dbe
Armani Ferrante <armaniferrante@gmail.com>
2022-08-30 19:50:19 -0400

    Revert "undo me"
    This reverts commit 0dca14d3d83f164487ebfb28602530736cdae241.

--- examples/xnft/prices/src/app.tsx
+++ examples/xnft/prices/src/app.tsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
-import {
-  Iframe,
-  Image,
-  useConnection,
-  usePublicKey,
-  Text,
-  View,
-} from "react-xnft";
+import { useEffect } from "react";
+import { useConnection, usePublicKey, Text, View } from "react-xnft";
 
 export function App() {
-  const [count, setCount] = useState(0);
   const pk = usePublicKey();
   const conn = useConnection();
   console.log("ARMANI WTF", pk.toString());
@@ -22,14 +14,9 @@ export function App() {
     })();
   }, []);
 
-  const a =
-    "https://pbs.twimg.com/profile_images/1562977867948085248/af4SiYBK_400x400.jpg";
-  const b =
-    "https://pbs.twimg.com/profile_images/1537219386116800513/chacpod0_400x400.jpg";
-
   return (
     <View>
-      <Iframe xnft src="http://localhost:3001" />
+      <Text>Hello, World! {pk.toString()}</Text>
     </View>
   );
 }
