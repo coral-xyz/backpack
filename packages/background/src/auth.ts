d63776c1243cd5c96da9d1ce05f59e9f2c2ef52c
wentokay <wentokay@users.noreply.github.com>
2022-09-19 11:47:00 -0700

    refactor auth

--- packages/background/src/auth.ts
+++ packages/background/src/auth.ts
@@ -1,4 +1,58 @@
-const FIXED_SALT = "backpack";
+const API_URL = "https://authenticator.backpack.workers.dev";
+
+export const checkInviteCode = async (inviteCode: string) => {
+  try {
+    const cleanCode = inviteCode.trim();
+    if (
+      !cleanCode.match(
+        /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+      )
+    ) {
+      throw new Error("Invalid invite code");
+    }
+    await apiRequest("check-invite-code", { inviteCode: cleanCode });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export const signup = async (
+  username: string,
+  password: string,
+  inviteCode: string
+) => {
+  try {
+    if (!username.match(/^[a-z0-9_]{4,16}$/)) {
+      throw new Error(
+        "username must be 4-16 characters long and only contain letters, numbers, and underscores"
+      );
+    }
+    await apiRequest("/signup", {
+      username,
+      password: await createInsecurePasswordHashForTransit(password),
+      invite_code: inviteCode,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export const signin = async (username: string, password: string) =>
+  apiRequest("signin", {
+    username,
+    password: await createInsecurePasswordHashForTransit(password),
+  });
+
+const apiRequest = async (path: string, body: any = {}) => {
+  const res = await fetch([API_URL, path].join("/"), {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  if (!res.ok) throw new Error(await res.text());
+};
 
 const sha256 = async (message: string) => {
   const msgBuffer = new TextEncoder().encode(message);
@@ -10,62 +64,5 @@ const sha256 = async (message: string) => {
   return hashHex;
 };
 
-const createInsecurePasswordHash = (password: string) =>
-  sha256(`${password}${FIXED_SALT}`);
-
-export const checkInviteCode = async (inviteCode: string) => {
-  const cleanCode = inviteCode.trim();
-  if (
-    !cleanCode.match(
-      /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
-    )
-  ) {
-    throw new Error("Invalid invite code");
-  }
-  const res = await fetch("http://localhost:8787/check-invite-code", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({ code: cleanCode }),
-  });
-  if (!res.ok) throw new Error(await res.text());
-};
-
-export const signup = async (
-  username: string,
-  password: string,
-  inviteCode: string
-) => {
-  if (!username.match(/^[a-z0-9_]{4,16}$/)) {
-    throw new Error(
-      "username must be 4-16 characters long and only contain letters, numbers, and underscores"
-    );
-  }
-  const res = await fetch("http://localhost:8787/signup", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password: await createInsecurePasswordHash(password),
-      invite_code: inviteCode,
-    }),
-  });
-  if (!res.ok) throw new Error(await res.text());
-};
-
-export const signin = async (username: string, password: string) => {
-  const res = await fetch("http://localhost:8787/signin", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password: await createInsecurePasswordHash(password),
-    }),
-  });
-  if (!res.ok) throw new Error(await res.text());
-};
+const createInsecurePasswordHashForTransit = (password: string) =>
+  sha256(password);
