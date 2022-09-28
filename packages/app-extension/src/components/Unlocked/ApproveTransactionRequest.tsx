12bd69f21a31c022e24a00b38591ab238ec93f54
Harkirat Singh <harkirat96@gmail.com>
2022-09-29 00:20:07 +0530

    Catches transaction errors in ApproveTransactionRequest

--- packages/app-extension/src/components/Unlocked/ApproveTransactionRequest.tsx
+++ packages/app-extension/src/components/Unlocked/ApproveTransactionRequest.tsx
@@ -312,12 +312,14 @@ function _SendTransactionRequest({
   // into this component because it can be modified by the user to set
   // transaction specific settings (i.e. Etheruem gas).
   //
-  const onConfirm = async () => {
-    const signature = await background.request({
-      method: uiRpcMethod,
-      params: [transactionToSend, publicKey],
-    });
-    onResolve(signature);
+  const onConfirm = () => {
+    background
+      .request({
+        method: uiRpcMethod,
+        params: [transactionToSend, publicKey],
+      })
+      .then((signature) => onResolve(signature))
+      .catch((_) => onReject());
   };
 
   //
@@ -480,12 +482,14 @@ function SignMessageRequest({
   //
   // Executes when the modal clicks "Approve" in the drawer popup
   //
-  const onConfirm = async () => {
-    const signature = await background.request({
-      method: uiRpcMethod,
-      params: [message, publicKey],
-    });
-    onResolve(signature);
+  const onConfirm = () => {
+    background
+      .request({
+        method: uiRpcMethod,
+        params: [message, publicKey],
+      })
+      .then((signature) => onResolve(signature))
+      .catch((_) => onReject());
   };
 
   return (
