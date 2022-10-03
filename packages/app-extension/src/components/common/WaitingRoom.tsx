24d13eb6a8f1b16c7cc5b213f8241e0651d45de2
Matthew Callens <callensmatt@gmail.com>
2022-10-03 14:30:29 -0400

    remove nav stack title from waiting room

--- packages/app-extension/src/components/common/WaitingRoom.tsx
+++ packages/app-extension/src/components/common/WaitingRoom.tsx
@@ -1,4 +1,6 @@
 import { useUsername } from "@coral-xyz/recoil";
+import { useEffect } from "react";
+import { useNavStack } from "./Layout/NavStack";
 
 const WAITLIST_RES_ID_KEY = "waitlist-form-res-id";
 
@@ -8,19 +10,27 @@ export const setWaitlistId = (responseId: string) =>
 export const getWaitlistId = () =>
   window.localStorage.getItem(WAITLIST_RES_ID_KEY) ?? undefined;
 
-const WaitingRoom = ({ onboarded }: { onboarded?: boolean }) => (
-  <iframe
-    style={{
-      border: "none",
-      height: "98%",
-      width: "100%",
-      overflow: "hidden",
-    }}
-    allow="clipboard-read; clipboard-write"
-    src={`https://beta-waiting-room.vercel.app/?id=${
-      onboarded ? useUsername() : getWaitlistId()
-    }&onboarded=${onboarded ?? false}&v=2`}
-  />
-);
+const WaitingRoom = ({ onboarded }: { onboarded?: boolean }) => {
+  const nav = useNavStack();
+
+  useEffect(() => {
+    nav.setTitle("");
+  }, []);
+
+  return (
+    <iframe
+      style={{
+        border: "none",
+        height: "98%",
+        width: "100%",
+        overflow: "hidden",
+      }}
+      allow="clipboard-read; clipboard-write"
+      src={`https://beta-waiting-room.vercel.app/?id=${
+        onboarded ? useUsername() : getWaitlistId()
+      }&onboarded=${onboarded ?? false}&v=2`}
+    />
+  );
+};
 
 export default WaitingRoom;
