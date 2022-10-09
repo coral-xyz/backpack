06ac6fe3c418008574e1ca5db8723c8e3d3b64fe
kirat <harkirat96@gmail.com>
2022-10-10 04:09:25 +0530

    Follow ups versioned transactions (#1052)

--- packages/common/src/solana/transaction-helpers.ts
+++ packages/common/src/solana/transaction-helpers.ts
@@ -3,20 +3,7 @@ import { ethers } from "ethers";
 const { base58: bs58 } = ethers.utils;
 
 export const deserializeTransaction = (serializedTx: string) => {
-  try {
-    const transaction = Transaction.from(bs58.decode(serializedTx));
-    return transaction;
-  } catch (e) {
-    return VersionedTransaction.deserialize(bs58.decode(serializedTx));
-  }
-};
-
-export const getSerializedMessage = (txStr: string) => {
-  const tx = deserializeTransaction(txStr);
-  const versioned = "version" in tx;
-  return !versioned
-    ? Transaction.from(bs58.decode(txStr)).serializeMessage()
-    : VersionedTransaction.deserialize(bs58.decode(txStr)).message.serialize();
+  return VersionedTransaction.deserialize(bs58.decode(serializedTx));
 };
 
 export const isVersionedTransaction = (
