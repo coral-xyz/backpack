6a0d7364dafa8df7cb1c377dcb8aed3c37ec770f
wentokay <101902546+wentokay@users.noreply.github.com>
2022-10-11 11:08:18 -0700

    encode password for memoryrouter (#1063)

--- packages/app-extension/src/components/Onboarding/pages/Finish.tsx
+++ packages/app-extension/src/components/Onboarding/pages/Finish.tsx
@@ -48,7 +48,7 @@ export const Finish = () => {
           params: [
             params.mnemonic,
             derivationPath,
-            params.password,
+            decodeURIComponent(params.password!),
             accounts,
             _username,
             params.inviteCode,
