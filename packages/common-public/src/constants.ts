f383a3b627f9c63b82def56fd56ac982160c0dd6
kirat <harkirat96@gmail.com>
2022-10-12 19:48:13 +0530

    Added cdn (#1080)

--- packages/common-public/src/constants.ts
+++ packages/common-public/src/constants.ts
@@ -27,4 +27,4 @@ export const CHANNEL_PLUGIN_REACT_RECONCILER_BRIDGE =
 
 export const MOBILE_CHANNEL_LOGS = "mobile-logs";
 
-export const IMAGE_PROXY_URL = "https://proxy-img.fly.dev";
+export const IMAGE_PROXY_URL = "https://d1a7965zcx2lcc.cloudfront.net";
