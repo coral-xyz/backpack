c9018125b611b2bd94619d70bdd11ae1a5ad67b8
Matthew Callens <callensmatt@gmail.com>
2022-10-17 11:40:32 -0400

    add new table to manage the review queue for appstore curation (#1154)

---
+++ backend/reef/hasura/migrations/default/1666019527389_create_schema_appstore/down.sql
@@ -0,0 +1,1 @@
+drop schema "appstore" cascade;
---
+++ backend/reef/hasura/migrations/default/1666019774360_create_table_appstore_reviews/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "appstore"."reviews" ("id" serial NOT NULL, "created_at" timestamptz NOT NULL, "authority" text NOT NULL, "xnft" text NOT NULL, "data" jsonb NOT NULL, PRIMARY KEY ("id","xnft") , UNIQUE ("id"), UNIQUE ("xnft"));
