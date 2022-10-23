36c7774b2866097b1c6d4b7356b9244960ac6919
Harkirat Singh <harkirat96@gmail.com>
2022-10-24 03:59:32 +0530

    minor

--- packages/app-extension/src/components/Unlocked/Balances/TokensWidget/Ramp.tsx
+++ packages/app-extension/src/components/Unlocked/Balances/TokensWidget/Ramp.tsx
@@ -134,7 +134,7 @@ export function Ramp({
             },
           }}
         />
-        <RampCard2
+        <RampCard
           searchFilter={searchFilter}
           key={blockchain}
           blockchain={blockchain}
@@ -167,7 +167,7 @@ export function Ramp({
         }}
       >
         {activeWallets.map(({ blockchain, name, publicKey }) => (
-          <RampCard2
+          <RampCard
             searchFilter={searchFilter}
             key={blockchain}
             blockchain={blockchain}
@@ -183,7 +183,7 @@ export function Ramp({
   );
 }
 
-export function RampCard2({
+export function RampCard({
   blockchain,
   name,
   publicKey,
@@ -246,76 +246,6 @@ export function RampCard2({
           ))}
       </BalancesTableContent>
     </BalancesTable>
-  );
-}
-export function RampCard({
-  blockchain,
-  name,
-  publicKey,
-  onStartRamp,
-}: {
-  blockchain: Blockchain;
-  name: string;
-  publicKey: string;
-  onStartRamp: any;
-}) {
-  const theme = useCustomTheme();
-  const blockchainLogo = useBlockchainLogo(blockchain);
-  const startRamp = () => {
-    onStartRamp({
-      blockchain,
-      publicKey,
-    });
-  };
-
-  return (
-    <>
-      <div
-        style={{
-          marginBottom: "12px",
-          borderRadius: "8px",
-          padding: "10px",
-          background: theme.custom.colors.nav,
-          border: `${theme.custom.colors.borderFull}`,
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <div>
-            <Typography
-              style={{
-                color: theme.custom.colors.fontColor,
-                fontWeight: 500,
-              }}
-            >
-              {`${name} (${walletAddressDisplay(publicKey)})`}
-            </Typography>
-            <div>
-              <img
-                src={blockchainLogo}
-                style={{
-                  width: "14px",
-                  borderRadius: "2px",
-                }}
-              />
-            </div>
-          </div>
-          <div style={{ display: "flex" }}>
-            <PrimaryButton
-              onClick={startRamp}
-              label={"Buy using Link"}
-              style={{
-                marginTop: "40px",
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </>
   );
 }
 
