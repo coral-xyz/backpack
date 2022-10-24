ce0e1cf2596c59d2fef3e31e26347437219fc664
Jordan Sexton <jordan@jordansexton.com>
2022-10-24 00:08:02 -0500

    add setup function

--- packages/wallet-standard/src/register.ts
+++ packages/wallet-standard/src/register.ts
@@ -1,12 +1,10 @@
 import type { ProviderSolanaInjection } from '@coral-xyz/provider-core';
-import type { WalletsWindow } from '@wallet-standard/base';
+import { setupWindowNavigatorWallets } from './setup.js';
 import { BackpackWallet } from './wallet.js';
-
-declare const window: WalletsWindow;
 
 export function register(backpack: ProviderSolanaInjection): void {
     try {
-        (window.navigator.wallets ||= []).push(({ register }) => register(new BackpackWallet(backpack)));
+        setupWindowNavigatorWallets(({ register }) => register(new BackpackWallet(backpack)));
     } catch (error) {
         console.error(error);
     }
