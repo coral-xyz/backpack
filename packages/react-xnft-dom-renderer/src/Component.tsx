eb5ad8654dbba77989c5ea672ca15b73932a88c3
kirat <harkirat96@gmail.com>
2022-10-26 05:46:06 +0530

    Fixes scrollbars (#1276)

--- packages/react-xnft-dom-renderer/src/Component.tsx
+++ packages/react-xnft-dom-renderer/src/Component.tsx
@@ -21,6 +21,7 @@ import { TextareaAutosize as MuiTextArea } from "@mui/base";
 import { ExpandMore, ExpandLess } from "@mui/icons-material";
 import { ViewRenderer } from "./ViewRenderer";
 import { useDomContext } from "./Context";
+import { ScrollbarNew } from "./components/Scrollbar";
 
 const useStyles = styles((theme) => ({
   blockchainLogo: {
@@ -967,56 +968,7 @@ export function ScrollBarImpl(props: any) {
   const theme = useCustomTheme();
   return (
     <>
-      <Scrollbars
-        style={{ width: "100%", height: "100%" }}
-        renderTrackHorizontal={(props) => (
-          <div {...props} className="track-horizontal" />
-        )}
-        renderTrackVertical={(props) => (
-          <div
-            style={{ backgroundColor: theme.custom.colors.scrollbarTrack }}
-            {...props}
-            className="track-vertical"
-          />
-        )}
-        renderThumbHorizontal={(props) => (
-          <div {...props} className="thumb-horizontal" />
-        )}
-        renderThumbVertical={(props) => (
-          <div
-            style={{ backgroundColor: theme.custom.colors.scrollbarThumb }}
-            {...props}
-            className="thumb-vertical"
-          />
-        )}
-        renderView={(props) => (
-          <div
-            {...props}
-            style={{
-              position: "absolute",
-              top: "0px",
-              left: "0px",
-              right: "0px",
-              bottom: "0px",
-              overflow: "auto",
-            }}
-          />
-        )}
-        autoHide
-        thumbMinSize={30}
-      >
-        {props.children}
-      </Scrollbars>
-      <style>
-        {`
-          .track-vertical {
-            background: ${theme.custom.colors.scrollbarTrack};
-          }
-          .track-vertical .thumb-vertical {
-            background-color: ${theme.custom.colors.scrollbarThumb};
-          }
-				`}
-      </style>
+      <ScrollbarNew {...props}>{props.children}</ScrollbarNew>
     </>
   );
 }
