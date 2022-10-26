0932eaeeacd32339903cee9c3300881cefddfaa4
Harkirat Singh <harkirat96@gmail.com>
2022-10-27 04:54:46 +0530

    Checkpoint

---
+++ packages/themes/src/renderer.ts
@@ -0,0 +1,29 @@
+import {
+  BACKGROUND_COLOR_1,
+  FONT_COLOR_2,
+  LIGHT_BACKGROUND_COLOR_1,
+  LIGHT_FONT_COLOR_2,
+  LIGHT_TEXT_COLOR,
+  TEXT_COLOR,
+} from "./index";
+
+let BACKGROUND_COLOR_1;
+export const lightTheme = {
+  custom: {
+    colors: {
+      text: LIGHT_TEXT_COLOR,
+      textBackground: LIGHT_BACKGROUND_COLOR_1,
+      fontColor2: LIGHT_FONT_COLOR_2,
+    },
+  },
+};
+
+export const darkTheme = {
+  custom: {
+    colors: {
+      text: TEXT_COLOR,
+      textBackground: BACKGROUND_COLOR_1,
+      fontColor2: FONT_COLOR_2,
+    },
+  },
+};
