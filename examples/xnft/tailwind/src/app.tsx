18783aed10db4f35c679bc526c3641a4227c9cd0
kirat <harkirat96@gmail.com>
2022-10-29 00:37:59 +0530

    Fixed styles (#1323)

--- examples/xnft/tailwind/src/app.tsx
+++ examples/xnft/tailwind/src/app.tsx
@@ -70,6 +70,50 @@ function Metadata() {
           onChange={(e) => setInputText(e.target.value)}
         />
       </View>
+      <View tw="p-4">
+        <View tw="relative">
+          <View tw="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <Svg
+              aria-hidden="true"
+              tw="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <Path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></Path>
+            </Svg>
+          </View>
+          <TextField
+            tw="block p-4 pl-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Amount"
+          />
+          <Button
+            type="submit"
+            tw="flex text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <Image
+              tw="w-8 pr-2"
+              src={
+                "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png"
+              }
+            />
+            <View>ETH</View>
+            <Svg
+              tw="fill-current w-4 h-4 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <Path d="M19 9l-7 7-7-7" />
+            </Svg>
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }
