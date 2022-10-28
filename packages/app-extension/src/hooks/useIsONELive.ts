44bd8b223cd8760d4a2319017697eaa080a78606
Philipp Adrian <hello@philippadrian.com>
2022-10-28 11:02:48 -0300

    One Entry Skeleton

--- packages/app-extension/src/hooks/useIsONELive.ts
+++ packages/app-extension/src/hooks/useIsONELive.ts
@@ -5,7 +5,7 @@ const isLiveCheck = fetch("https://xnft.wao.gg/api/isLive")
   .catch(() => ({ isLive: false }));
 
 export function useIsONELive() {
-  const [isLive, setIsLive] = useState(false);
+  const [isLive, setIsLive] = useState<boolean | "loading">("loading");
   useEffect(() => {
     isLiveCheck.then((response) => {
       setIsLive(response.isLive);
