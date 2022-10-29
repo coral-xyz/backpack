7cb394ab43c4830354e2763beffb409618ad0bff
Harkirat Singh <harkirat96@gmail.com>
2022-10-29 19:35:32 +0530

    Removed onramp

--- backend/workers/onramp/src/zodTypes.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const ZodChain = z.enum(["solana", "ethereum"]);
-export type BlockChain = z.infer<typeof ZodChain>;
-
-export const CreateSessionRequest = z.object({
-  chain: ZodChain,
-  publicKey: z.string(),
-});
