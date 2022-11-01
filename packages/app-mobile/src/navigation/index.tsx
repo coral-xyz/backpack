a8c89fbc3f3780c52055c65238b22f636697257f
Peter Piekarczyk <peter@peterp.me>
2022-11-01 13:10:11 -0500

    onboarding navigation

--- packages/app-mobile/src/navigation/index.tsx
+++ packages/app-mobile/src/navigation/index.tsx
@@ -1,12 +1,19 @@
-import { UI_RPC_METHOD_KEYRING_STORE_UNLOCK } from "@coral-xyz/common";
+import {
+  BACKPACK_LINK,
+  DISCORD_INVITE_LINK,
+  TWITTER_LINK,
+  UI_RPC_METHOD_KEYRING_STORE_UNLOCK,
+} from "@coral-xyz/common";
 import { useBackgroundClient, useKeyringStoreState } from "@coral-xyz/recoil";
 import {
   DarkTheme,
   DefaultTheme,
   NavigationContainer,
 } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Linking } from "expo-linking";
 import { useForm } from "react-hook-form";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import tw from "twrnc";
 
 import { CustomButton } from "../components/CustomButton";
@@ -14,15 +21,97 @@ import { ErrorMessage } from "../components/ErrorMessage";
 import { PasswordInput } from "../components/PasswordInput";
 import ResetAppButton from "../components/ResetAppButton";
 import { ButtonFooter, MainContent } from "../components/Templates";
-
-import UnlockedNavigator from "./UnlockedNavigator";
 import OnboardingNavigator from "./OnboardingNavigator";
+import UnlockedNavigator from "./UnlockedNavigator";
+
+const Stack = createStackNavigator();
 
 interface FormData {
   password: string;
 }
 
-const LockedScreen = () => {
+function ListItem({ label, onPress }) {
+  return (
+    <View
+      style={{
+        padding: 8,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Button title={label} onPress={onPress} />
+    </View>
+  );
+}
+
+function LockedHelpMenuModal({ navigation }) {
+  const options = [
+    {
+      // icon: <AccountCircleIcon style={{ color: theme.custom.colors.icon }} />,
+      text: "Reset Secret Recovery Phrase",
+      onPress: () => {
+        navigation.push("Reset");
+      },
+      // onPress: () => nav.push("reset"),
+      // suffix: (
+      //   <ChevronRightIcon
+      //     style={{
+      //       flexShrink: 1,
+      //       alignSelf: "center",
+      //       color: theme.custom.colors.icon,
+      //     }}
+      //   />
+      // ),
+    },
+    {
+      // icon: <LockIcon style={{ color: theme.custom.colors.icon }} />,
+      text: "Backpack.app",
+      onPress: () => Linking.openURL(BACKPACK_LINK),
+    },
+    {
+      // icon: <TwitterIcon style={{ color: theme.custom.colors.icon }} />,
+      text: "Twitter",
+      onPress: () => Linking.openURL(TWITTER_LINK),
+    },
+    {
+      // icon: <DiscordIcon fill={theme.custom.colors.icon} />,
+      text: "Need help? Hop into Discord",
+      onPress: () => Linking.openURL(DISCORD_INVITE_LINK),
+    },
+  ];
+  return (
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <View>
+        {options.map((o, idx) => (
+          <ListItem key={idx} label={o.text} onPress={o.onPress} />
+        ))}
+      </View>
+    </View>
+  );
+}
+
+function ResetWelcomeScreen({ navigation }) {
+  return <View style={{ flex: 1, backgroundColor: "orange" }} />;
+}
+
+function LockedNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Locked"
+      screenOptions={{ headerTitle: "", presentation: "modal" }}
+    >
+      <Stack.Screen name="Locked" component={LockedScreen} />
+      <Stack.Screen name="HelpMenuModal" component={LockedHelpMenuModal} />
+      <Stack.Group>
+        <Stack.Screen name="ResetWelcome" component={ResetWelcomeScreen} />
+        <Stack.Screen name="ResetWarning" component={LockedScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+}
+
+const LockedScreen = ({ navigation }) => {
   const background = useBackgroundClient();
   const {
     control,
@@ -38,7 +127,6 @@ const LockedScreen = () => {
         method: UI_RPC_METHOD_KEYRING_STORE_UNLOCK,
         params: [password],
       });
-      // navigate("/");
     } catch (err) {
       console.error(err);
       setError("password", { message: "Invalid password" });
@@ -62,6 +150,12 @@ const LockedScreen = () => {
       <ButtonFooter>
         <ResetAppButton />
         <CustomButton text="Unlock" onPress={handleSubmit(onSubmit)} />
+        <CustomButton
+          text="Open Help"
+          onPress={() => {
+            navigation.push("HelpMenuModal");
+          }}
+        />
       </ButtonFooter>
     </>
   );
@@ -88,7 +182,7 @@ function RootNavigator() {
     case "needs-onboarding":
       return <OnboardingNavigator />;
     case "locked":
-      return <LockedScreen />;
+      return <LockedNavigator />;
     case "unlocked":
       return <UnlockedNavigator />;
     default:
