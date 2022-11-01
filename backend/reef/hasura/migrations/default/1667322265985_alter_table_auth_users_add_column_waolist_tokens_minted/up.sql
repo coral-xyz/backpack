20fe8e0a7281b591a4216411f0e67b01f2d6d648
Philipp Adrian <hello@philippadrian.com>
2022-11-01 15:22:29 -0300

    Add waolist_tokens_minted and waolist_tokens_eligible fields

---
+++ backend/reef/hasura/migrations/default/1667322265985_alter_table_auth_users_add_column_waolist_tokens_minted/up.sql
@@ -0,0 +1,2 @@
+alter table "auth"."users" add column "waolist_tokens_minted" integer
+ not null default '0';
---
+++ backend/reef/hasura/migrations/default/1667322290509_alter_table_auth_users_add_column_waolist_tokens_eligible/up.sql
@@ -0,0 +1,2 @@
+alter table "auth"."users" add column "waolist_tokens_eligible" integer
+ not null default '1';
