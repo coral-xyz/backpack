4d3a9f92d58057fdf929e1ae680e58fd7c9f458e
Tom Linton <tlinton@fastmail.fm>
2022-11-11 19:33:05 +0000

    use a proxy on window.ethereum (#1419)
    * stash
    
    * cleanup
    
    * commenting
    
    * add auto refresh on network change for compatability
    
    * unfreeze, metamask impersonation
    
    * set transaction type if not set

---
+++ packages/provider-injection/src/types.ts
@@ -0,0 +1,33 @@
+type WalletProvider = {
+  on: (
+    eventName: string | symbol,
+    listener: (...args: unknown[]) => void
+  ) => unknown;
+  removeListener: (
+    eventName: string | symbol,
+    listener: (...args: unknown[]) => void
+  ) => unknown;
+  [optionalProps: string]: unknown;
+};
+
+type BackpackProvider = WalletProvider & {
+  isBackpack: true;
+};
+
+type WindowEthereum = WalletProvider & {
+  isMetaMask?: boolean;
+  isBackpack?: boolean;
+  autoRefreshOnNetworkChange?: boolean;
+};
+
+interface Window {
+  walletRouter?: {
+    currentProvider: WalletProvider;
+    providers: WalletProvider[];
+    getProviderInfo: (
+      provider: WalletProvider
+    ) => WalletProvider["providerInfo"];
+    addProvider: (newProvider: WalletProvider) => void;
+  };
+  ethereum?: WindowEthereum;
+}
