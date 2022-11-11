da0df7804c32a28438406b80757f66b28d86a7ad
Peter Piekarczyk <peter@peterp.me>
2022-11-11 10:15:49 -0600

    turbo caching from metro yay

--- packages/app-mobile/metro.config.js
+++ packages/app-mobile/metro.config.js
@@ -1,34 +1,46 @@
+// Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
 const path = require("path");
 
 const workspaceRoot = path.resolve(__dirname, "../..");
 const projectRoot = __dirname;
 
-module.exports = () => {
-  const config = getDefaultConfig(projectRoot);
+const config = getDefaultConfig(projectRoot);
 
-  config.watchFolders = [workspaceRoot];
+// Watch all files in the monorepo
+config.watchFolders = [workspaceRoot];
 
-  config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, "node_modules"),
-    path.resolve(workspaceRoot, "node_modules"),
-  ];
+// Force resolving nested modules to the folders below (which DOES NOT work for us right now)
+// config.resolver.disableHierarchicalLookup = true;
 
-  // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-  // config.resolver.disableHierarchicalLookup = true;
+// Try resolving with project modules first, then workspace modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
 
-  config.transformer = {
-    ...config.transformer,
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  };
+// Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+// config.resolver.disableHierarchicalLookup = true;
 
-  // seems like backpack/node_modules/@solana/web3.js/node_modules/superstruct/lib/index.cjs needs this
-  config.resolver.sourceExts = [...config.resolver.sourceExts, "cjs", "svg"];
-
-  return config;
+config.transformer = {
+  ...config.transformer,
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  }),
 };
+
+// seems like backpack/node_modules/@solana/web3.js/node_modules/superstruct/lib/index.cjs needs this
+config.resolver.sourceExts = [...config.resolver.sourceExts, "cjs", "svg"];
+
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({
+    root: path.join(projectRoot, "node_modules", ".cache", "metro"),
+  }),
+];
+
+module.exports = config;
