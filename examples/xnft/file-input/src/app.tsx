f392edbf85797b6bb9f9f5978db4cc773734c290
joku <jokumusic@gmail.com>
2022-11-11 18:36:38 -0800

    uncommented FileInput stuff.

--- examples/xnft/file-input/src/app.tsx
+++ examples/xnft/file-input/src/app.tsx
@@ -1,24 +1,20 @@
 import { useState } from "react";
-import ReactXnft, {
-  View,
-  Text,
-  //FileInput
-} from "react-xnft";
+import ReactXnft, { View, Text, FileInput } from "react-xnft";
 
 export function App() {
-  //const [selectedFiles, setSelectedFiles] = useState([]);
+  const [selectedFiles, setSelectedFiles] = useState([]);
   return (
     <View style={{ color: "white" }}>
       <Text>Select Files:</Text>
-      {/*
-      <FileInput onChange={(e)=> setSelectedFiles(e.target.files)} />
-  
-      {
-        selectedFiles.map(f=>(
-          <Text>{f?.name ?? ""}</Text>
-        ))
-      }
-    */}
+      <FileInput
+        onChange={(e) => setSelectedFiles(e.target.files || selectedFiles)}
+      />
+
+      <Text>Files you Selected:</Text>
+
+      {selectedFiles.map((f) => (
+        <Text>{f?.name ?? ""}</Text>
+      ))}
     </View>
   );
 }
