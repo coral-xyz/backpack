35762fdabb41d778a9ea1c7000b460446c69e31b
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-11-11 21:05:10 -0600

    expo final monorepo changes (#1469)
    * patch package updates
    
    * fixes patch
    
    * removes postinstall

--- packages/app-mobile/metro.config.js
+++ packages/app-mobile/metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { FileStore } = require("metro-cache");
 const path = require("path");
 
-const workspaceRoot = path.resolve(__dirname, "../..");
 const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
 
@@ -21,7 +21,7 @@ config.resolver.nodeModulesPaths = [
 ];
 
 // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-// config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = true;
 
 config.transformer = {
   ...config.transformer,
@@ -42,5 +42,18 @@ config.cacheStores = [
     root: path.join(projectRoot, "node_modules", ".cache", "metro"),
   }),
 ];
+
+// If your monorepo tooling can give you the list of monorepo workspaces linked
+// in your app workspace, you can automate this list instead of hardcoding them.
+const monorepoPackages = {
+  "@coral-xyz/common": path.resolve(workspaceRoot, "packages/common"),
+  "@coral-xyz/recoil": path.resolve(workspaceRoot, "packages/recoil"),
+};
+
+// Add the monorepo workspaces as `extraNodeModules` to Metro.
+// If your monorepo tooling creates workspace symlinks in the `node_modules` folder,
+// you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
+// See: https://facebook.github.io/metro/docs/configuration/#extranodemodules
+config.resolver.extraNodeModules = monorepoPackages;
 
 module.exports = config;
