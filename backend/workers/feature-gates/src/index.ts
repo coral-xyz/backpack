f77c6f52a48d01da917e06c6b2706b2c53761a09
Harkirat Singh <harkirat96@gmail.com>
2022-11-15 04:22:28 +0530

    Don't show onramp in unsupported regions

--- backend/workers/feature-gates/src/index.ts
+++ backend/workers/feature-gates/src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { FEATURE_GATES } from "./FEATURES";
+import { getStripeEnabledGate } from "./stripe";
 
 const app = new Hono();
 
@@ -8,7 +9,18 @@ app.get("/", (c) => {
 });
 
 app.get("/gates", async (c) => {
-  return c.json({ gates: FEATURE_GATES }, 200);
+  return c.json(
+    {
+      gates: {
+        ...FEATURE_GATES,
+        STRIPE_ENABLED: await getStripeEnabledGate(
+          c.req.headers.get("x-real-ip") || "127.0.0.1",
+          c.env.STRIPE_SECRET
+        ),
+      },
+    },
+    200
+  );
 });
 
 export default app;
