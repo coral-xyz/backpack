af9f2fecb63ca6703565c28b286858a570f4a7cb
Philipp Adrian <hello@philippadrian.com>
2022-11-15 16:59:38 -0300

    move app mounting into react-xnft

--- examples/xnft/native-example/src/index.tsx
+++ examples/xnft/native-example/src/index.tsx
@@ -1,12 +1,4 @@
 import App from "./App";
-import { AppRegistry } from "react-native";
+import { RegisterNativeXnft } from "react-xnft";
 
-// hide legacy react-xnft container
-document.getElementById("container")!.style.display = "none";
-document.getElementById("native-container")!.style.display = "flex";
-
-AppRegistry.registerComponent("App", () => App);
-
-AppRegistry.runApplication("App", {
-  rootTag: document.getElementById("native-container"),
-});
+RegisterNativeXnft("xNFT", () => App);
--- packages/react-xnft/src/native.ts
+++ packages/react-xnft/src/native.ts
@@ -1,6 +1,16 @@
+import { AppRegistry } from "react-native-web";
+
 export function RegisterNativeXnft(
   name: string,
   componentProvider: () => () => JSX.Element
 ) {
-  window.RegisterXnft(name, componentProvider);
+  // hide legacy react-xnft container
+  document.getElementById("container")!.style.display = "none";
+  document.getElementById("native-container")!.style.display = "flex";
+
+  AppRegistry.registerComponent(name, componentProvider);
+
+  AppRegistry.runApplication(name, {
+    rootTag: document.getElementById("native-container"),
+  });
 }
