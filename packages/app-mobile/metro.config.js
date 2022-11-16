5597c28b59de903ce661af06b77c3e9a0b6935f6
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-11-16 10:13:21 -0600

    move metro config around

--- packages/app-mobile/metro.config.js
+++ packages/app-mobile/metro.config.js
@@ -8,14 +8,15 @@ const workspaceRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
 
-// Watch all files in the monorepo
-config.watchFolders = [workspaceRoot];
+// If your monorepo tooling can give you the list of monorepo workspaces linked
+// in your app workspace, you can automate this list instead of hardcoding them.
+const monorepoPackages = {
+  "@coral-xyz/common": path.resolve(workspaceRoot, "packages/common"),
+  "@coral-xyz/recoil": path.resolve(workspaceRoot, "packages/recoil"),
+};
 
-// Try resolving with project modules first, then workspace modules
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
+// 1. Watch the local app folder, and all related packages (limiting the scope and speeding it up)
+config.watchFolders = [__dirname, ...Object.values(monorepoPackages)];
 
 config.transformer = {
   ...config.transformer,
@@ -37,18 +38,17 @@ config.cacheStores = [
   }),
 ];
 
-// If your monorepo tooling can give you the list of monorepo workspaces linked
-// in your app workspace, you can automate this list instead of hardcoding them.
-const monorepoPackages = {
-  "@coral-xyz/common": path.resolve(workspaceRoot, "packages/common"),
-  "@coral-xyz/recoil": path.resolve(workspaceRoot, "packages/recoil"),
-};
-
 // Add the monorepo workspaces as `extraNodeModules` to Metro.
 // If your monorepo tooling creates workspace symlinks in the `node_modules` folder,
 // you can either add symlink support to Metro or set the `extraNodeModules` to avoid the symlinks.
 // See: https://facebook.github.io/metro/docs/configuration/#extranodemodules
 config.resolver.extraNodeModules = monorepoPackages;
+
+// Try resolving with project modules first, then workspace modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
 
 // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
