e5a87ef5e0e3d2ee41337e8a5971f8af20b82ec8
Harkirat Singh <harkirat96@gmail.com>
2022-11-16 10:47:37 +0530

    Updated deploy notifications

--- scripts/deploy_notifications.sh
+++ scripts/deploy_notifications.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mv docker/notifications/fly.toml .
 fly deploy
-#--env VAPID_PUBLIC_KEY=$VAPID_PUBLIC_KEY --env VAPID_PRIVATE_KEY=$VAPID_PRIVATE_KEY --env AUTH_JWT=$AUTH_JWT --env HASURA_URL=$HASURA_URL
