382029fbaeec7d44e5208bc44ba1ef95239a0489
Philipp Adrian <hello@philippadrian.com>
2022-11-17 17:07:19 -0300

    rename react native watch -> dev

--- packages/xnft-cli/native/dev.js
+++ packages/xnft-cli/native/dev.js
@@ -3,10 +3,10 @@ const express = require("express");
 const esconfig = require("./esbuild.config.web");
 const getHtmlWrapper = require("./getHtmlWrapper");
 module.exports = (program) => {
-  program.command("watch").action(() => {
+  program.command("dev").action(() => {
     const app = express(); // create express app
 
-    let js;
+    let html;
     let port = 9933;
 
     require("esbuild")
@@ -15,17 +15,17 @@ module.exports = (program) => {
         watch: {
           onRebuild(error, result) {
             if (error) console.error("build error", JSON.stringify(err));
-            js = fs.readFileSync("dist/index.js", { encoding: "utf-8" });
+            html = fs.readFileSync("dist/index.html", { encoding: "utf-8" });
           },
         },
       })
       .then((result) => {
         console.log("watching...");
-        js = fs.readFileSync("dist/index.js", { encoding: "utf-8" });
+        html = fs.readFileSync("dist/index.html", { encoding: "utf-8" });
       });
 
     app.get("/", (req, res) => {
-      res.send(getHtmlWrapper(js));
+      res.send(html);
     });
 
     app.listen(port, () => {
--- packages/xnft-cli/native/index.js
+++ packages/xnft-cli/native/index.js
@@ -1,10 +1,10 @@
 const build = require("./build");
 const start = require("./start");
-const watch = require("./watch");
+const dev = require("./dev");
 
 module.exports = (program) => {
   program.description("CLI for react-native xNFTs");
-  watch(program);
+  dev(program);
   build(program);
   start(program);
 };
