56612911e03bf9adaf1b1a34b9bc3eb42b2edd4c
Harkirat Singh <harkirat96@gmail.com>
2022-11-22 10:32:53 +0530

    Added proxy

--- backend/native/backpack-api/src/routes/v1/proxy.ts
+++ backend/native/backpack-api/src/routes/v1/proxy.ts
@@ -2,9 +2,14 @@ import router from "./preferences";
 import request from "request";
 
 router.get("/*", async (req, res) => {
-  const url = req.path.slice(1);
+  const url = (req.path || "")?.slice(1);
   try {
-    request(url).pipe(res);
+    const req = request(url);
+    req.on("error", function (err) {
+      console.log(err);
+      res.status(404).json({});
+    });
+    req.pipe(res);
   } catch (e) {
     res.status(500).json({ msg: "Error while proxying request" });
   }
