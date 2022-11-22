36fc83631e3f72d95bc198eb09b1899205862221
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-11-21 20:28:12 -0600

    receive flows

--- packages/app-mobile/src/screens/Unlocked/DepositScreen.tsx
+++ packages/app-mobile/src/screens/Unlocked/DepositScreen.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import { Blockchain } from "@coral-xyz/common";
-import { Screen } from "@components";
-import { StyleSheet, FlatList, Text, View, Image } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Margin, Screen } from "@components";
+import type { Blockchain } from "@coral-xyz/common";
+import { walletAddressDisplay } from "@coral-xyz/common";
 import { useActiveWallets } from "@coral-xyz/recoil";
-import { useTheme, useBlockchainLogo } from "@hooks";
-
-// TODO(peter) copy from app-extension
-function walletAddressDisplay(publicKey: string) {
-  return publicKey;
-}
+import { MaterialIcons } from "@expo/vector-icons";
+import { useBlockchainLogo, useTheme } from "@hooks";
 
 export default function DepositModal({ navigation }) {
   const activeWallets = useActiveWallets();
@@ -22,6 +27,7 @@ export default function DepositModal({ navigation }) {
         style={{ flex: 1 }}
         data={activeWallets}
         keyExtractor={(item) => item.publicKey}
+        ItemSeparatorComponent={<View style={{ height: 12 }} />}
         renderItem={({ item }) => {
           return (
             <BlockchainDepositCard
@@ -36,6 +42,116 @@ export default function DepositModal({ navigation }) {
   );
 }
 
+function CircleButton({
+  icon,
+  onPress,
+}: {
+  icon: string;
+  onPress: () => void;
+}) {
+  const theme = useTheme();
+  return (
+    <Pressable
+      onPress={onPress}
+      style={{
+        width: 50,
+        height: 50,
+        borderRadius: 80,
+        backgroundColor: theme.custom.colors.bg2,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MaterialIcons
+        name={icon}
+        size={20}
+        color={theme.custom.colors.fontColor}
+      />
+    </Pressable>
+  );
+}
+
+function QRCodeModal({
+  onRequestClose,
+  name,
+  blockchainLogo,
+  blockchainDisplay,
+  publicKey,
+}) {
+  const theme = useTheme();
+  return (
+    <View
+      style={{
+        maxHeight: "60%",
+        marginHorizontal: 24,
+        backgroundColor: theme.custom.colors.background,
+        borderRadius: 12,
+        padding: 24,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+      }}
+    >
+      <Pressable
+        style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          top: 12,
+          left: 12,
+        }}
+        onPress={() => {
+          // TODO(peter) figure out why i can't close
+          console.log("hi");
+          onRequestClose();
+        }}
+      >
+        <MaterialIcons
+          name="close"
+          color={theme.custom.colors.fontColor}
+          size={24}
+        />
+      </Pressable>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Margin right={8}>
+          <Image
+            source={blockchainLogo}
+            style={{ width: 20, height: 20, borderRadius: 40 }}
+          />
+        </Margin>
+        <Text
+          style={{
+            fontSize: 22,
+            color: theme.custom.colors.fontColor,
+          }}
+        >
+          {blockchainDisplay}
+        </Text>
+      </View>
+      <Margin vertical={24}>
+        <View style={{ width: 200, height: 200, backgroundColor: "orange" }} />
+      </Margin>
+      <Text
+        style={{
+          textAlign: "center",
+          color: theme.custom.colors.fontColor,
+          fontSize: 16,
+        }}
+      >
+        {name}
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          color: theme.custom.colors.secondary,
+        }}
+      >
+        ({walletAddressDisplay(publicKey)})
+      </Text>
+    </View>
+  );
+}
+
 function BlockchainDepositCard({
   blockchain,
   name,
@@ -47,16 +163,20 @@ function BlockchainDepositCard({
 }) {
   const theme = useTheme();
 
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-  const [tooltipOpenModal, setTooltipOpenModal] = useState(false);
+  // const [tooltipOpen, setTooltipOpen] = useState(false);
+  // const [tooltipOpenModal, setTooltipOpenModal] = useState(false);
   const [showQrCode, setShowQrCode] = useState(false);
 
   const blockchainLogo = useBlockchainLogo(blockchain);
   const blockchainDisplay =
     blockchain.slice(0, 1).toUpperCase() + blockchain.slice(1);
 
-  const onQrCode = () => {
+  const onPressQrCode = () => {
     setShowQrCode(true);
+  };
+
+  const onPressCopy = () => {
+    console.log("copy");
   };
 
   return (
@@ -70,24 +190,58 @@ function BlockchainDepositCard({
         },
       ]}
     >
-      <Text style={{ fontWeight: "500", color: theme.custom.colors.fontColor }}>
-        Your {blockchainDisplay} address
-      </Text>
-      <Image
-        source={blockchainLogo}
-        style={{ width: 14, borderRadius: 2, aspectRatio: 1 }}
-      />
-      <Text
-        style={{
-          fontWeight: "500",
-          fontSize: 14,
-          marginTop: 6,
-          marginBottom: 6,
-          color: theme.custom.colors.secondary,
+      <View>
+        <Text
+          style={{ fontWeight: "500", color: theme.custom.colors.fontColor }}
+        >
+          Your {blockchainDisplay} address
+        </Text>
+        <Text
+          style={{
+            fontWeight: "500",
+            fontSize: 14,
+            marginTop: 6,
+            marginBottom: 6,
+            color: theme.custom.colors.secondary,
+          }}
+        >
+          {`${name} (${walletAddressDisplay(publicKey)})`}
+        </Text>
+        <Image
+          source={blockchainLogo}
+          style={{ width: 14, borderRadius: 2, aspectRatio: 1 }}
+        />
+      </View>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Margin right={8}>
+          <CircleButton icon="qr-code" onPress={onPressQrCode} />
+        </Margin>
+        <CircleButton icon="content-copy" onPress={onPressCopy} />
+      </View>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={showQrCode}
+        onRequestClose={() => {
+          setShowQrCode(false);
         }}
       >
-        {`${name} (${walletAddressDisplay(publicKey)})`}
-      </Text>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "rgba(0, 0, 0, 0.4)",
+            justifyContent: "center",
+          }}
+        >
+          <QRCodeModal
+            blockchainLogo={blockchainLogo}
+            blockchainDisplay={blockchainDisplay}
+            name={name}
+            publicKey={publicKey}
+            onRequestClose={() => setShowQrCode(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -95,6 +249,7 @@ function BlockchainDepositCard({
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    justifyContent: "space-between",
     padding: 16,
   },
 });
