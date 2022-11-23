c62e0135e5fbe3bbadb6d5baec13f8ceee9741fc
Philipp Adrian <hello@philippadrian.com>
2022-11-22 23:14:11 -0300

    Add twitter_user table

---
+++ backend/reef/hasura/migrations/default/1669168636344_alter_table_auth_twitter_users_add_column_pfp_staked_since/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "auth"."twitter_users" add column "pfp_staked_since" timestamptz
+--  null;
