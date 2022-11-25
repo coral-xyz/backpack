3562de8bd31e1c9d0f7439f16a8301f1b25f58a8
Harkirat Singh <harkirat96@gmail.com>
2022-11-25 18:17:06 +0530

    Auth to video

--- backend/native/backend-ws/src/index.ts
+++ backend/native/backend-ws/src/index.ts
@@ -1,14 +1,16 @@
 import { WebSocketServer } from "ws";
 import { PORT } from "./config";
 import { UserManager } from "./users/UserManager";
+import { extractUserId } from "./auth";
 const wss = new WebSocketServer({ port: PORT });
 
-wss.on("connection", (ws, req) => {
-  const cookies = req.headers.cookie;
+wss.on("connection", async (ws, req) => {
+  const cookie = req.headers.cookie;
+  const userId = await extractUserId(cookie || "");
 
   UserManager.getInstance().addUser(
     ws,
     Math.floor(Math.random() * 10000000).toString(),
-    "123"
+    userId || ""
   );
 });
