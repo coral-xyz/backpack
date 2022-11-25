965fd5116cba7013b3601cf09a5e17f395a4bac5
Harkirat Singh <harkirat96@gmail.com>
2022-11-25 16:38:35 +0530

    Updated cookies

--- backend/native/backend-ws/src/index.ts
+++ backend/native/backend-ws/src/index.ts
@@ -1,15 +1,14 @@
 import { WebSocketServer } from "ws";
-import url from "url";
 import { PORT } from "./config";
 import { UserManager } from "./users/UserManager";
 const wss = new WebSocketServer({ port: PORT });
 
 wss.on("connection", (ws, req) => {
-  const url_parts = url.parse(req.url || "", true);
-  const query = url_parts.query;
+  const cookies = req.headers.cookie;
+
   UserManager.getInstance().addUser(
     ws,
     Math.floor(Math.random() * 10000000).toString(),
-    query.userId || ""
+    "123"
   );
 });
