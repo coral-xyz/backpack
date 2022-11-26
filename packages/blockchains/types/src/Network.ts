311f430cf391079bcaf2a0595786bdb8218aee72
Tom Linton <tlinton@fastmail.fm>
2022-11-27 10:28:12 +1300

    stash

--- packages/blockchains/types/src/Network.ts
+++ packages/blockchains/types/src/Network.ts
@@ -2,10 +2,4 @@ export interface Network {
   name: string;
   // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
   coinType: string;
-  isTestnet: boolean;
-  chainId?: string | number;
-  rpcUrl?: string;
-  scraperUrl?: string;
-  explorerUrl?: string;
-  helperUrl?: string;
 }
