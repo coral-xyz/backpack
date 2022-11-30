1f12a49c81d6a6a6a78dadc0b8233e58d25b68c3
Tom Linton <tlinton@fastmail.fm>
2022-11-30 17:19:04 +1300

    rpc proxy cors headers

--- backend/workers/rpc-proxy/src/index.ts
+++ backend/workers/rpc-proxy/src/index.ts
@@ -5,6 +5,16 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    };
+    if (request.method === "OPTIONS") {
+      return new Response("ok", {
+        headers: corsHeaders,
+      });
+    }
     if (request.method === "POST") {
       return await fetch(
         env.RPC_URL || "https://api.mainnet-beta.solana.com",
