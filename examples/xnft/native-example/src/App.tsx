abd85ad19e288ce1b3e9c977626aec555375630e
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-11-30 10:02:53 -0600

    resets native example

--- examples/xnft/native-example/src/App.tsx
+++ examples/xnft/native-example/src/App.tsx
@@ -1,77 +1,33 @@
-import { RecoilRoot } from "recoil";
-import { ActivityIndicator, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import React from "react";
+import { Button, View } from "react-native";
+import { atom, RecoilRoot, useRecoilState } from "recoil";
+import tw from "twrnc";
 
-import { ExamplesScreens } from "./screens/ExamplesScreen";
-import { HomeScreen } from "./screens/HomeScreen";
-import { TokenListNavigator } from "./screens/TokenNavigator";
+const testAtom = atom<"native" | "bright">({
+  key: "testAtom",
+  default: "native",
+});
 
-const Tab = createBottomTabNavigator();
-
-function TabNavigator() {
+function Root() {
+  const [future, setFuture] = useRecoilState(testAtom);
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: "#e91e63",
-      }}
+    <View
+      style={tw`h-full ${
+        future === "bright" ? "bg-yellow-100" : "bg-blue-100"
+      } flex justify-center items-center`}
     >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
+      <Button
+        title={`The Future is ${future}`}
+        color={future === "bright" ? "rgb(228, 208, 10)" : "rgb(33, 150, 243)"}
+        onPress={() => setFuture(future === "bright" ? "native" : "bright")}
       />
-      <Tab.Screen
-        name="TokenList"
-        component={TokenListNavigator}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Tokens",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bank" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Examples"
-        component={ExamplesScreens}
-        options={{
-          tabBarLabel: "Examples",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    </View>
   );
 }
-
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Inter_900Black,
-  });
-
-  if (!fontsLoaded) {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ActivityIndicator />
-      </View>
-    );
-  }
-
   return (
     <RecoilRoot>
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
+      <Root />
     </RecoilRoot>
   );
 }
