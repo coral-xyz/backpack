f67789d0dafaa5fc358e6ba56892984b97312618
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-11-30 10:03:18 -0600

    resets native example (#1635)

---
+++ examples/xnft/native-example/src/index.tsx
@@ -0,0 +1,8 @@
+import { AppRegistry } from "react-native";
+import { RunNativeXnft } from "react-xnft";
+
+import App from "./App";
+
+AppRegistry.registerComponent("xNFT", () => App);
+
+RunNativeXnft(AppRegistry);
