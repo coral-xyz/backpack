53c9ab9e11fa9eb313088f7d373816be70cfd7aa
Tom Linton <tlinton@fastmail.fm>
2022-12-01 15:46:16 +1300

    fix imports

--- backend/native/backpack-api/src/index.ts
+++ backend/native/backpack-api/src/index.ts
@@ -1,15 +1,16 @@
-import express, { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
+import express from "express";
 import { ZodError } from "zod";
-import { zodErrorToString } from "./util";
+
 import authenticateRouter from "./routes/v1/authenticate";
+import chatRouter from "./routes/v1/chats";
+import friendsRouter from "./routes/v1/friends";
+import inboxRouter from "./routes/v1/inbox";
 import notificationRoutes from "./routes/v1/notifications";
 import preferenceRoutes from "./routes/v1/preferences";
 import proxyRouter from "./routes/v1/proxy";
-import userRouter from "./routes/v1/uers";
-import chatRouter from "./routes/v1/chats";
-import inboxRouter from "./routes/v1/inbox";
-import friendsRouter from "./routes/v1/friends";
 import usersRouter from "./routes/v1/users";
+import { zodErrorToString } from "./util";
 
 const app = express();
 const bodyParser = require("body-parser");
