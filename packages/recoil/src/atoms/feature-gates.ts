d2b17d68aed2fed26910198ce9a02c224c748112
Kirat <harkirat96@gmail.com>
2022-12-01 23:29:17 +0530

    atom -> selector (#1646)

--- packages/recoil/src/atoms/feature-gates.ts
+++ packages/recoil/src/atoms/feature-gates.ts
@@ -6,18 +6,15 @@ import {
   buildFullFeatureGatesMap,
 } from "@coral-xyz/common";
 
-export const featureGates = atom<FEATURE_GATES_MAP>({
+export const featureGates = selector<FEATURE_GATES_MAP>({
   key: "featureGates",
-  default: selector({
-    key: "featureGatesDefault",
-    get: async ({ get }) => {
-      const background = get(backgroundClient);
-      const response = await background.request({
-        method: UI_RPC_METHOD_GET_FEATURE_GATES,
-        params: [],
-      });
-      const gates = buildFullFeatureGatesMap(response);
-      return gates;
-    },
-  }),
+  get: async ({ get }) => {
+    const background = get(backgroundClient);
+    const response = await background.request({
+      method: UI_RPC_METHOD_GET_FEATURE_GATES,
+      params: [],
+    });
+    const gates = buildFullFeatureGatesMap(response);
+    return gates;
+  },
 });
