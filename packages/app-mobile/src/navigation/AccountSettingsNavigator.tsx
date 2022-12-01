d07675e0c81348a100dd10fe61d71909e14ea5c8
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-12-01 13:41:05 -0600

    solana / ethereum preferences flow hookup (#1649)

--- packages/app-mobile/src/navigation/AccountSettingsNavigator.tsx
+++ packages/app-mobile/src/navigation/AccountSettingsNavigator.tsx
@@ -1,23 +1,30 @@
-import { Alert, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
 import { Screen } from "@components";
 import type { Blockchain, ChannelAppUiClient } from "@coral-xyz/common";
 import {
   DerivationPath,
   EthereumConnectionUrl,
   SolanaCluster,
+  SolanaExplorer,
   UI_RPC_METHOD_BLOCKCHAIN_KEYRINGS_ADD,
   UI_RPC_METHOD_BLOCKCHAIN_KEYRINGS_READ,
   UI_RPC_METHOD_BLOCKCHAINS_ENABLED_ADD,
   UI_RPC_METHOD_BLOCKCHAINS_ENABLED_DELETE,
   UI_RPC_METHOD_ETHEREUM_CHAIN_ID_UPDATE,
   UI_RPC_METHOD_ETHEREUM_CONNECTION_URL_UPDATE,
+  UI_RPC_METHOD_SOLANA_COMMITMENT_UPDATE,
+  UI_RPC_METHOD_SOLANA_CONNECTION_URL_UPDATE,
+  UI_RPC_METHOD_SOLANA_EXPLORER_UPDATE,
 } from "@coral-xyz/common";
 import {
   useBackgroundClient,
   useEnabledBlockchains,
   useEthereumConnectionUrl,
   useKeyringType,
+  useSolanaCommitment,
   useSolanaConnectionUrl,
+  useSolanaExplorer,
 } from "@coral-xyz/recoil";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -30,6 +37,7 @@ import {
 } from "@screens/Unlocked/Settings/components/SettingsRow";
 import { PreferencesScreen } from "@screens/Unlocked/Settings/PreferencesScreen";
 import { PreferencesTrustedSitesScreen } from "@screens/Unlocked/Settings/PreferencesTrustedSitesScreen";
+import type { Commitment } from "@solana/web3.js";
 import { ethers } from "ethers";
 const { hexlify } = ethers.utils;
 
@@ -73,6 +81,11 @@ export default function AccountSettingsNavigator() {
       />
       <Stack.Screen
         options={{ title: "Preferences" }}
+        name="PreferencesEthereumCustomRpcUrl"
+        component={PreferencesEthereumCustomRpcUrl}
+      />
+      <Stack.Screen
+        options={{ title: "Preferences" }}
         name="PreferencesSolana"
         component={PreferencesSolana}
       />
@@ -80,6 +93,21 @@ export default function AccountSettingsNavigator() {
         options={{ title: "Preferences" }}
         name="PreferencesSolanaConnection"
         component={PreferencesSolanaConnection}
+      />
+      <Stack.Screen
+        options={{ title: "Preferences" }}
+        name="PreferencesSolanaCommitment"
+        component={PreferencesSolanaCommitment}
+      />
+      <Stack.Screen
+        options={{ title: "Preferences" }}
+        name="PreferencesSolanaExplorer"
+        component={PreferencesSolanaExplorer}
+      />
+      <Stack.Screen
+        options={{ title: "Preferences" }}
+        name="PreferencesSolanaCustomRpcUrl"
+        component={PreferencesSolanaCustomRpcUrl}
       />
       <Stack.Screen
         options={{ title: "Trusted Sites" }}
@@ -100,25 +128,98 @@ export default function AccountSettingsNavigator() {
   );
 }
 
+function PreferencesSolanaCustomRpcUrl({ navigation }) {
+  const background = useBackgroundClient();
+  const [rpcUrl, setRpcUrl] = useState("");
+
+  const [rpcUrlError, setRpcUrlError] = useState(false);
+
+  const changeNetwork = () => {
+    try {
+      background
+        .request({
+          method: UI_RPC_METHOD_SOLANA_CONNECTION_URL_UPDATE,
+          params: [rpcUrl],
+        })
+        .then(close)
+        .catch(console.error);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!rpcUrl) {
+      setRpcUrlError(false);
+      return;
+    }
+    try {
+      new URL(rpcUrl.trim());
+      setRpcUrlError(false);
+    } catch (e: any) {
+      setRpcUrlError(true);
+    }
+  }, [rpcUrl]);
+
+  // return (
+  //   <div style={{ paddingTop: "16px", height: "100%" }}>
+  //     <form
+  //       onSubmit={changeNetwork}
+  //       style={{ display: "flex", height: "100%", flexDirection: "column" }}
+  //     >
+  //       <div style={{ flex: 1, flexGrow: 1 }}>
+  //         <Inputs error={rpcUrlError}>
+  //           <InputListItem
+  //             isFirst={true}
+  //             isLast={true}
+  //             button={false}
+  //             title={"RPC"}
+  //             placeholder={"RPC URL"}
+  //             value={rpcUrl}
+  //             onChange={(e) => {
+  //               setRpcUrl(e.target.value);
+  //             }}
+  //           />
+  //         </Inputs>
+  //       </div>
+  //       <div style={{ padding: 16 }}>
+  //         <PrimaryButton
+  //           disabled={!rpcUrl || rpcUrlError}
+  //           label="Switch"
+  //           type="submit"
+  //         />
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
+
+  return (
+    <View style={{ padding: 16 }}>
+      <Text>TODO form lists</Text>
+      <Text>{JSON.stringify({ rpcUrl, rpcUrlError })}</Text>
+    </View>
+  );
+}
+
 function PreferencesSolanaConnection({ navigation }) {
   const background = useBackgroundClient();
   const currentUrl = useSolanaConnectionUrl();
   const menuItems = {
     "Mainnet (Beta)": {
       onPress: () => changeNetwork(SolanaCluster.MAINNET),
-      detail: currentUrl === SolanaCluster.MAINNET ? <Checkmark /> : <></>,
+      detail: currentUrl === SolanaCluster.MAINNET ? <IconCheckmark /> : <></>,
     },
     Devnet: {
       onPress: () => changeNetwork(SolanaCluster.DEVNET),
-      detail: currentUrl === SolanaCluster.DEVNET ? <Checkmark /> : <></>,
+      detail: currentUrl === SolanaCluster.DEVNET ? <IconCheckmark /> : <></>,
     },
     Localnet: {
       onPress: () => changeNetwork(SolanaCluster.LOCALNET),
-      detail: currentUrl === SolanaCluster.LOCALNET ? <Checkmark /> : <></>,
+      detail: currentUrl === SolanaCluster.LOCALNET ? <IconCheckmark /> : <></>,
     },
     Custom: {
       onPress: () => {
-        navigation.push("preferences-solana-edit-rpc-connection");
+        navigation.push("PreferencesSolanaCustomRpcUrl");
       },
       detail:
         currentUrl !== SolanaCluster.MAINNET &&
@@ -151,25 +252,97 @@ function PreferencesSolanaConnection({ navigation }) {
   return <SettingsList menuItems={menuItems} />;
 }
 
+export function PreferencesSolanaCommitment({ navigation }) {
+  const commitment = useSolanaCommitment();
+  const background = useBackgroundClient();
+
+  const menuItems = {
+    Processed: {
+      onPress: () => changeCommitment("processed"),
+      detail: commitment === "processed" ? <IconCheckmark /> : <></>,
+    },
+    Confirmed: {
+      onPress: () => changeCommitment("confirmed"),
+      detail: commitment === "confirmed" ? <IconCheckmark /> : <></>,
+    },
+    Finalized: {
+      onPress: () => changeCommitment("finalized"),
+      detail: commitment === "finalized" ? <IconCheckmark /> : <></>,
+    },
+  };
+
+  const changeCommitment = (commitment: Commitment) => {
+    background
+      .request({
+        method: UI_RPC_METHOD_SOLANA_COMMITMENT_UPDATE,
+        params: [commitment],
+      })
+      .catch(console.error);
+  };
+
+  return <SettingsList menuItems={menuItems} />;
+}
+
+export function PreferencesSolanaExplorer({ navigation }) {
+  const background = useBackgroundClient();
+  const explorer = useSolanaExplorer();
+
+  const menuItems = {
+    "Solana Beach": {
+      onPress: () => changeExplorer(SolanaExplorer.SOLANA_BEACH),
+      detail:
+        explorer === SolanaExplorer.SOLANA_BEACH ? <IconCheckmark /> : <></>,
+    },
+    "Solana Explorer": {
+      onPress: () => changeExplorer(SolanaExplorer.SOLANA_EXPLORER),
+      detail:
+        explorer === SolanaExplorer.SOLANA_EXPLORER ? <IconCheckmark /> : <></>,
+    },
+    "Solana FM": {
+      onPress: () => changeExplorer(SolanaExplorer.SOLANA_FM),
+      detail: explorer === SolanaExplorer.SOLANA_FM ? <IconCheckmark /> : <></>,
+    },
+    Solscan: {
+      onPress: () => changeExplorer(SolanaExplorer.SOLSCAN),
+      detail: explorer === SolanaExplorer.SOLSCAN ? <IconCheckmark /> : <></>,
+    },
+  };
+
+  const changeExplorer = (explorer: string) => {
+    try {
+      background
+        .request({
+          method: UI_RPC_METHOD_SOLANA_EXPLORER_UPDATE,
+          params: [explorer],
+        })
+        .catch(console.error);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return <SettingsList menuItems={menuItems} />;
+}
+
 function PreferencesSolana({ route, navigation }) {
   const { blockchain } = route.params;
 
   const menuItems = {
     "RPC Connection": {
-      onPress: () => navigation.push("preferences-solana-rpc-connection"),
+      onPress: () => navigation.push("PreferencesSolanaConnection"),
     },
     "Confirmation Commitment": {
-      onPress: () => navigation.push("preferences-solana-commitment"),
+      onPress: () => navigation.push("PreferencesSolanaCommitment"),
     },
     Explorer: {
-      onPress: () => navigation.push("preferences-solana-explorer"),
+      onPress: () => navigation.push("PreferencesSolanaExplorer"),
     },
   };
 
   return (
     <Screen>
       <PreferencesBlockchain blockchain={blockchain} />
-      <SettingsList menuItems={menuItems} />;
+      <SettingsList menuItems={menuItems} />
     </Screen>
   );
 }
@@ -195,6 +368,79 @@ export const changeNetwork = async (
     params: [chainId],
   });
 };
+
+function PreferencesEthereumCustomRpcUrl({ navigation }) {
+  const background = useBackgroundClient();
+  const [rpcUrl, setRpcUrl] = useState("");
+  const [chainId, setChainId] = useState("");
+  const [rpcUrlError, setRpcUrlError] = useState(false);
+
+  useEffect(() => {
+    if (!rpcUrl) {
+      setRpcUrlError(false);
+      return;
+    }
+    try {
+      new URL(rpcUrl.trim());
+      setRpcUrlError(false);
+    } catch (e: any) {
+      setRpcUrlError(true);
+    }
+  }, [rpcUrl]);
+
+  async function onSubmit() {
+    await changeNetwork(background, rpcUrl, chainId);
+  }
+
+  // <div style={{ paddingTop: "16px", height: "100%" }}>
+  //   <form
+  //     onSubmit={async () => {
+  //         await changeNetwork(background, rpcUrl, chainId);
+  //       close();
+  //     }}
+  //     style={{ display: "flex", height: "100%", flexDirection: "column" }}
+  //   >
+  //     <div style={{ flex: 1, flexGrow: 1 }}>
+  //       <Inputs error={rpcUrlError}>
+  //         <InputListItem
+  //           isLast={false}
+  //           isFirst={true}
+  //           button={false}
+  //           title={"RPC"}
+  //           placeholder={"RPC URL"}
+  //           value={rpcUrl}
+  //           onChange={(e) => {
+  //             setRpcUrl(e.target.value);
+  //           }}
+  //         />
+  //         <InputListItem
+  //           isLast={true}
+  //           isFirst={false}
+  //           button={false}
+  //           title={"Chain"}
+  //           placeholder={"Chain ID"}
+  //           value={chainId}
+  //           onChange={(e) => setChainId(e.target.value)}
+  //         />
+  //       </Inputs>
+  //     </div>
+  //     <div style={{ padding: 16 }}>
+  //       <PrimaryButton
+  //         disabled={!rpcUrl || rpcUrlError}
+  //         label="Switch"
+  //         type="submit"
+  //       />
+  //     </div>
+  //   </form>
+  // </div>
+
+  return (
+    <View style={{ padding: 16 }}>
+      <Text>TODO form lists</Text>
+      <Text>{JSON.stringify({ rpcUrl, chainId, rpcUrlError })}</Text>
+    </View>
+  );
+}
 
 function PreferencesEthereumConnection({ navigation }) {
   const background = useBackgroundClient();
@@ -234,8 +480,7 @@ function PreferencesEthereumConnection({ navigation }) {
         ),
     },
     Custom: {
-      onPress: () =>
-        navigation.push("preferences-ethereum-edit-rpc-connection"),
+      onPress: () => navigation.push("PreferencesEthereumCustomRpcUrl"),
       detail:
         currentUrl !== EthereumConnectionUrl.MAINNET &&
         currentUrl !== EthereumConnectionUrl.GOERLI &&
