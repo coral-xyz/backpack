3ada455ff758c0c43572ec9f6f3fdc7031c56a2c
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-12-01 20:18:39 -0600

    balances updates

--- packages/app-mobile/src/screens/Unlocked/BalancesScreen.tsx
+++ packages/app-mobile/src/screens/Unlocked/BalancesScreen.tsx
@@ -8,19 +8,54 @@ import {
   toTitleCase,
   // walletAddressDisplay,
 } from "@coral-xyz/common";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import { TokenTables } from "./components/Balances";
 import { BalanceSummaryWidget } from "./components/BalanceSummaryWidget";
 import type { Token } from "./components/index";
 
-export default function BalancesScreen({ navigation }) {
+const Stack = createStackNavigator();
+
+export function BalancesNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="BalanceList"
+      screenOptions={{ presentation: "modal" }}
+    >
+      <Stack.Group screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="BalanceList" component={BalanceListScreen} />
+      </Stack.Group>
+      <Stack.Screen
+        name="BalanceDetail"
+        component={BalanceDetailScreen}
+        options={({ route: { params } }) => {
+          const title = `${toTitleCase(params.blockchain)} / ${
+            params.token.ticker
+          }`;
+          return {
+            title,
+          };
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function BalanceDetailScreen({ route, navigation }) {
+  const { blockchain, token } = route.params;
+
+  const onNavigate = (route) => navigation.navigate(route);
+
+  return (
+    <Screen>
+      <TransferWidget rampEnabled={false} onNavigate={onNavigate} />
+    </Screen>
+  );
+}
+
+function BalanceListScreen({ navigation }) {
   const onPressTokenRow = (blockchain: Blockchain, token: Token) => {
-    navigation.push("BlockchainTickerScreenTODO", {
-      title: `${toTitleCase(blockchain)} / ${token.ticker}`,
-      blockchain,
-      tokenAddress: token.address,
-      // componentId: NAV_COMPONENT_TOKEN,
-    });
+    navigation.push("BalanceDetail", { token, blockchain });
   };
 
   const onNavigate = (route) => navigation.navigate(route);
