bb518d0e321a742acae969c444632c658651574d
Philipp Adrian <hello@philippadrian.com>
2022-12-06 16:50:57 -0500

    Setup swr-data cache proxy for all workers and rpc-proxy

--- backend/workers/data/src/index.ts
+++ backend/workers/data/src/index.ts
@@ -21,11 +21,10 @@ app.get("/metaplex-nft/:mintAddress/image", async (c) => {
       TOKEN_METADATA_PROGRAM_ID
     )[0];
 
-    const metadataAccount = await (
-      await c.env.rpc.fetch(
-        new Request("https://solana-rpc.xnfts.dev", {
-          method: "POST",
-          body: `{
+    const metadataAccountResponse = await c.env.rpc.fetch(
+      new Request("https://solana-rpc.xnfts.dev/rpc-proxy", {
+        method: "POST",
+        body: `{
 				"jsonrpc": "2.0",
 				"id": 1,
 				"method": "getAccountInfo",
@@ -36,9 +35,9 @@ app.get("/metaplex-nft/:mintAddress/image", async (c) => {
 					}
 				]
 			}`,
-        })
-      )
-    ).json();
+      })
+    );
+    const metadataAccount = await metadataAccountResponse.json();
 
     const data = metadataAccount?.result?.value?.data?.[0];
     if (!metadataAccount || !data) {
@@ -62,11 +61,13 @@ app.get("/metaplex-nft/:mintAddress/image", async (c) => {
       return c.status(404);
     }
 
-    c.res.headers.set(
+    const imageResponse = await fetch(externalResourceUri(imageUrl));
+    const response = new Response(imageResponse.body);
+    response.headers.set(
       "Cache-Control",
       `max-age=${60 * 5}, s-maxage=${60 * 5}, stale-while-revalidate=${60 * 5}`
     );
-    return fetch(externalResourceUri(imageUrl));
+    return response;
   } catch (e) {
     console.log("Error", e);
     return c.status(500);
