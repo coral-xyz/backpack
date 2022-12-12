6ace398fe261feb8f0938dff6bf74481201e26af
wentokay <wentokay@users.noreply.github.com>
2022-12-12 21:28:43 +0000

    ensure options.html gets built

--- packages/app-extension/src/options/index.tsx
+++ packages/app-extension/src/options/index.tsx
@@ -1,17 +1,16 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 
+import Options from "./Options";
+
 const LedgerIframe = lazy(() => import("../components/LedgerIframe"));
-const Options = lazy(() => import("./Options"));
 
 //
 // Render the UI.
 //
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={null}>
-      <Options />
-    </Suspense>
+    <Options />
     <Suspense fallback={null}>
       <LedgerIframe />
     </Suspense>
