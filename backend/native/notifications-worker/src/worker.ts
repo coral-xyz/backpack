5851c72998237d59f9262c3472d6b36550ce8946
Kirat <harkirat96@gmail.com>
2022-12-14 16:47:47 +0530

    Adds pipeline to process and send message notifications (#1843)
    * Added redis queue
    
    * Updated schema
    
    * lint
    
    * Updated notifications worker
    
    * Updated notification worker
    
    * update dockerfile link
    
    * Added deps
    
    * Updated worker
    
    * update typo
    
    * added workflwo
    
    * updated conf
    
    * Added temp vapid details
    
    * Added timeout

---
+++ backend/native/notifications-worker/src/worker.ts
@@ -0,0 +1,23 @@
+import { processMessage } from "./processors";
+import { Redis } from "./Redis";
+
+export const processQueue = async () => {
+  const response = await Redis.getInstance().fetch();
+  await processResponse(response);
+  return processQueue();
+};
+
+const processResponse = async (response: string) => {
+  try {
+    const parsedResponse = JSON.parse(response);
+    const { type, payload } = parsedResponse;
+    switch (type) {
+      case "message":
+        await processMessage(payload);
+        break;
+    }
+  } catch (e) {
+    console.log(`ERROR: while processing queue`);
+    console.log(e);
+  }
+};
