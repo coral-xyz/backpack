c08d923721f31a8aff5a0a202677f7d10074c4e9
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-12-20 11:02:24 -0600

    cleans up git rev-parse

--- packages/app-mobile/app.config.js
+++ packages/app-mobile/app.config.js
@@ -1,23 +1,27 @@
 const { execSync } = require("child_process");
 
-function getLastCommitHash() {
-  const output = execSync("git rev-parse HEAD").toString();
-  return output.substring(0, 7);
-}
-
 const projectID = "55bf074d-0473-4e61-9d9d-ecf570704635";
 const packageName = "peterpme.coral.backpack";
 
 const localhostUrl = "http://localhost:9333";
-const baseUrl = "https://coral-xyz.github.io/backpack/background-scripts/";
-const commitHash = getLastCommitHash();
-const webWorkerUrl = `${baseUrl}/${commitHash}/service-worker-loader.html`;
 
 const Config = {
   webWorkerUrl: localhostUrl,
 };
 
+function getLastCommitHash() {
+  try {
+    const output = execSync("git rev-parse HEAD").toString();
+    return output.substring(0, 7);
+  } catch (error) {
+    return process.env.EAS_BUILD_GIT_COMMIT_HASH.substring(0, 7);
+  }
+}
+
 if (process.env.APP_ENV === "production") {
+  const baseUrl = "https://coral-xyz.github.io/backpack/background-scripts/";
+  const commitHash = getLastCommitHash();
+  const webWorkerUrl = `${baseUrl}/${commitHash}/service-worker-loader.html`;
   Config.webWorkerUrl = webWorkerUrl;
 }
 
