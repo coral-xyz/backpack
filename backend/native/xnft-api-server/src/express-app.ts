adf4421760ee7a305f72b0fd14b600bf9fddfbc4
Kirat <harkirat96@gmail.com>
2023-01-03 15:52:18 +0530

    Fixed issues in secret generation (#1986)

--- backend/native/xnft-api-server/src/express-app.ts
+++ backend/native/xnft-api-server/src/express-app.ts
@@ -2,8 +2,11 @@ import express from "express";
 export const app = express();
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import v1Routes from "./v1/index";
 
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "application/*+json" }));
