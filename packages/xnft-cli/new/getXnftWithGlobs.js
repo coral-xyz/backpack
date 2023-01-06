a5f09b8b4369b68a63cd175e77c4da3a3492fb21
Philipp Adrian <hello@philippadrian.com>
2023-01-06 19:06:17 +0100

    Update xnft.json schema and verify

--- packages/xnft-cli/new/getXnftWithGlobs.js
+++ packages/xnft-cli/new/getXnftWithGlobs.js
@@ -2,13 +2,41 @@ const { promises, statSync } = require("fs");
 const path = require("path");
 const glob = require("fast-glob");
 const { NOTFOUND } = require("dns");
+const { object, string, array, record, any, union, literal } = require("zod");
 const { readFile, writeFile, mkdir } = promises;
+
+const xnftJson = object({
+  name: string(),
+  description: string(),
+  icon: record(
+    string().refine((key) => ["sm", "md", "lg"].includes(key), {
+      message: "Available sizes: sm, md, lg",
+    }),
+    string()
+  ),
+  screenshots: array(string()),
+  entrypoints: record(
+    record(
+      string().refine((key) => ["web", "android", "ios"].includes(key), {
+        message: "Available platforms: web, android, ios",
+      }),
+      string()
+    )
+  ).refine((entrypoints) => !!entrypoints.default, {
+    message: "Must provide a 'default' entrypoint.",
+  }),
+  props: any(),
+});
 
 module.exports = async (xnftPath) => {
   const xnftBuffer = await readFile(xnftPath);
-  const xnft = JSON.parse(await xnftBuffer.toString());
+  const xnft = await xnftJson.parse(JSON.parse(await xnftBuffer.toString()));
 
-  const include = [path.basename(xnftPath), xnft.icon, ...xnft.screenshots];
+  const include = [
+    path.basename(xnftPath),
+    ...Object.values(xnft.icon),
+    ...xnft.screenshots,
+  ];
 
   Object.values(xnft.entrypoints).forEach((entrypoint) => {
     Object.values(entrypoint).forEach((filePath) => {
@@ -22,10 +50,12 @@ module.exports = async (xnftPath) => {
     });
   });
 
-  console.log(include);
+  const unique = [...new Set(include)];
+
+  console.log(unique);
 
   return {
     ...xnft,
-    globs: include,
+    globs: unique,
   };
 };
