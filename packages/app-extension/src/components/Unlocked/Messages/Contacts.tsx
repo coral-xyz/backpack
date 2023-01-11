0dbd9acdf858cf18df3d460b1b1a2bbeac11b3b1
Harkirat Singh <harkirat96@gmail.com>
2023-01-11 20:26:11 +0530

    more message stuff

--- packages/app-extension/src/components/Unlocked/Messages/Contacts.tsx
+++ packages/app-extension/src/components/Unlocked/Messages/Contacts.tsx
@@ -5,11 +5,6 @@ import { useNavStack } from "../../common/Layout/NavStack";
 import { SearchUsers } from "./SearchUsers";
 
 export const Contacts = () => {
-  const nav = useNavStack();
-  useEffect(() => {
-    nav.setTitle("Contacts");
-  }, []);
-
   return (
     <div>
       <SearchUsers />
