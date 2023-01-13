567ea8d71f2d25b3f24dbbe3ca7ee39a7efd2d8c
Harkirat Singh <harkirat96@gmail.com>
2023-01-13 19:05:00 +0530

    updates

---
+++ backend/chat-reef/hasura/migrations/default/1673616688406_create_table_public_chat_media_messages/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."chat_media_messages" ("id" serial NOT NULL, "media_kind" text NOT NULL, "media_link" text NOT NULL, "message_client_generated_uuid" text NOT NULL, PRIMARY KEY ("id") );
