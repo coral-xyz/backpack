2fc7611fa8dad03f7b590f0d8bbdd9569c958b2d
Tom Linton <tlinton@fastmail.fm>
2023-01-15 21:55:41 +1300

    display error if trying to onboard with an already onboarded wallet

---
+++ packages/app-extension/src/components/Onboarding/pages/AlreadyOnboarded.tsx
@@ -0,0 +1,16 @@
+import { EmptyState } from "@coral-xyz/react-common";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+
+export const AlreadyOnboarded = () => {
+  return (
+    <EmptyState
+      icon={(props: any) => <ErrorOutlineIcon {...props} />}
+      title={"Already setup"}
+      subtitle={
+        "Your Backpack has already been setup. If you want to set it up again, reset it first."
+      }
+      buttonText={"Close"}
+      onClick={window.close}
+    />
+  );
+};
