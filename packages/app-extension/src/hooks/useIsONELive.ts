50dce08acf69e2955fb0cb4df7514e4901a6603d
Philipp Adrian <hello@philippadrian.com>
2023-01-16 22:57:09 +0100

    Update useIsONELive.ts

--- packages/app-extension/src/hooks/useIsONELive.ts
+++ packages/app-extension/src/hooks/useIsONELive.ts
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import { useActiveSolanaWallet } from "@coral-xyz/recoil";
 
-const isLiveCheck = fetch("https://xnft.wao.gg/api/isLive")
+const isLiveCheck = fetch("https://one.xnfts.dev/api/isLive")
   .then((r) => r.json())
   .catch(() => ({ isLive: false }));
 
 export function useIsONELive() {
   const [isLive, setIsLive] = useState<boolean | "loading">("loading");
   const wallet = useActiveSolanaWallet();
-
   useEffect(() => {
     isLiveCheck.then((response) => {
       setIsLive(response.isLive);
