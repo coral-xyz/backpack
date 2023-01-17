3807373e6c3bd789a0113a4f1943a98c60e83e9d
Philipp Adrian <hello@philippadrian.com>
2023-01-17 16:52:40 +0100

    Add vote type & user_vote_sums

---
+++ backend/reef/hasura/migrations/default/1673965909118_mad_feed_post_vote_sums_migration/up.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE VIEW "public"."mad_feed_post_vote_sums" AS 
+ SELECT mad_feed_posts.post_id,
+    COALESCE(sum(mad_feed_votes.value), (0)::numeric) AS total,
+    COALESCE((
+        SELECT SUM(auth.mad_feed_votes.value)
+        FROM auth.mad_feed_votes
+        WHERE auth.mad_feed_posts.post_id=auth.mad_feed_votes.post_id 
+            AND auth.mad_feed_votes.type = 'VOTE'
+        GROUP BY auth.mad_feed_posts.post_id
+    ), 0)  AS votes, 
+    COALESCE((
+        SELECT SUM(auth.mad_feed_votes.value)
+        FROM auth.mad_feed_votes
+        WHERE auth.mad_feed_posts.post_id=auth.mad_feed_votes.post_id 
+            AND auth.mad_feed_votes.type = 'BACKPACK'
+        GROUP BY auth.mad_feed_posts.post_id
+    ), 0)  AS backpack
+   FROM (auth.mad_feed_posts
+     LEFT JOIN auth.mad_feed_votes ON ((mad_feed_posts.post_id = mad_feed_votes.post_id)))
+  GROUP BY mad_feed_posts.post_id, auth.mad_feed_votes.post_id;
