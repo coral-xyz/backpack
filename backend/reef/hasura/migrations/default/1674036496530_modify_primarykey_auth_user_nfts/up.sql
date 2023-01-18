e3562b062618e6b2806438893927382cd3bbf952
Harkirat Singh <harkirat96@gmail.com>
2023-01-18 15:39:20 +0530

    updates

---
+++ backend/reef/hasura/migrations/default/1674036496530_modify_primarykey_auth_user_nfts/up.sql
@@ -0,0 +1,6 @@
+BEGIN TRANSACTION;
+ALTER TABLE "auth"."user_nfts" DROP CONSTRAINT "user_nfts_pkey";
+
+ALTER TABLE "auth"."user_nfts"
+    ADD CONSTRAINT "user_nfts_pkey" PRIMARY KEY ("public_key", "nft_id", "centralized_group");
+COMMIT TRANSACTION;
