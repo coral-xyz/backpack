25e679fca74f111a3fc5abd03ca66bc1e0f0b090
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-01-18 12:44:45 -0600

    adds eslint react native config (#2138)
    * adds react-native config
    
    * adds react native eslint

--- packages/eslint-config-custom/native.js
+++ packages/eslint-config-custom/native.js
@@ -17,6 +17,7 @@ module.exports = {
     "./shared/react.js",
     // "./shared/prettier.js", // i added extends prettier to core
   ],
+  plugins: ["react-native"],
   globals: {
     __DEV__: false,
     Atomics: false,
@@ -52,4 +53,12 @@ module.exports = {
       env: { browser: true },
     },
   ],
+  rules: {
+    "react-native/no-unused-styles": "warn",
+    "react-native/split-platform-components": "warn",
+    // "react-native/no-inline-styles": "warn",
+    // "react-native/no-color-literals": "warn",
+    // "react-native/no-raw-text": "warn",
+    // "react-native/no-single-element-style-arrays": "warn",
+  },
 };
