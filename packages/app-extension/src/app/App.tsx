ee099fab6d62cf41036996eafed7989b178cee15
Armani Ferrante <armaniferrante@gmail.com>
2023-01-19 15:15:31 -0600

    quick click to pop load times (#2160)

--- packages/app-extension/src/app/App.tsx
+++ packages/app-extension/src/app/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { HashRouter } from "react-router-dom";
 import {
   NotificationsProvider,
   useBackgroundKeepAlive,
 } from "@coral-xyz/recoil";
+import { useCustomTheme } from "@coral-xyz/themes";
 import { RecoilRoot } from "recoil";
 
 import "@fontsource/inter";
@@ -11,22 +12,23 @@ import "@fontsource/inter";
 import { WithTheme } from "../components/common/WithTheme";
 
 import { ErrorBoundary } from "./ErrorBoundary";
-import { Router } from "./Router";
+
+const Router = lazy(() => import("./Router"));
 
 import "./App.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "react-toastify/dist/ReactToastify.css";
 
+const BACKDROP_STYLE = {
+  height: "100vh",
+  minHeight: "600px",
+  minWidth: "375px",
+};
+
 export default function App() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        minHeight: "600px",
-        minWidth: "375px",
-      }}
-    >
+    <div style={BACKDROP_STYLE}>
       <HashRouter>
         <RecoilRoot>
           <_App />
@@ -42,9 +44,27 @@ function _App() {
     <WithTheme>
       <NotificationsProvider>
         <ErrorBoundary>
-          <Router />
+          <_Router />
         </ErrorBoundary>
       </NotificationsProvider>
     </WithTheme>
+  );
+}
+
+function _Router() {
+  const theme = useCustomTheme();
+  return (
+    <Suspense
+      fallback={
+        <div
+          style={{
+            ...BACKDROP_STYLE,
+            background: theme.custom.colors.backgroundBackdrop,
+          }}
+        ></div>
+      }
+    >
+      <Router />
+    </Suspense>
   );
 }
