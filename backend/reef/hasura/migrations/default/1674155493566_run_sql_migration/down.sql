1c503b889ebb14305ceaee878e6a99c9934f72b2
Philipp Adrian <hello@philippadrian.com>
2023-01-19 22:37:06 +0100

    refactor view layouts to calculate total_score & rank per user

---
+++ backend/reef/hasura/migrations/default/1674155493566_run_sql_migration/down.sql
@@ -0,0 +1,15 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE VIEW "public"."mad_feed_user_promoted_post_sums" AS
+--  SELECT auth.mad_feed_votes.user_id,
+--     COALESCE(sum(public.mad_feed_post_vote_sums.votes), 0 ) AS promoted_votes
+--    FROM (
+--         auth.mad_feed_votes
+--         LEFT JOIN public.mad_feed_post_vote_sums ON (
+--           (auth.mad_feed_votes.post_id = public.mad_feed_post_vote_sums.post_id)
+--         )
+--       )
+--     WHERE (
+--             (auth.mad_feed_votes.type = 'PROMOTED' :: text)
+--         )
+--     GROUP BY auth.mad_feed_votes.user_id, auth.mad_feed_votes.type;
