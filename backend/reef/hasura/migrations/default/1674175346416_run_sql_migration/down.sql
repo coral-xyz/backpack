1b1ae20be65fb0a5d2c0b6fd91e9d6297b9db881
Philipp Adrian <greenish@users.noreply.github.com>
2023-01-20 02:07:34 +0100

    Mad feed promoted post hasura updates (#2161)
    * Separate vote.ranking & vote.value + improved user vote aggregation
    
    * refactor view layouts to calculate total_score & rank per user
    
    * include promotion_results in total score

---
+++ backend/reef/hasura/migrations/default/1674175346416_run_sql_migration/down.sql
@@ -0,0 +1,8 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE VIEW "public"."mad_feed_user_promo_results" AS
+--  SELECT mad_feed_votes.user_id,
+--     COALESCE(sum(mad_feed_votes.value), (0)::numeric) AS total_promotion_results
+--    FROM auth.mad_feed_votes
+--   WHERE (mad_feed_votes.type = 'PROMOTION_RESULT'::text)
+--   GROUP BY mad_feed_votes.user_id;
---
+++ backend/reef/hasura/migrations/default/1674175365413_drop_view_public_mad_feed_user_promo_result_sums/up.sql
@@ -0,0 +1,1 @@
+DROP VIEW "public"."mad_feed_user_promo_result_sums";
