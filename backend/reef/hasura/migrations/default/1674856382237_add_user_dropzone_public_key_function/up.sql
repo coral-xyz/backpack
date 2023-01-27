821f36511d10228cf485a7ca05fb6ca7647b8057
wentokay <wentokay@users.noreply.github.com>
2023-01-27 22:10:20 +0000

    specify solana

--- backend/reef/hasura/migrations/default/1674856382237_add_user_dropzone_public_key_function/up.sql
+++ backend/reef/hasura/migrations/default/1674856382237_add_user_dropzone_public_key_function/up.sql
@@ -3,5 +3,6 @@ RETURNS auth.public_keys AS $$
   SELECT *
   FROM auth.public_keys
   WHERE auth.public_keys.user_id = user_row.id
+  AND auth.public_keys.blockchain = 'solana'
   LIMIT 1
 $$ LANGUAGE sql STABLE;
