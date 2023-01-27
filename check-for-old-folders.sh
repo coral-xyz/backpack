8b95080cd94572270015d809cadf1262f911161c
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-01-27 12:18:25 -0600

    adds check-for-old-folders.sh script

---
+++ check-for-old-folders.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+one_week_ago=$(date -v-1w +%Y-%m-%d)
+two_weeks_ago=$(date -v-2w +%Y-%m-%d)
+
+before=$one_week_ago
+after=$two_weeks_ago
+
+# Iterate through all the commits and find the ones that have the deploy: message
+# The commits on this branch are different than master, which is why we're doing this!
+commits=$(
+   git log --pretty=format:"%s" --after="$after" --before="$before" \
+   --grep="deploy:" | sed 's/deploy: //g' | cut -c1-8
+ )
+
+# Split the commits into an array, iterate through and delete older folders
+IFS=$'\n' read -rd '' -a commits <<< "$commits"
+for short_sha in "${commits[@]}"; do
+  folder="background-scripts/$short_sha";
+  if [ -d "$folder" ]; then
+    echo "deleting: $short_sha"
+    rm -rf "$folder";
+    git add "$folder";
+  fi
+done
+
+git commit -m "delete folders $before:$after" -m "commit string"
+git push
