9ff511ae70970c58248a0939e9e712c1c1b7654e
Renfeng Shi <renfeng@keyst.one>
2023-01-29 18:37:44 +0800

    import keystone

--- packages/app-extension/src/components/Unlocked/Settings/AddConnectWallet/ConnectHardware/ConnectHardwareKeystone.tsx
+++ packages/app-extension/src/components/Unlocked/Settings/AddConnectWallet/ConnectHardware/ConnectHardwareKeystone.tsx
@@ -1,3 +1,39 @@
+import { UI_RPC_METHOD_KEYSTONE_IMPORT } from "@coral-xyz/common";
+import { useBackgroundClient } from "@coral-xyz/recoil";
+import { URType, useAnimatedQRScanner } from "@keystonehq/animated-qr";
+
 export function ConnectHardwareKeystone() {
-  return <p>Keystone</p>;
+  const { AnimatedQRScanner, setIsDone } = useAnimatedQRScanner({});
+  const background = useBackgroundClient();
+
+  let t = Date.now();
+  const handleError = (err: string) => {
+    console.log("handleError", Date.now() - t, err);
+    t = Date.now();
+    setIsDone(false);
+  };
+  const handleScan = async ({ type, cbor }: { type: string; cbor: string }) => {
+    console.log("handleScan", Date.now() - t, type, cbor);
+    t = Date.now();
+    await background.request({
+      method: UI_RPC_METHOD_KEYSTONE_IMPORT,
+      params: [type, cbor],
+    });
+  };
+
+  return (
+    <>
+      <p>Keystone1</p>
+      <AnimatedQRScanner
+        urTypes={[URType.CRYPTO_MULTI_ACCOUNTS]}
+        handleError={handleError}
+        handleScan={handleScan}
+        options={{
+          width: 200,
+          height: 200,
+          blur: false,
+        }}
+      />
+    </>
+  );
 }
