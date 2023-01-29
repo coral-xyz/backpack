6abab17ad363116e9bcd151954a505a721d37c80
Matthew Callens <callensmatt@gmail.com>
2023-01-29 13:33:35 -0500

    fix skeleton styles for user list icons

--- packages/message-sdk/src/components/UserList.tsx
+++ packages/message-sdk/src/components/UserList.tsx
@@ -204,5 +204,11 @@ function UserListItem({
 
 function UserIcon({ image }: any) {
   const classes = useStyles();
-  return <ProxyImage src={image} className={classes.iconCircular} />;
+  return (
+    <ProxyImage
+      loadingStyles={{ marginRight: "8px", height: "32px", width: "32px" }}
+      src={image}
+      className={classes.iconCircular}
+    />
+  );
 }
