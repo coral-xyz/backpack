e06abe0beb7738303c3b1fb8727e99e148737248
Kirat <harkirat96@gmail.com>
2023-02-02 22:12:07 +0530

    fixed proxy image (#2423)

--- packages/react-common/src/components/base/LocalImage.tsx
+++ packages/react-common/src/components/base/LocalImage.tsx
@@ -2,21 +2,18 @@ import { useRef, useState } from "react";
 import { OFFLINE_IMAGES } from "@coral-xyz/common";
 import { useFeatureGates } from "@coral-xyz/recoil";
 
+import { ProxyImage } from "./ProxyImage";
+
 export const LocalImage = (props) => {
   const featureGates = useFeatureGates();
-  const imageRef = useRef<HTMLImageElement>(null);
 
   return (
-    <img
+    <ProxyImage
       src={
         featureGates[OFFLINE_IMAGES]
           ? localStorage.getItem(`img-${props.src}`) || props.src
           : props.src
       }
-      ref={imageRef}
-      onError={(...e) => {
-        if (imageRef.current) imageRef.current.src = props.src;
-      }}
       onClick={props.onClick}
       alt={props.alt}
       className={props.className}
