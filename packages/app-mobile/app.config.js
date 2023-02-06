0dea08d3f6c8ca750d44e2260c1d6bb7f039edf1
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-02-06 11:27:47 -0600

    init

--- packages/app-mobile/app.config.js
+++ packages/app-mobile/app.config.js
@@ -1,21 +1,22 @@
 const { execSync } = require("child_process");
+const DEFAULT_HASH = "3603ba14";
 
-function getLastCommitHash() {
-  if (process.env.EAS_BUILD_GIT_COMMIT_HASH) {
-    return process.env.EAS_BUILD_GIT_COMMIT_HASH.substring(0, 7);
-  }
-
-  if (process.env.COMMIT_HASH) {
-    return process.env.COMMIT_HASH.trim().substring(0, 7);
-  }
-
-  try {
-    const output = execSync("git rev-parse HEAD").toString();
-    return output.substring(0, 7);
-  } catch (_) {
-    return "6ed26f6"; // falls back to a recent commit hash until we have a better solution
-  }
-}
+// function getLastCommitHash() {
+//   if (process.env.EAS_BUILD_GIT_COMMIT_HASH) {
+//     return process.env.EAS_BUILD_GIT_COMMIT_HASH.substring(0, 7);
+//   }
+//
+//   if (process.env.COMMIT_HASH) {
+//     return process.env.COMMIT_HASH.trim().substring(0, 7);
+//   }
+//
+//   try {
+//     const output = execSync("git rev-parse HEAD").toString();
+//     return output.substring(0, 7);
+//   } catch (_) {
+//     return DEFAULT_HASH
+//   }
+// }
 
 const projectID = "55bf074d-0473-4e61-9d9d-ecf570704635";
 const packageName = "peterpme.coral.backpack";
@@ -25,8 +26,8 @@ const Config = {
 };
 
 if (process.env.APP_ENV === "production") {
-  const commitHash = getLastCommitHash();
-  Config.webviewUrl = `https://coral-xyz.github.io/backpack/background-scripts/${commitHash}/service-worker-loader.html`;
+  // const commitHash = getLastCommitHash();
+  Config.webviewUrl = `https://coral-xyz.github.io/backpack/background-scripts/${DEFAULT_HASH}/service-worker-loader.html`;
 }
 
 export default {
