129c9027c2e5594b7ed0399d196e857ef40b0666
wentokay <wentokay@users.noreply.github.com>
2023-02-07 19:20:17 -0600

    add `no-floating-promises` eslint rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -23,6 +23,7 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-non-null-assertion": "off",
     "import/default": "off",
