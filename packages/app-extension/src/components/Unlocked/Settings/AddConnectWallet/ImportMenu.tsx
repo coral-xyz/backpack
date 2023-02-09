7bfcccf6c9dd4ef97df7d39aa2e4a9431e367476
Tom Linton <tlinton@fastmail.fm>
2023-02-09 19:46:52 +1300

    remove extra element

--- packages/app-extension/src/components/Unlocked/Settings/AddConnectWallet/ImportMenu.tsx
+++ packages/app-extension/src/components/Unlocked/Settings/AddConnectWallet/ImportMenu.tsx
@@ -36,69 +36,67 @@ export function ImportMenu({ blockchain }: { blockchain: Blockchain }) {
   }, [blockchain]);
 
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100%",
-        }}
-      >
-        <Box sx={{ margin: "24px" }}>
-          <Header text="Advanced import" />
-          <SubtextParagraph>
-            Import a wallet and associate it with your Backpack account.
-          </SubtextParagraph>
-        </Box>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+    >
+      <Box sx={{ margin: "24px" }}>
+        <Header text="Advanced import" />
+        <SubtextParagraph>
+          Import a wallet and associate it with your Backpack account.
+        </SubtextParagraph>
+      </Box>
 
-        <Box sx={{ margin: "0 16px" }}>
-          <Grid container spacing={2}>
-            {(keyringExists || hasMnemonic) && (
-              // The blockchain keyring must exist or there must be a mnemonic
-              // to allow imports from secret recovery phrases. If there is no
-              // mnemonic and the keyring does not exist a secret recovery phrase
-              // import can only done via a private key, and you can't currently
-              // init a blockchain keyring using a private key based wallet.
-              <Grid item xs={12}>
-                <ActionCard
-                  text="Import from secret recovery phrase"
-                  subtext="Select from a list of wallets found using a secret recovery phrase."
-                  onClick={() =>
-                    nav.push("import-from-mnemonic", {
-                      blockchain,
-                      keyringExists,
-                      hasMnemonic,
-                    })
-                  }
-                />
-              </Grid>
-            )}
+      <Box sx={{ margin: "0 16px" }}>
+        <Grid container spacing={2}>
+          {(keyringExists || hasMnemonic) && (
+            // The blockchain keyring must exist or there must be a mnemonic
+            // to allow imports from secret recovery phrases. If there is no
+            // mnemonic and the keyring does not exist a secret recovery phrase
+            // import can only done via a private key, and you can't currently
+            // init a blockchain keyring using a private key based wallet.
             <Grid item xs={12}>
               <ActionCard
-                text="Import from hardware wallet"
-                subtext="Select from a list of wallets found using your hardware wallet."
-                onClick={() => {
-                  openConnectHardware(blockchain, "import");
-                  window.close();
-                }}
+                text="Import from secret recovery phrase"
+                subtext="Select from a list of wallets found using a secret recovery phrase."
+                onClick={() =>
+                  nav.push("import-from-mnemonic", {
+                    blockchain,
+                    keyringExists,
+                    hasMnemonic,
+                  })
+                }
               />
             </Grid>
-            {keyringExists && (
-              // TODO allow creating a keyring from just a private key
-              // https://github.com/coral-xyz/backpack/issues/2164
-              <Grid item xs={12}>
-                <ActionCard
-                  text="Import from secret key"
-                  subtext="Import a wallet using a secret key."
-                  onClick={() =>
-                    nav.push("import-from-secret-key", { blockchain })
-                  }
-                />
-              </Grid>
-            )}
+          )}
+          <Grid item xs={12}>
+            <ActionCard
+              text="Import from hardware wallet"
+              subtext="Select from a list of wallets found using your hardware wallet."
+              onClick={() => {
+                openConnectHardware(blockchain, "import");
+                window.close();
+              }}
+            />
           </Grid>
-        </Box>
-      </div>
-    </>
+          {keyringExists && (
+            // TODO allow creating a keyring from just a private key
+            // https://github.com/coral-xyz/backpack/issues/2164
+            <Grid item xs={12}>
+              <ActionCard
+                text="Import from secret key"
+                subtext="Import a wallet using a secret key."
+                onClick={() =>
+                  nav.push("import-from-secret-key", { blockchain })
+                }
+              />
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </div>
   );
 }
