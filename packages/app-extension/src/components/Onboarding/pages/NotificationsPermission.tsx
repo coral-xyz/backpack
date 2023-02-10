85fa0c2598d488234b7ff2a9de4dd0d95b8abd58
Matthew Callens <callensmatt@gmail.com>
2023-02-09 21:50:30 -0500

    update notification onboarding page with new illustration (#2605)

--- packages/app-extension/src/components/Onboarding/pages/NotificationsPermission.tsx
+++ packages/app-extension/src/components/Onboarding/pages/NotificationsPermission.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import type { CSSProperties } from "react";
 import { PrimaryButton, SecondaryButton } from "@coral-xyz/react-common";
 import { useCustomTheme } from "@coral-xyz/themes";
 import { Box } from "@mui/material";
@@ -53,26 +53,14 @@ export const NotificationsPermission = ({ onNext }: { onNext: () => void }) => {
       }}
     >
       <Box>
-        <Box sx={{ mt: "24px", ml: "24px", mr: "24px" }}>
+        <Box sx={{ mt: "24px", mx: "24px" }}>
           <Header text="Allow notifications?" />
-          <SubtextParagraph style={{ marginBottom: 0 }}>
-            Find out when you have:
+          <SubtextParagraph style={{ marginBottom: "20px" }}>
+            These appear for messages and contact requests.
           </SubtextParagraph>
-          <div style={{ marginBottom: "20px" }}>
-            <ul
-              style={{
-                marginTop: 0,
-                paddingLeft: "25px",
-                color: theme.custom.colors.subtext,
-              }}
-            >
-              <li>New messages</li>
-              <li>Contact requests</li>
-            </ul>
-          </div>
-          <SubtextParagraph>
-            You can change this later in preferences.
-          </SubtextParagraph>
+        </Box>
+        <Box sx={{ mx: "20px" }}>
+          <SkeletonNotifications style={{ width: "100%" }} />
         </Box>
       </Box>
       <Box sx={{ mx: "16px", mb: "16px", display: "flex", gap: "10px" }}>
@@ -91,3 +79,60 @@ export const NotificationsPermission = ({ onNext }: { onNext: () => void }) => {
     </Box>
   );
 };
+
+const SkeletonNotifications = ({ style }: { style?: CSSProperties }) => (
+  <svg
+    style={{ height: "inherit", width: "inherit", ...style }}
+    width="343"
+    height="194"
+    viewBox="0 0 343 194"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_18596_144803)">
+      <rect width="343" height="64" fill="white" />
+      <circle cx="28" cy="32" r="16" fill="#DFE0E6" />
+      <rect x="52" y="20" width="72" height="8" rx="4" fill="#DFE0E6" />
+      <rect x="52" y="36" width="176" height="8" rx="4" fill="#DFE0E6" />
+      <rect x="291" y="20" width="40" height="8" rx="4" fill="#DFE0E6" />
+      <g opacity="0.5">
+        <rect
+          width="343"
+          height="64"
+          transform="translate(0 65)"
+          fill="white"
+        />
+        <circle cx="28" cy="97" r="16" fill="#DFE0E6" />
+        <rect x="52" y="85" width="72" height="8" rx="4" fill="#DFE0E6" />
+        <rect x="52" y="101" width="176" height="8" rx="4" fill="#DFE0E6" />
+        <rect x="291" y="85" width="40" height="8" rx="4" fill="#DFE0E6" />
+      </g>
+      <g opacity="0.5">
+        <rect
+          width="343"
+          height="64"
+          transform="translate(0 130)"
+          fill="white"
+        />
+        <circle cx="28" cy="162" r="16" fill="#DFE0E6" />
+        <rect x="52" y="150" width="72" height="8" rx="4" fill="#DFE0E6" />
+        <rect x="52" y="166" width="176" height="8" rx="4" fill="#DFE0E6" />
+        <rect x="291" y="150" width="40" height="8" rx="4" fill="#DFE0E6" />
+      </g>
+    </g>
+    <rect
+      x="1"
+      y="1"
+      width="341"
+      height="192"
+      rx="11"
+      stroke="#F0F0F2"
+      strokeWidth="2"
+    />
+    <defs>
+      <clipPath id="clip0_18596_144803">
+        <rect width="343" height="194" rx="12" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
