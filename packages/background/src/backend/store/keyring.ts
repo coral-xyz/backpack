b433478418ffff280e7a1673e489b276810e7b26
Armani Ferrante <armaniferrante@gmail.com>
2023-02-12 12:16:38 -0600

    stash

--- packages/background/src/backend/store/keyring.ts
+++ packages/background/src/backend/store/keyring.ts
@@ -8,6 +8,7 @@ import { migrate_0_2_0_510 } from "./migrations/migrate_0_2_0_510";
 import { migrate_0_2_0_2408 } from "./migrations/migrate_0_2_0_2408";
 import { LocalStorageDb } from "./db";
 import { getMigration, setMigration } from "./migrations";
+import { getWalletData_DEPRECATED } from "./preferences";
 
 const KEY_KEYRING_STORE = "keyring-store";
 
@@ -61,13 +62,32 @@ async function runMigrationsIfNeeded(
   uuid: string,
   password: string
 ) {
+  const LATEST_MIGRATION_BUILD = 2408; // Update this everytime a migration is added.
   const lastMigration = await getMigration();
+
+  //
+  // If we've already migrated to the latest build, then exit.
+  //
+  if (
+    lastMigration?.state === "finalized" &&
+    lastMigration?.build === LATEST_MIGRATION_BUILD
+  ) {
+    return;
+  }
+
+  //
+  // If a migration step terminated early, then we're in a corrupt state, so
+  // exit with an error.
+  //
   if (lastMigration !== undefined && lastMigration?.state !== "end") {
-    logger.error("a previous migration failed, please re-install Backpack");
     throw new Error("migration failed, please re-install Backpack");
   }
 
-  if ((await getMigration()) === undefined) {
+  //
+  // Execute all migrations, if needed.
+  //
+  const needs510Migration = (await getWalletData_DEPRECATED()) !== undefined;
+  if (needs510Migration) {
     logger.debug("running migration 510");
     await setMigration({
       build: 510,
@@ -92,6 +112,16 @@ async function runMigrationsIfNeeded(
       state: "end",
     });
     logger.debug("migration 2408 was a success");
+  }
+
+  //
+  // Set the last migration as finalized.
+  //
+  if ((await getMigration())?.state !== "finalized") {
+    await setMigration({
+      build: LATEST_MIGRATION_BUILD, // Represents the latest build.
+      state: "finalized",
+    });
   }
 }
 
--- packages/background/src/backend/store/migrations/index.ts
+++ packages/background/src/backend/store/migrations/index.ts
@@ -1,14 +1,21 @@
 import { LocalStorageDb } from "../db";
 
 /**
- * Stores the build number for the last migration.
+ * Migrations are setup with two pieces of data.
+ *
+ * 1) The latest migration. This stores the build number for the last migration
+ *    alongside its state.
+ * 2) A linear log of all migrations ever executed for this local client.
+ *    This is provided mostly for debugging purposes, as it maintains a
+ *    history of all migration state transitions.
  */
 
 const STORE_MIGRATION_KEY = "last-migration";
+const STORE_MIGRATION_LOG_KEY = "migration-log";
 
 export type Migration = {
   build: number;
-  state: "start" | "end";
+  state: "start" | "end" | "finalized";
 };
 
 export async function getMigration(): Promise<Migration | undefined> {
@@ -18,4 +25,18 @@ export async function getMigration(): Promise<Migration | undefined> {
 
 export async function setMigration(m: Migration) {
   await LocalStorageDb.set(STORE_MIGRATION_KEY, m);
+  await pushMigrationLog(m);
+}
+
+async function getMigrationLog(): Promise<Array<Migration> | undefined> {
+  return await LocalStorageDb.get(STORE_MIGRATION_LOG_KEY);
+}
+
+async function pushMigrationLog(m: Migration) {
+  let log = await getMigrationLog();
+  if (log === undefined) {
+    log = [];
+  }
+  log.push(m);
+  await LocalStorageDb.set(STORE_MIGRATION_LOG_KEY, m);
 }
