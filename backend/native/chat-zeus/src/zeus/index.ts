524088b38bfe42aa04f425bf5bf00099b058fbba
Harkirat Singh <harkirat96@gmail.com>
2023-02-17 03:19:45 +0530

    fix

--- backend/native/chat-zeus/src/zeus/index.ts
+++ backend/native/chat-zeus/src/zeus/index.ts
@@ -604,6 +604,7 @@ export const ResolveFromPath = (
   returns: ReturnTypesType,
   ops: Operations
 ) => {
+  //@ts-ignore
   const ResolvePropsType = (mappedParts: Part[]) => {
     const oKey = ops[mappedParts[0].v];
     const propsP1 = oKey ? props[oKey] : props[mappedParts[0].v];
@@ -646,6 +647,7 @@ export const ResolveFromPath = (
       }
     }
   };
+  // @ts-ignore
   const ResolveReturnType = (mappedParts: Part[]) => {
     if (mappedParts.length === 0) {
       return "not";
