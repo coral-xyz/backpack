b8de1ef76237df51bfb225c407d6cef34169be60
Tom Linton <tlinton@fastmail.fm>
2023-02-17 22:46:26 +1300

    remove extra refresh and fix debounce timer typing

--- packages/message-sdk/src/components/Inbox.tsx
+++ packages/message-sdk/src/components/Inbox.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SearchBox } from "@coral-xyz/app-extension/src/components/Unlocked/Messages/SearchBox";
 import type {
   CollectionChatData,
@@ -6,10 +6,7 @@ import type {
   RemoteUserData,
 } from "@coral-xyz/common";
 import { BACKEND_API_URL } from "@coral-xyz/common";
-import {
-  EmptyState,
-  refreshGroupsAndFriendships,
-} from "@coral-xyz/react-common";
+import { EmptyState } from "@coral-xyz/react-common";
 import {
   useFriendships,
   useGroupCollections,
@@ -25,7 +22,7 @@ import { MessagesSkeleton } from "./MessagesSkeleton";
 import { useStyles } from "./styles";
 import { UserList } from "./UserList";
 
-let debouncedTimer;
+let debouncedTimer: ReturnType<typeof setTimeout>;
 
 export function Inbox() {
   return <InboxInner />;
@@ -68,14 +65,10 @@ export function InboxInner() {
         .includes(result.username)
   );
 
-  useEffect(() => {
-    refreshGroupsAndFriendships(uuid);
-  }, [uuid]);
-
   const debouncedInit = (prefix: string) => {
     clearTimeout(debouncedTimer);
-    debouncedTimer = setTimeout(() => {
-      handleContactSearch(prefix);
+    debouncedTimer = setTimeout(async () => {
+      await handleContactSearch(prefix);
     }, 250);
   };
 
