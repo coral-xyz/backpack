27ef114deacb1b08bf930202c2e7ee119acd442a
Tom Linton <tlinton@fastmail.fm>
2023-02-20 10:09:09 +1300

    fix null case

--- packages/chat-sdk/src/components/ParsedMessage.tsx
+++ packages/chat-sdk/src/components/ParsedMessage.tsx
@@ -11,50 +11,54 @@ export function ParsedMessage({ message }) {
   const parts = parseMessage(message);
   const theme = useCustomTheme();
   const { usersMetadata } = useChatContext();
-  return parts.map((part) => {
-    if (part.type === "text") {
-      return (
-        <span style={{ wordBreak: "break-word" }}>
-          <Linkify options={{ target: "_blank" }}>{part.value}</Linkify>
-        </span>
-      );
-    } else {
-      const user = usersMetadata[part.value];
-      if (user) {
-        const handle = `@${user.username}`;
-        return (
-          <span
-            onClick={() => {
-              push({
-                title: `@${user.username}`,
-                componentId: NAV_COMPONENT_MESSAGE_CHAT,
-                componentProps: {
-                  userId: user.uuid,
-                  username: user.username,
-                },
-              });
-            }}
-            style={{
-              cursor: "pointer",
-              color: theme.custom.colors.blue,
-            }}
-          >
-            {handle}
-          </span>
-        );
-      } else {
-        return (
-          <div style={{ display: "flex" }}>
-            <Skeleton
-              style={{ marginTop: 3, marginLeft: 3, marginRight: 1 }}
-              variant="circular"
-              width={15}
-              height={15}
-            />
-            <Skeleton width={30} height={20} style={{ marginTop: "0px" }} />
-          </div>
-        );
-      }
-    }
-  });
+  return (
+    <>
+      {parts.map((part) => {
+        if (part.type === "text") {
+          return (
+            <span style={{ wordBreak: "break-word" }}>
+              <Linkify options={{ target: "_blank" }}>{part.value}</Linkify>
+            </span>
+          );
+        } else {
+          const user = usersMetadata[part.value];
+          if (user) {
+            const handle = `@${user.username}`;
+            return (
+              <span
+                onClick={() => {
+                  push({
+                    title: `@${user.username}`,
+                    componentId: NAV_COMPONENT_MESSAGE_CHAT,
+                    componentProps: {
+                      userId: user.uuid,
+                      username: user.username,
+                    },
+                  });
+                }}
+                style={{
+                  cursor: "pointer",
+                  color: theme.custom.colors.blue,
+                }}
+              >
+                {handle}
+              </span>
+            );
+          } else {
+            return (
+              <div style={{ display: "flex" }}>
+                <Skeleton
+                  style={{ marginTop: 3, marginLeft: 3, marginRight: 1 }}
+                  variant="circular"
+                  width={15}
+                  height={15}
+                />
+                <Skeleton width={30} height={20} style={{ marginTop: "0px" }} />
+              </div>
+            );
+          }
+        }
+      })}
+    </>
+  );
 }
