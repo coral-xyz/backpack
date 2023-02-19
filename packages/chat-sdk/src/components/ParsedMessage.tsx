c67442e78efbc63ecd813dd4f136c365f1f9e24c
Tom Linton <tlinton@fastmail.fm>
2023-02-20 09:58:00 +1300

    fix user tagging breaking chat layout in long messages

--- packages/chat-sdk/src/components/ParsedMessage.tsx
+++ packages/chat-sdk/src/components/ParsedMessage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NAV_COMPONENT_MESSAGE_CHAT, parseMessage } from "@coral-xyz/common";
 import { useNavigation } from "@coral-xyz/recoil";
 import { useCustomTheme } from "@coral-xyz/themes";
@@ -12,59 +11,50 @@ export function ParsedMessage({ message }) {
   const parts = parseMessage(message);
   const theme = useCustomTheme();
   const { usersMetadata } = useChatContext();
-  return (
-    <div style={{ display: "flex" }}>
-      {parts.map((part, i) => {
-        if (part.type === "text") {
-          return (
-            <span style={{ wordBreak: "break-word" }}>
-              <Linkify options={{ target: "_blank" }}>{part.value}</Linkify>
-            </span>
-          );
-        } else {
-          const user = usersMetadata[part.value];
-          if (user) {
-            const handle = `@${user.username}`;
-            return (
-              <div
-                onClick={() => {
-                  push({
-                    title: `@${user.username}`,
-                    componentId: NAV_COMPONENT_MESSAGE_CHAT,
-                    componentProps: {
-                      userId: user.uuid,
-                      username: user.username,
-                    },
-                  });
-                }}
-                style={{
-                  cursor: "pointer",
-                  display: "flex",
-                  color: theme.custom.colors.blue,
-                }}
-              >
-                <div>
-                  {i > 0 && <>&nbsp;</>}
-                  {handle}
-                  {i < parts.length - 1 && <>&nbsp;</>}
-                </div>
-              </div>
-            );
-          } else {
-            return (
-              <div style={{ display: "flex" }}>
-                <Skeleton
-                  style={{ marginTop: 3, marginLeft: 3, marginRight: 1 }}
-                  variant="circular"
-                  width={15}
-                  height={15}
-                />
-                <Skeleton width={30} height={20} style={{ marginTop: "0px" }} />
-              </div>
-            );
-          }
-        }
-      })}
-    </div>
-  );
+  return parts.map((part) => {
+    if (part.type === "text") {
+      return (
+        <span style={{ wordBreak: "break-word" }}>
+          <Linkify options={{ target: "_blank" }}>{part.value}</Linkify>
+        </span>
+      );
+    } else {
+      const user = usersMetadata[part.value];
+      if (user) {
+        const handle = `@${user.username}`;
+        return (
+          <span
+            onClick={() => {
+              push({
+                title: `@${user.username}`,
+                componentId: NAV_COMPONENT_MESSAGE_CHAT,
+                componentProps: {
+                  userId: user.uuid,
+                  username: user.username,
+                },
+              });
+            }}
+            style={{
+              cursor: "pointer",
+              color: theme.custom.colors.blue,
+            }}
+          >
+            {handle}
+          </span>
+        );
+      } else {
+        return (
+          <div style={{ display: "flex" }}>
+            <Skeleton
+              style={{ marginTop: 3, marginLeft: 3, marginRight: 1 }}
+              variant="circular"
+              width={15}
+              height={15}
+            />
+            <Skeleton width={30} height={20} style={{ marginTop: "0px" }} />
+          </div>
+        );
+      }
+    }
+  });
 }
