dcec6f462c954de7ebc922314ac86db043614891
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-02-20 10:25:33 -0600

    removes dom renderer

--- packages/app-extension/webpack.dev.config.js
+++ packages/app-extension/webpack.dev.config.js
@@ -3,10 +3,6 @@ const options = require("./webpack.config");
 
 options.resolve.alias = {
   "@coral-xyz/chat-sdk": path.resolve(__dirname, "../chat-sdk/src"),
-  "@coral-xyz/react-xnft-dom-renderer": path.resolve(
-    __dirname,
-    "../react-xnft-dom-renderer/src"
-  ),
   "@coral-xyz/themes": path.resolve(__dirname, "../themes/src"),
   "@coral-xyz/recoil": path.resolve(__dirname, "../recoil/src"),
   "@coral-xyz/react-common": path.resolve(__dirname, "../react-common/src"),
