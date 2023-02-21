7a6026e3168a3d3e8de7bbe5a2300678530e7ee5
Tom Linton <tlinton@fastmail.fm>
2023-02-22 12:04:08 +1300

    remove native

--- packages/xnft-cli/native/build.js
+++
@@ -1,9 +0,0 @@
-const esconfig = require("./esbuild.config.web");
-module.exports = (program) => {
-  program.command("build").action(() => {
-    require("esbuild").build({
-      ...esconfig,
-      minify: true,
-    });
-  });
-};
