3440d7c926ce8984d2c1555f76f26c99f1a27892
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-02-22 17:07:05 -0600

    eslint fixes useless renders

--- packages/app-extension/src/permissions/PermissionsPage.tsx
+++ packages/app-extension/src/permissions/PermissionsPage.tsx
@@ -6,46 +6,44 @@ export const PermissionsPage = ({
   inProgress: boolean;
 }) => {
   return (
-    <>
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",
-          background:
-            "radial-gradient(farthest-side at 0 0, #6360FF, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 100% 0, #C061F7, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 0 100%, #28DBD1 25%, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 100% 100%, #FE6F5C 25%, rgba(255,255,255,0) 100%)",
-        }}
-      >
-        {inProgress && (
-          <img
-            src="/assets/will.png"
-            style={{
-              transform: "rotate(10deg) scaleX(-1)",
-              marginLeft: 350,
-              maxHeight: "90vh",
-            }}
-          ></img>
-        )}
-        <div>
-          {permissionGranted && (
-            <div style={{ fontSize: 50, paddingTop: "45vh" }}>
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <div>
-                  <div>Permissions granted</div>
-                </div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background:
+          "radial-gradient(farthest-side at 0 0, #6360FF, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 100% 0, #C061F7, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 0 100%, #28DBD1 25%, rgba(255,255,255,0) 100%),radial-gradient(farthest-side at 100% 100%, #FE6F5C 25%, rgba(255,255,255,0) 100%)",
+      }}
+    >
+      {inProgress ? (
+        <img
+          src="/assets/will.png"
+          style={{
+            transform: "rotate(10deg) scaleX(-1)",
+            marginLeft: 350,
+            maxHeight: "90vh",
+          }}
+        />
+      ) : null}
+      <div>
+        {permissionGranted ? (
+          <div style={{ fontSize: 50, paddingTop: "45vh" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div>
+                <div>Permissions granted</div>
               </div>
             </div>
-          )}
-          {!permissionGranted && (
-            <div style={{ fontSize: 50, paddingTop: "45vh" }}>
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <div>
-                  <div>Permissions rejected</div>
-                </div>
+          </div>
+        ) : null}
+        {!permissionGranted ? (
+          <div style={{ fontSize: 50, paddingTop: "45vh" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div>
+                <div>Permissions rejected</div>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        ) : null}
       </div>
-    </>
+    </div>
   );
 };
