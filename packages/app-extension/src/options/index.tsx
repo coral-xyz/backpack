aca080380cd5bba227781222671cd8a5d60e4ddf
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-02-22 16:44:21 -0600

    remove createRoot and strict mode (#2930)

--- packages/app-extension/src/options/index.tsx
+++ packages/app-extension/src/options/index.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import Options from "./Options";
 
@@ -8,12 +8,12 @@ const LedgerIframe = lazy(() => import("../components/LedgerIframe"));
 // Render the UI.
 // TOOD(react) createRoot is required: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const container = document.getElementById("options");
-render(
-  <React.StrictMode>
+const root = createRoot(container!);
+root.render(
+  <>
     <Options />
     <Suspense fallback={null}>
       <LedgerIframe />
     </Suspense>
-  </React.StrictMode>,
-  container
+  </>
 );
--- packages/app-extension/src/permissions/index.tsx
+++ packages/app-extension/src/permissions/index.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import Permissions from "./Permissions";
 
 // Render the UI.
 // TOOD(react) createRoot is required: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const container = document.getElementById("permissions");
-render(
-  <React.StrictMode>
-    <Permissions />
-  </React.StrictMode>,
-  container
-);
+const root = createRoot(container!);
+root.render(<Permissions />);
