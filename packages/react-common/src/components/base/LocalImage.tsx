ee42a6f92d17497f8ae9507b506d00a983eb9144
Harkirat Singh <harkirat96@gmail.com>
2023-02-25 02:49:40 +0530

    Update conf

--- packages/react-common/src/components/base/LocalImage.tsx
+++ packages/react-common/src/components/base/LocalImage.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+import { LocalImageManager } from "@coral-xyz/db";
+
 import { ProxyImage } from "./ProxyImage";
 
 export const LocalImage = (props) => {
-  const localSrc = localStorage.getItem(props.localKey ?? `img-${props.src}`);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    if (props.src) {
+      try {
+        const parsedEl = JSON.parse(
+          localStorage.getItem(`image-${props.src}`) || ""
+        );
+        LocalImageManager.getInstance().addToQueue({
+          image: parsedEl.image,
+        });
+        setImageUrl(parsedEl?.url || props.src);
+      } catch (e) {
+        setImageUrl(props.src);
+      }
+    }
+  }, [props.src]);
 
   return (
     <ProxyImage
-      src={localSrc || props.src}
+      src={imageUrl}
       onClick={props.onClick}
       alt={props.alt}
       className={props.className}
