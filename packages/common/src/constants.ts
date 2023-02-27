a84feeab79a14e39ed591b135243c54754d2bf3e
Harkirat Singh <harkirat96@gmail.com>
2023-02-28 03:23:57 +0530

    Fix url

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -575,7 +575,7 @@ export const ALCHEMY_ETHEREUM_MAINNET_API_KEY =
   "DlJr6QuBC2EaE-L60-iqQQGq9hi9-XSZ";
 
 export const AVATAR_BASE_URL = "https://swr.xnfts.dev/avatars";
-export const BACKEND_API_URL = "https://backpack.xnfts.dev";
+export const BACKEND_API_URL = "https://backpack-api.xnfts.dev";
 export const REALTIME_API_URL = "https://backend-ws.xnfts.dev";
 export const MESSAGING_COMMUNICATION_PUSH = "MESSAGING_COMMUNICATION_PUSH";
 export const MESSAGING_COMMUNICATION_FETCH = "MESSAGING_COMMUNICATION_FETCH";
