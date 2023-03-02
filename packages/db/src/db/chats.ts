a8cfb06e3175f55d42798be40eb9c2c032aa0114
Kirat <harkirat96@gmail.com>
2023-03-02 09:11:14 +0530

    Fix build (#3093)

--- packages/db/src/db/chats.ts
+++ packages/db/src/db/chats.ts
@@ -35,7 +35,7 @@ export const deleteChat = async (uuid: string, clientGeneratedUuid: string) => {
       message: "",
       message_metadata: {},
     });
-    return { ...(await db.messages.get(clientGeneratedUuid)), deleted: true };
+    return db.messages.get(clientGeneratedUuid);
   }
   return null;
 };
