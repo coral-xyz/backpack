ca3c37ae2044f95e6d38aeb88001035afca074dc
Kirat <harkirat96@gmail.com>
2023-03-05 00:56:04 +0530

    Fix routes (#3170)
    * Fix
    
    * Fix

--- backend/native/xnft-api-server/src/v1/users.ts
+++ backend/native/xnft-api-server/src/v1/users.ts
@@ -6,56 +6,15 @@ import { getUser, getUserFromUsername, getUserIdFromPubkey } from "../db/users";
 const router = express.Router();
 
 router.get("/fromPubkey", authMiddleware, async (req, res) => {
-  // @ts-ignore
-  const publicKey: string = req.query.publicKey;
-  // @ts-ignore
-  const blockchain: string = req.query.blockchain;
-
-  await getUserIdFromPubkey({ blockchain, publicKey })
-    .then((user) => {
-      if (user) {
-        res.status(200).json({ user });
-      } else {
-        res.status(411).json({ msg: "User not found" });
-      }
-    })
-    .catch((e) => {
-      res.status(500).json({ msg: "Failed to fetch user details" });
-    });
+  return res.json({});
 });
 
 router.get("/fromUsername", authMiddleware, async (req, res) => {
-  // @ts-ignore
-  const username: string = req.query.username;
-
-  await getUserFromUsername({ username })
-    .then((user) => {
-      if (user) {
-        res.status(200).json({ user });
-      } else {
-        res.status(411).json({ msg: "User not found" });
-      }
-    })
-    .catch((e) => {
-      res.status(500).json({ msg: "Failed to fetch user details" });
-    });
+  return res.json({});
 });
 
 router.get("/", authMiddleware, async (req, res) => {
-  // @ts-ignore
-  const id: string = req.query.user_id;
-
-  await getUser(id)
-    .then((user) => {
-      if (user) {
-        res.status(200).json({ user });
-      } else {
-        res.status(411).json({ msg: "User not found" });
-      }
-    })
-    .catch((e) => {
-      res.status(500).json({ msg: "Failed to fetch user details" });
-    });
+  return res.json({});
 });
 
 export default router;
