91141b3a18595c7dd1d88f7807b3b041efb3d952
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-08 17:05:43 -0600

    cleanup

--- packages/app-extension/webpack.config.js
+++ packages/app-extension/webpack.config.js
@@ -190,7 +190,7 @@ const options = {
       },
       babelLoaderConfiguration,
       swcLoaderConfiguration,
-      tamaguiLoaderConfiguration,
+      // tamaguiLoaderConfiguration,
     ],
   },
   resolve: {
