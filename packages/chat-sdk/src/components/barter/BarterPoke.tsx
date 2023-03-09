4995001f4a107c022289992219e6439fbcbe0c3c
Kirat <harkirat96@gmail.com>
2023-03-09 16:27:42 -0700

    Barter polishes (#3258)
    * Fixes
    
    * stuff
    
    * FIxed ui
    
    * Fixed poke
    
    * Fixed poke

--- packages/chat-sdk/src/components/barter/BarterPoke.tsx
+++ packages/chat-sdk/src/components/barter/BarterPoke.tsx
@@ -1,3 +1,58 @@
-export function BarterPoke({ barterId }: { barterId: string }) {
-  return <div>Poke! {barterId}</div>;
+import { useUser } from "@coral-xyz/recoil";
+import { useCustomTheme } from "@coral-xyz/themes";
+
+import { useChatContext } from "../ChatContext";
+
+export function BarterPoke({
+  barterId,
+  sender,
+}: {
+  barterId: string;
+  sender: string;
+}) {
+  const { remoteUsername, setOpenPlugin } = useChatContext();
+  const { username, uuid } = useUser();
+  const theme = useCustomTheme();
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "inline-flex",
+          background: theme.custom.colors.linkColor,
+          padding: "12px 16px",
+          color: "#fff",
+          marginTop: 5,
+          borderRadius: 16,
+        }}
+      >
+        <div style={{ flex: 1 }}>
+          {sender === uuid
+            ? `You poked ${remoteUsername}`
+            : `${remoteUsername} wants to trade`}{" "}
+        </div>
+        <div
+          style={{
+            marginLeft: 10,
+            cursor: "pointer",
+            color: theme.custom.colors.icon,
+          }}
+          onClick={() => {
+            setOpenPlugin({
+              type: "barter",
+              metadata: {
+                barterId,
+              },
+            });
+          }}
+        >
+          View{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
