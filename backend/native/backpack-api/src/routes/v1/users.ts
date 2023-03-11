cea6caa0b0d2fd1494b0aac08e4bec1758040992
Harkirat Singh <harkirat96@gmail.com>
2023-03-11 14:23:01 -0700

    Fixes

--- backend/native/backpack-api/src/routes/v1/users.ts
+++ backend/native/backpack-api/src/routes/v1/users.ts
@@ -43,10 +43,10 @@ import {
 } from "../../validation/user";
 
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 const router = express.Router();
