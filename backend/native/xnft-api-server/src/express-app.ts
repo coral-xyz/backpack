e05baff7d58b43670cc65931e61998b577da3b81
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-12 18:07:49 -0500

    wip

--- backend/native/xnft-api-server/src/express-app.ts
+++ backend/native/xnft-api-server/src/express-app.ts
@@ -1,10 +1,33 @@
-import express from "express";
-export const app = express();
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import express from "express";
 
 import v1Routes from "./v1/index";
+
+export const app = express();
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
 app.use(cookieParser());
@@ -14,6 +37,9 @@ app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(bodyParser.raw({}));
 
 app.use("/v1", v1Routes);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 export default app;
 
