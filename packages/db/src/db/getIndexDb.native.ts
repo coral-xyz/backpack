1af245f66fb00a104e3d4a0cb1bdb5d179bc37d6
Harkirat Singh <harkirat96@gmail.com>
2023-03-14 10:40:05 -0600

    added comment

--- packages/db/src/db/getIndexDb.native.ts
+++ packages/db/src/db/getIndexDb.native.ts
@@ -1,3 +1,8 @@
+/*
+ * This is added to instruct dexie to use
+ * the shimmed indexedDB
+ * https://github.com/dexie/Dexie.js/issues/354#issuecomment-714642331
+ */
 export const getIndexDb = () => ({
   // @ts-ignore
   indexedDB: global.window.indexedDB,
