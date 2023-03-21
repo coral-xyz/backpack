1084f538ccc5e3b8e7643654857459d0652c24a1
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-21 10:28:26 -0500

    adds updates

--- packages/app-mobile/src/hooks/useTheme.tsx
+++ packages/app-mobile/src/hooks/useTheme.tsx
@@ -1,40 +1,15 @@
 // import { useDarkMode } from "@coral-xyz/recoil";
 import { useColorScheme } from "react-native";
 
-import { LIGHT_COLORS, DARK_COLORS } from "@coral-xyz/themes";
+import { MOBILE_DARK_THEME, MOBILE_LIGHT_THEME } from "@coral-xyz/themes";
 
 export function useTheme() {
   // const isDarkMode = useDarkMode();
   const colorScheme = useColorScheme();
-  const theme = colorScheme === "dark" ? darkTheme : lightTheme;
+  const theme = colorScheme === "dark" ? MOBILE_DARK_THEME : MOBILE_LIGHT_THEME;
 
   return {
     custom: theme.custom,
     colorScheme: colorScheme === "dark" ? "dark" : "light",
   };
 }
-
-const baseTheme = {
-  custom: {
-    borderRadius: {
-      large: 12,
-      medium: 8,
-    },
-  },
-};
-
-export const darkTheme = {
-  ...baseTheme,
-  custom: {
-    colors: LIGHT_COLORS,
-  },
-};
-
-export const lightTheme = {
-  ...baseTheme,
-  custom: {
-    colors: DARK_COLORS,
-  },
-};
-
-export type CustomTheme = typeof lightTheme & typeof darkTheme;
