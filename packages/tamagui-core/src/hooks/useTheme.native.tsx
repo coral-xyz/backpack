bd67dfeed55a9aaa814fc54e7632901980274d9d
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-21 12:19:54 -0500

    coalesce useTheme

--- packages/tamagui-core/src/hooks/useTheme.native.tsx
+++ packages/tamagui-core/src/hooks/useTheme.native.tsx
@@ -1,10 +1,10 @@
 import { useColorScheme } from "react-native";
-
-import { darkTheme, lightTheme } from "../theme";
+// @ts-expect-error
+import { MOBILE_DARK_THEME, MOBILE_LIGHT_THEME } from "@coral-xyz/themes";
 
 export function useCustomTheme() {
   const colorScheme = useColorScheme();
-  const theme = colorScheme === "dark" ? darkTheme : lightTheme;
+  const theme = colorScheme === "dark" ? MOBILE_DARK_THEME : MOBILE_LIGHT_THEME;
 
   return {
     custom: theme.custom,
