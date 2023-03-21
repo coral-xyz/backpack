053e37f90e7fcdedfbe61d83b96289905048849a
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-21 10:28:03 -0500

    wip

--- packages/themes/src/theme.native.tsx
+++ packages/themes/src/theme.native.tsx
@@ -1,18 +1,18 @@
 import { baseTheme } from "./base";
 import { DARK_COLORS, LIGHT_COLORS } from "./colors";
 
-export const darkTheme = {
+export const MOBILE_LIGHT_THEME = {
   ...baseTheme,
   custom: {
     colors: LIGHT_COLORS,
   },
 };
 
-export const lightTheme = {
+export const MOBILE_DARK_THEME = {
   ...baseTheme,
   custom: {
     colors: DARK_COLORS,
   },
 };
 
-export type CustomTheme = typeof lightTheme & typeof darkTheme;
+export type CustomTheme = typeof MOBILE_LIGHT_THEME & typeof MOBILE_DARK_THEME;
