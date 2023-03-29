12e019c8da3e7125f84f3f64a575a57d0e8e5906
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-29 14:12:23 -0500

    fix dumb eslint config warnings (#3481)
    * fix dumb eslint rootdir warnings
    
    * removes metro app config from main one
    
    * fixes annoying eslint

--- packages/app-mobile/.eslintrc.js
+++ packages/app-mobile/.eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
   root: true,
   extends: ["custom/native"],
-  // overrides: [
-  //   {
-  //     files: ["*.ts", "*.tsx", "*.d.ts"],
-  //     parserOptions: {
-  //       project: "./tsconfig.json",
-  //     },
-  //   },
-  // ],
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname, // eslint-disable-line
+    project: ["./tsconfig.eslint.json", "./tsconfig.json"],
   },
 };
