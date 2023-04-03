23be86576a6bd7515eb933790689c911b3b23624
RashulChutani <rashul.chutani@gmail.com>
2023-04-04 01:03:26 +0530

    Add Map of UserId to ColorIndex to not update the users Variable #3459

--- packages/tamagui-core/src/hooks/chat/useChatsWithMetadata.tsx
+++ packages/tamagui-core/src/hooks/chat/useChatsWithMetadata.tsx
@@ -31,37 +31,38 @@ export const useChatsWithMetadata = ({
   const uniqueUserIds = userIds
     .filter((x, index) => userIds.indexOf(x) === index)
     .filter((x) => x);
-  let users = useUsersMetadata({ remoteUserIds: uniqueUserIds });
+  const users = useUsersMetadata({ remoteUserIds: uniqueUserIds });
 
   console.log("debug2", { chats, userIds, uniqueUserIds });
-  
+
   // Make Sure that both the users in a DM chat do not have the same color
+  const colorIndexMap = new Map();
   if (uniqueUserIds.length === 2) {
     const [userId1, userId2] = uniqueUserIds;
 
-    function haveSameColor(user1, user2) {
-        return (user1 && user2) && user1.colorIndex === user2.colorIndex;
-    }
-
     if (haveSameColor(users[userId1], users[userId2])) {
-        const newColorIndex = (users[userId1].colorIndex + 1) % NEW_COLORS.length;
-        users[userId2].colorIndex = newColorIndex;
+      const newColorIndex = (users[userId1].colorIndex + 1) % NEW_COLORS.length;
+      colorIndexMap[userId2] = newColorIndex;
     }
   }
-  
+
   return {
     chats: chats.map((chat) => ({
       ...chat,
       image: users[chat.uuid]?.image || "",
       username: users[chat.uuid]?.username || "",
       color: users[chat.uuid]?.color,
-      colorIndex: users[chat.uuid]?.colorIndex,
+      colorIndex: colorIndexMap[chat.uuid] || users[chat.uuid]?.colorIndex,
       parent_message_author_username:
         users[chat.parent_message_author_uuid]?.username,
     })),
     usersMetadata: users,
   };
 };
+
+function haveSameColor(user1, user2) {
+  return user1 && user2 && user1.colorIndex === user2.colorIndex;
+}
 
 function getAllUserIdsInMessage(message) {
   const userIds: string[] = [];
