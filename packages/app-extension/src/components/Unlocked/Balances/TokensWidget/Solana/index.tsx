1f775018b2886cd7215ac7d121b5e2d65cc01fa4
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-04-06 22:26:30 -0500

    consolidate sending transactions

--- packages/app-extension/src/components/Unlocked/Balances/TokensWidget/Solana/index.tsx
+++ packages/app-extension/src/components/Unlocked/Balances/TokensWidget/Solana/index.tsx
@@ -1,33 +1,19 @@
-import { useState } from "react";
-import {
-  Blockchain,
-  confirmTransaction,
-  getLogger,
-  isCardinalWrappedToken,
-  isCreatorStandardToken,
-  isOpenCreatorProtocol,
-  isProgrammableNftToken,
-  SOL_NATIVE_MINT,
-  Solana,
-} from "@coral-xyz/common";
+import { Blockchain } from "@coral-xyz/common";
 import { PrimaryButton, UserIcon } from "@coral-xyz/react-common";
 import {
   useActiveWallet,
   useAvatarUrl,
   useSolanaCtx,
-  useSolanaTokenMint,
+  useSolanaTransaction,
 } from "@coral-xyz/recoil";
 import { styles, useCustomTheme } from "@coral-xyz/themes";
 import { Typography } from "@mui/material";
-import { PublicKey } from "@solana/web3.js";
 import type { BigNumber } from "ethers";
 
 import { CopyablePublicKey } from "../../../../common/CopyablePublicKey";
 import { SettingsList } from "../../../../common/Settings/List";
 import { TokenAmountHeader } from "../../../../common/TokenAmountHeader";
 import { Error, Sending } from "../Send";
-
-const logger = getLogger("send-solana-confirmation-card");
 
 const useStyles = styles((theme) => ({
   confirmTableListItem: {
@@ -63,119 +49,14 @@ export function SendSolanaConfirmationCard({
   onComplete?: (txSig?: any) => void;
   onViewBalances?: () => void;
 }) {
-  const [txSignature, setTxSignature] = useState<string | null>(null);
-  const solanaCtx = useSolanaCtx();
-  const [error, setError] = useState(
-    "Error 422. Transaction time out. Runtime error. Reticulating splines."
-  );
-  const [cardType, setCardType] = useState<
-    "confirm" | "sending" | "complete" | "error"
-  >("confirm");
-  const mintInfo = useSolanaTokenMint({
-    publicKey: solanaCtx.walletPublicKey.toString(),
-    tokenAddress: token.address,
+  const { txSignature, onConfirm, cardType, error } = useSolanaTransaction({
+    token,
+    destinationAddress,
+    amount,
+    onComplete: (txid) => {
+      onComplete?.(txid);
+    },
   });
-
-  const onConfirm = async () => {
-    setCardType("sending");
-    //
-    // Send the tx.
-    //
-    let txSig;
-
-    try {
-      const mintId = new PublicKey(token.mint?.toString() as string);
-      if (token.mint === SOL_NATIVE_MINT.toString()) {
-        txSig = await Solana.transferSol(solanaCtx, {
-          source: solanaCtx.walletPublicKey,
-          destination: new PublicKey(destinationAddress),
-          amount: amount.toNumber(),
-        });
-      } else if (
-        await isProgrammableNftToken(
-          solanaCtx.connection,
-          token.mint?.toString() as string
-        )
-      ) {
-        txSig = await Solana.transferProgrammableNft(solanaCtx, {
-          destination: new PublicKey(destinationAddress),
-          mint: new PublicKey(token.mint!),
-          amount: amount.toNumber(),
-          decimals: token.decimals,
-          source: new PublicKey(token.address),
-        });
-      }
-      // Use an else here to avoid an extra request if we are transferring sol native mints.
-      else {
-        const ocpMintState = await isOpenCreatorProtocol(
-          solanaCtx.connection,
-          mintId,
-          mintInfo
-        );
-        if (ocpMintState !== null) {
-          txSig = await Solana.transferOpenCreatorProtocol(
-            solanaCtx,
-            {
-              destination: new PublicKey(destinationAddress),
-              amount: amount.toNumber(),
-              mint: new PublicKey(token.mint!),
-            },
-            ocpMintState
-          );
-        } else if (isCreatorStandardToken(mintId, mintInfo)) {
-          txSig = await Solana.transferCreatorStandardToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        } else if (
-          await isCardinalWrappedToken(solanaCtx.connection, mintId, mintInfo)
-        ) {
-          txSig = await Solana.transferCardinalManagedToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        } else {
-          txSig = await Solana.transferToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        }
-      }
-    } catch (err: any) {
-      logger.error("solana transaction failed", err);
-      setError(err.toString());
-      setCardType("error");
-      return;
-    }
-
-    setTxSignature(txSig);
-
-    //
-    // Confirm the tx.
-    //
-    try {
-      await confirmTransaction(
-        solanaCtx.connection,
-        txSig,
-        solanaCtx.commitment !== "confirmed" &&
-          solanaCtx.commitment !== "finalized"
-          ? "confirmed"
-          : solanaCtx.commitment
-      );
-      setCardType("complete");
-      if (onComplete) onComplete(txSig);
-    } catch (err: any) {
-      logger.error("unable to confirm", err);
-      setError(err.toString());
-      setCardType("error");
-    }
-  };
 
   return (
     <>
--- packages/app-mobile/src/components/BottomDrawerSolanaConfirmation.tsx
+++ packages/app-mobile/src/components/BottomDrawerSolanaConfirmation.tsx
@@ -1,21 +1,13 @@
 import type { BigNumber } from "ethers";
 
-import { useState } from "react";
 import { Text } from "react-native";
 
+import { Blockchain, walletAddressDisplay } from "@coral-xyz/common";
 import {
-  Blockchain,
-  confirmTransaction,
-  SOL_NATIVE_MINT,
-  Solana,
-  walletAddressDisplay,
-  isCardinalWrappedToken,
-  isCreatorStandardToken,
-  isOpenCreatorProtocol,
-  isProgrammableNftToken,
-} from "@coral-xyz/common";
-import { useSolanaCtx, useSolanaTokenMint } from "@coral-xyz/recoil";
-import { PublicKey } from "@solana/web3.js";
+  useSolanaCtx,
+  useSolanaTransaction,
+  SolTransactionStep,
+} from "@coral-xyz/recoil";
 
 import {
   Error,
@@ -26,8 +18,6 @@ import {
 import { Margin, PrimaryButton, TokenAmountHeader } from "~components/index";
 import { useTheme } from "~hooks/useTheme";
 import { SettingsList } from "~screens/Unlocked/Settings/components/SettingsMenuList";
-
-type Step = "confirm" | "sending" | "complete" | "error";
 
 export function SendSolanaConfirmationCard({
   navigation,
@@ -46,123 +36,16 @@ export function SendSolanaConfirmationCard({
   };
   destinationAddress: string;
   amount: BigNumber;
-  onCompleteStep?: (step: Step) => void;
+  onCompleteStep?: (step: SolTransactionStep) => void;
 }): JSX.Element {
-  const [txSignature, setTxSignature] = useState<string | null>(null);
-  const solanaCtx = useSolanaCtx();
-  const [error, setError] = useState(
-    "Error 422. Transaction time out. Runtime error. Reticulating splines."
-  );
-  const [cardType, setCardType] = useState<Step>("confirm");
-
-  const handleChangeStep = (step: Step) => {
-    setCardType(step);
-    if (onCompleteStep) {
-      onCompleteStep(step);
-    }
-  };
-
-  const mintInfo = useSolanaTokenMint({
-    publicKey: solanaCtx.walletPublicKey.toString(),
-    tokenAddress: token.address,
+  const { txSignature, onConfirm, cardType, error } = useSolanaTransaction({
+    token,
+    destinationAddress,
+    amount,
+    onComplete: () => {
+      onCompleteStep?.(cardType);
+    },
   });
-
-  const onConfirm = async () => {
-    handleChangeStep("sending");
-    //
-    // Send the tx.
-    //
-    let txSig;
-    try {
-      const mintId = new PublicKey(token.mint?.toString() as string);
-      if (token.mint === SOL_NATIVE_MINT.toString()) {
-        txSig = await Solana.transferSol(solanaCtx, {
-          source: solanaCtx.walletPublicKey,
-          destination: new PublicKey(destinationAddress),
-          amount: amount.toNumber(),
-        });
-      } else if (
-        await isProgrammableNftToken(
-          solanaCtx.connection,
-          token.mint?.toString() as string
-        )
-      ) {
-        txSig = await Solana.transferProgrammableNft(solanaCtx, {
-          destination: new PublicKey(destinationAddress),
-          mint: new PublicKey(token.mint!),
-          amount: amount.toNumber(),
-          decimals: token.decimals,
-          source: new PublicKey(token.address),
-        });
-      }
-      // Use an else here to avoid an extra request if we are transferring sol native mints.
-      else {
-        const ocpMintState = await isOpenCreatorProtocol(
-          solanaCtx.connection,
-          mintId,
-          mintInfo
-        );
-        if (ocpMintState !== null) {
-          txSig = await Solana.transferOpenCreatorProtocol(
-            solanaCtx,
-            {
-              destination: new PublicKey(destinationAddress),
-              amount: amount.toNumber(),
-              mint: new PublicKey(token.mint!),
-            },
-            ocpMintState
-          );
-        } else if (isCreatorStandardToken(mintId, mintInfo)) {
-          txSig = await Solana.transferCreatorStandardToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        } else if (
-          await isCardinalWrappedToken(solanaCtx.connection, mintId, mintInfo)
-        ) {
-          txSig = await Solana.transferCardinalManagedToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        } else {
-          txSig = await Solana.transferToken(solanaCtx, {
-            destination: new PublicKey(destinationAddress),
-            mint: new PublicKey(token.mint!),
-            amount: amount.toNumber(),
-            decimals: token.decimals,
-          });
-        }
-      }
-    } catch (err: any) {
-      setError(err.toString());
-      handleChangeStep("error");
-      return;
-    }
-
-    setTxSignature(txSig);
-
-    //
-    // Confirm the tx.
-    //
-    try {
-      await confirmTransaction(
-        solanaCtx.connection,
-        txSig,
-        solanaCtx.commitment !== "confirmed" &&
-          solanaCtx.commitment !== "finalized"
-          ? "confirmed"
-          : solanaCtx.commitment
-      );
-      handleChangeStep("complete");
-    } catch (err: any) {
-      setError(err.toString());
-      handleChangeStep("error");
-    }
-  };
 
   return (
     <>
