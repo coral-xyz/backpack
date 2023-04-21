b74a9300f8aebf1d442c6bf8efaf76091b7f18de
wentokay <101902546+wentokay@users.noreply.github.com>
2023-04-21 21:25:54 +0100

    dedicated

--- backend/workers/rpc-proxy/src/index.ts
+++ backend/workers/rpc-proxy/src/index.ts
@@ -1,5 +1,6 @@
 export interface Env {
   // in secrets, see `npx wrangler secret --help`
+  DEDICATED_RPC_URL: string;
   RPC_URL: string;
   BACKUP_RPC_URL: string;
 }
@@ -20,17 +21,16 @@ export default {
       // GET for websockets
       (request.method === "GET" && request.headers.get("Upgrade"))
     ) {
-      // TODO:  is there a alternative way of doing this without cloning?
-      //        The ReadableStream cannot be read again unless it's been
-      //        cloned, as it is read and consumed during the first fetch
-      const clonedRequestForBackupAttempt = request.clone();
-
-      let response = await fetchRpc(request, env.RPC_URL);
+      let response = await fetchRpc(
+        request.clone(),
+        env.RPC_URL,
+        env.DEDICATED_RPC_URL
+      );
 
       if (!response) {
         // Initial attempt failed, try to fetch using backup RPC if it exists
         response = await fetchRpc(
-          clonedRequestForBackupAttempt,
+          request,
           env.BACKUP_RPC_URL || env.RPC_URL // try again in no backup exists
         );
       }
@@ -55,10 +55,28 @@ export default {
 
 async function fetchRpc(
   request: Request,
-  rpcUrl: string
+  rpcUrl: string,
+  dedicatedUrl?: string
 ): Promise<Response | null> {
   try {
-    const { href, hostname } = new URL(rpcUrl);
+    const body = await request.clone().text();
+
+    const IS_HELIUS = rpcUrl.includes("helius");
+
+    const ACCOUNT_INDEX =
+      IS_HELIUS && /getTokenAccountsByOwner|getProgramAccounts/.test(body);
+
+    const RPC_STICKY =
+      IS_HELIUS &&
+      /getLatestBlockhash|getRecentBlockhash|sendTransaction|simulateTransaction/.test(
+        body
+      );
+
+    const USE_DEDICATED_RPC = !ACCOUNT_INDEX && dedicatedUrl;
+
+    const { href, hostname } = new URL(
+      USE_DEDICATED_RPC ? dedicatedUrl : rpcUrl
+    );
 
     const rpcResponse = await fetch(
       href,
@@ -66,6 +84,9 @@ async function fetchRpc(
         const _request = new Request(href, request);
         // Only forward necessary headers from the client request
         sanitizeHeaders(_request.headers);
+
+        if (ACCOUNT_INDEX) _request.headers.append("ACCOUNT_INDEX", "true");
+        if (RPC_STICKY) _request.headers.append("RPC_STICKY", "true");
         return _request;
       })()
     );
@@ -78,7 +99,16 @@ async function fetchRpc(
     }
 
     const response = new Response(rpcResponse.body, rpcResponse);
-    response.headers.append("x-backpack-rpc", hostname);
+
+    response.headers.append(
+      "x-backpack-rpc",
+      USE_DEDICATED_RPC ? "dedicated" : IS_HELIUS ? "helius" : hostname
+    );
+    response.headers.append("x-backpack-is-sticky-request", String(RPC_STICKY));
+    response.headers.append(
+      "x-backpack-uses-account-index",
+      String(ACCOUNT_INDEX)
+    );
 
     return response;
   } catch (error) {
