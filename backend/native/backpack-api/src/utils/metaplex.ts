0409e4027449f879c066d8eccc05bf4816a9b0ff
Kirat <harkirat96@gmail.com>
2023-04-25 12:32:13 +0530

    Revert "Added back ownership check (#3706)"
    This reverts commit eaa627a2c69641bf515ca5260b79951771846f94.

--- backend/native/backpack-api/src/utils/metaplex.ts
+++ backend/native/backpack-api/src/utils/metaplex.ts
@@ -8,9 +8,9 @@ const metaplex = new Metaplex(connection);
 
 export const validateOwnership = async (
   mint: string,
-  collection: string,
-  centralizedGroup: string,
-  owner: string
+  collection: string
+  // centralizedGroup: string,
+  // owner: string
 ) => {
   try {
     //TODO: make use of centralizedGroup group here
@@ -27,8 +27,8 @@ export const validateOwnership = async (
       largestAccounts.value[0]?.address
     );
     return (
-      nft.getResult()?.collection?.address.toString() === collection &&
-      largestAccountInfo?.value?.data?.parsed?.info?.owner === owner
+      nft.getResult().collection.address.toString() === collection &&
+      largestAccountInfo?.value?.data?.parsed?.info?.owner
     );
   } catch (e) {
     console.log(e);
