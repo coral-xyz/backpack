cb582d707538b7cd14014dd628cf66b655842466
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-04-25 12:44:35 -0500

    tamagui font updates (#3793)
    * tamagui config updates
    
    * clean up tamagui font stuff and separate it

--- packages/tamagui-core/src/tamagui.config.ts
+++ packages/tamagui-core/src/tamagui.config.ts
@@ -1,5 +1,3 @@
-// import { createAnimations } from "@tamagui/animations-react-native";
-// import { createMedia } from "@tamagui/react-native-media-driver";
 import {
   baseTheme,
   DARK_COLORS,
@@ -8,15 +6,27 @@ import {
   MOBILE_LIGHT_OVERRIDES,
 } from "@coral-xyz/themes";
 import { config } from "@tamagui/config";
-// import { createInterFont } from "@tamagui/font-inter";
 import { themes as _themes, tokens as _tokens } from "@tamagui/themes";
 import { createTamagui, createTheme, createTokens } from "tamagui";
+
+import { bodyFont } from "./fonts";
 
 const tokens = createTokens({
   ..._tokens,
   size: {
     ..._tokens.size,
     container: 48,
+    borderWidth: 2,
+    xs: 12,
+    sm: 14,
+    base: 16,
+    lg: 18,
+    xl: 20,
+    "2xl": 24,
+    "3xl": 28,
+    "4xl": 32,
+    "5xl": 36,
+    "6xl": 40,
   },
   radius: {
     ..._tokens.radius,
@@ -37,6 +47,10 @@ const lightTheme = createTheme({
 export const appConfig = createTamagui({
   ...config,
   tokens,
+  fonts: {
+    ...config.fonts,
+    body: bodyFont,
+  },
   themes: {
     dark: {
       ..._themes.dark,
@@ -51,7 +65,5 @@ export const appConfig = createTamagui({
 
 export type AppConfig = typeof appConfig;
 declare module "tamagui" {
-  // overrides TamaguiCustomConfig so your custom types
-  // work everywhere you import `tamagui`
   type TamaguiCustomConfig = AppConfig;
 }
