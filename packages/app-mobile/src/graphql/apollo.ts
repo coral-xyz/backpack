4add9c2525d30ab52486d3feeb3a9abde88e944b
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-05-01 16:58:36 -0500

    add auth header

--- packages/app-mobile/src/graphql/apollo.ts
+++ packages/app-mobile/src/graphql/apollo.ts
@@ -1,8 +1,24 @@
 import Constants from "expo-constants";
 
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 const API_URL = Constants.expoConfig?.extra?.graphqlApiUrl;
-export const apolloClient = new ApolloClient({
+
+const httpLink = createHttpLink({
   uri: API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = "Bearer abc1234";
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
