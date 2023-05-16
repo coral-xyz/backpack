b3a15216028f4b61e71262ff79db3029f081cec3
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-05-16 13:43:37 -0500

    v2 colors

--- packages/themes/src/colorsv2.ts
+++ packages/themes/src/colorsv2.ts
@@ -1,4 +1,4 @@
-
+import color from "color";
 // Opacities without color values ///////////////////////////////////////////////
 // Disabled elements should be 0.5 opacity
 // Hover can be 0.8 opacity if complex component
@@ -19,34 +19,34 @@ export const darkAccentBlue = "#4c94ff";
 export const darkAccentPurple = "#e17aff";
 
 // Neutral Colors
-export const base50 = rgba(244,244,246,1); // formerly #f8f8f9
-export const base100 = rgba(238,239,241); // formerly #f0f0f2
-export const base200 = rgba(227,228,232); // formerly #dfe0e5
-export const base300 = rgba(200,201,208,1); // formerly #c2c4cc
-export const base400 = rgba(142,145,159,1); // formerly #8f929e
-export const base500 = rgba(117,121,138,1); // formerly #787c89
-export const base600 = rgba(93,96,111,1); // formerly #555c6b
-export const base700 = rgba(70,72,83,1); // formerly #4e5768
-export const base800 = rgba(32,33,39,1); // formerly #212938
-export const base900 = rgba(20,21,27,1); // formerly #030a19
+export const base50 = "rgba(244, 244, 246, 1)"; // formerly #f8f8f9
+export const base100 = "rgba(238, 239, 241)"; // formerly #f0f0f2
+export const base200 = "rgba(227, 228, 232)"; // formerly #dfe0e5
+export const base300 = "rgba(200, 201, 208, 1)"; // formerly #c2c4cc
+export const base400 = "rgba(142, 145, 159, 1)"; // formerly #8f929e
+export const base500 = "rgba(117, 121, 138, 1)"; // formerly #787c89
+export const base600 = "rgba(93, 96, 111, 1)"; // formerly #555c6b
+export const base700 = "rgba(70, 72, 83, 1)"; // formerly #4e5768
+export const base800 = "rgba(32, 33, 39, 1)"; // formerly #212938
+export const base900 = "rgba(20, 21, 27, 1)"; // formerly #030a19
 
 // Success Colors
-export const green100 = rgba(224,248,239,1); // formerly #f1ffef
-export const green500 = rgba(0,194,120,1); //formerly #42c337
-export const green600 = rgba(0,158,99,1); // formerly #11a800 
-export const green700 = rgba(0,121,75,1); // formerly #138600
+export const green100 = "rgba(224, 248, 239, 1)"; // formerly #f1ffef
+export const green500 = "rgba(0, 194, 120, 1)"; //formerly #42c337
+export const green600 = "rgba(0, 158, 99, 1)"; // formerly #11a800
+export const green700 = "rgba(0, 121, 75, 1)"; // formerly #138600
 
 // Warning Colors
-export const yellow100 = rgba(255,249,223,1); // formerly #fff9df
-export const yellow500 = rgba(239,164,17,1); // formerly #efa411
-export const yellow600 = rgba(206,121,7,1); // formerly #ce7907
-export const yellow700 = rgba(177,87,0,1); // formerly #b15700
+export const yellow100 = "rgba(255, 249, 223, 1)"; // formerly #fff9df
+export const yellow500 = "rgba(239, 164, 17, 1)"; // formerly #efa411
+export const yellow600 = "rgba(206, 121, 7, 1)"; // formerly #ce7907
+export const yellow700 = "rgba(177, 87, 0, 1)"; // formerly #b15700
 
 // Error Colors
-export const red100 = rgba(255,237,235,1); // previously #ffedeb
-export const red500 = rgba(23,80,80,1); // previously FF6269
-export const red600 = rgba(234,56,59,1); // previously #f13236
-export const red700 = rgba(210,0,36,1); // previously #d20024
+export const red100 = "rgba(255, 237, 235, 1)"; // previously #ffedeb
+export const red500 = "rgba(23, 80, 80, 1)"; // previously #FF6269
+export const red600 = "rgba(234, 56, 59, 1)"; // previously #f13236
+export const red700 = "rgba(210, 0, 36, 1)"; // previously #d20024
 
 // Light Mode /////////////////////////////////////////////////////
 // Light Mode Backgrounds
@@ -111,8 +111,8 @@ export const darkBaseBackgroundL0 = base900; // dark container background
 export const darkBaseBackgroundL1 = base800; // dark content background
 
 // Dark Mode Borders
-export const darkBaseBorderLight = color(baseWhite).alpha(0.1);
-// export const darkBaseBorderMed = color(baseWhite).alpha(0.25); idk if we need this
+export const darkBaseBorderLight = color(brandWhite).alpha(0.1);
+// export const darkBaseBorderMed = color(brandWhite).alpha(0.25); idk if we need this
 
 // Dark Mode Buttons
 export const darkButtonPrimaryBackground = brandWhite;
@@ -125,56 +125,56 @@ export const darkGreenText = green500;
 export const darkGreenIcon = green600;
 export const darkGreenBorder = color(green500).alpha(0.4); // green500 at 40%
 export const darkGreenBackgroundOpacity = color(green500).alpha(0.08); // green500 at 8%
-export const darkGreenBackgroundSolid = rgba(18,35,34,1); // derived from putting green500 at 8% opacity on base900
+export const darkGreenBackgroundSolid = "rgba(18, 35, 34, 1)"; // derived from putting green500 at 8% opacity on base900
 
 // Dark Mode Warning
 export const darkYellowText = yellow500;
 export const darkYellowIcon = yellow600;
 export const darkYellowBorder = color(yellow500).alpha(0.5);
 export const darkYellowBackgroundTransparent = color(yellow500).alpha(0.08);
-export const darkYellowBackgroundSolid = rgba(38,32,26,1); // derived from putting yellow500 at 8% opacity on base900
+export const darkYellowBackgroundSolid = "rgba(38, 32, 26, 1)"; // derived from putting yellow500 at 8% opacity on base900
 
 // Dark Mode Error
 export const darkRedText = red500;
 export const darkRedIcon = red600;
 export const darkRedBorder = color(red600).alpha(0.5);
 export const darkRedBackgroundTransparent = color(red600).alpha(0.12);
-export const darkRedBackgroundSolid = rgba(53,26,31,1); // derived from putting red600 at 12% opacity on base900
+export const darkRedBackgroundSolid = "rgba(53, 26, 31, 1)"; // derived from putting red600 at 12% opacity on base900
 
 // Misc Color Definitions ////////////////////////////////////////
 export const overlayBackground = color(base900).alpha(0.4); // overlay background is base900 at 40% opacity
 
-// Username Colors in Messaging 
-const lightUsernameColors = {
-  color_01: '#E02929',
-  color_02: '#CC2578',
-  color_03: '#9930B8',
-  color_04: '#5E35B1',
-  color_05: '#3949AB',
-  color_06: '#0072DB',
-  color_07: '#0C5ADF',
-  color_08: '#008577',
-  color_09: '#1A841F',
-  color_10: '#6C7D26',
-  color_11: '#BD5B00',
-  color_12: '#CC4218',
-  color_13: '#6D4C41',
-  color_14: '#2D4363',
+// Username Colors in Messaging
+export const lightUsernameColors = {
+  color_01: "#E02929",
+  color_02: "#CC2578",
+  color_03: "#9930B8",
+  color_04: "#5E35B1",
+  color_05: "#3949AB",
+  color_06: "#0072DB",
+  color_07: "#0C5ADF",
+  color_08: "#008577",
+  color_09: "#1A841F",
+  color_10: "#6C7D26",
+  color_11: "#BD5B00",
+  color_12: "#CC4218",
+  color_13: "#6D4C41",
+  color_14: "#2D4363",
 };
 
-const darkUsernameColors = {
-  color_01: '#F88484',
-  color_02: '#E57AB0',
-  color_03: '#DA8BE7',
-  color_04: '#C2A6F4',
-  color_05: '#97A4F4',
-  color_06: '#57AEFF',
-  color_07: '#5596F6',
-  color_08: '#7ACCC7',
-  color_09: '#75DD7A',
-  color_10: '#BEE05A',
-  color_11: '#FFD080',
-  color_12: '#FA9476',
-  color_13: '#BCAAA4',
-  color_14: '#A3B5CF',
+export const darkUsernameColors = {
+  color_01: "#F88484",
+  color_02: "#E57AB0",
+  color_03: "#DA8BE7",
+  color_04: "#C2A6F4",
+  color_05: "#97A4F4",
+  color_06: "#57AEFF",
+  color_07: "#5596F6",
+  color_08: "#7ACCC7",
+  color_09: "#75DD7A",
+  color_10: "#BEE05A",
+  color_11: "#FFD080",
+  color_12: "#FA9476",
+  color_13: "#BCAAA4",
+  color_14: "#A3B5CF",
 };
