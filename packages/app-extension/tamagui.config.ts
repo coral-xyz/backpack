e04afc7717c01f778ea806488858bb393af3820c
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-05-19 11:41:40 -0500

    adds tamagui.config.ts to eslint

--- packages/app-extension/tamagui.config.ts
+++ packages/app-extension/tamagui.config.ts
@@ -1,2 +1,10 @@
 import { config } from "@coral-xyz/tamagui";
+
+export type Conf = typeof config;
+
+// @ts-expect-error
+declare module "tamagui" {
+  type TamaguiCustomConfig = Conf
+}
+
 export default config;
--- packages/app-mobile/tamagui.config.ts
+++ packages/app-mobile/tamagui.config.ts
@@ -1,3 +1,10 @@
 import { config } from "@coral-xyz/tamagui";
 
+export type Conf = typeof config;
+
+// @ts-expect-error
+declare module "tamagui" {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
 export default config;
