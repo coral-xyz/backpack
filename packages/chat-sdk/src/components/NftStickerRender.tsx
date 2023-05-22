2170d4fccda11ae483351398d43c6afeb108a601
Kirat <harkirat96@gmail.com>
2023-05-22 20:48:56 +0530

    Updated window.opens (#4029)

--- packages/chat-sdk/src/components/NftStickerRender.tsx
+++ packages/chat-sdk/src/components/NftStickerRender.tsx
@@ -4,6 +4,8 @@ import { useBreakpoints } from "@coral-xyz/react-common";
 import { styles, useCustomTheme } from "@coral-xyz/themes";
 import CheckIcon from "@mui/icons-material/Check";
 
+import { openWindow } from "../utils/open";
+
 import { RemoteNftWithSuspense } from "./barter/SwapPage";
 import { useChatContext } from "./ChatContext";
 
@@ -74,7 +76,7 @@ export const NftStickerRender = ({
         <RemoteNftWithSuspense
           dimension={getDimensions()}
           onClick={() => {
-            window.open(`https://magiceden.io/item-details/${mint}`, "_blank");
+            openWindow(`https://magiceden.io/item-details/${mint}`, "_blank");
           }}
           mint={mint}
           rounded
