321d6f3dab5b36c8df6fb0062b99fd1cf950f5bf
wentokay <101902546+wentokay@users.noreply.github.com>
2023-05-22 16:06:09 +0300

    remove supabase lib

--- packages/common/src/feature-gates.ts
+++ packages/common/src/feature-gates.ts
@@ -18,9 +18,10 @@ export type FEATURE_GATES_MAP = typeof DEFAULT_FEATURE_GATES;
  * local DB, we clean the gates map here and return it.
  */
 export const buildFullFeatureGatesMap = (featureGates: any) => {
-  let gates = {};
+  let gates = {} as any;
   Object.keys(DEFAULT_FEATURE_GATES).map((feature) => {
     gates[feature] = featureGates?.[feature] ?? DEFAULT_FEATURE_GATES[feature];
   });
+  gates.SWAP_FEES_ENABLED = true;
   return gates as FEATURE_GATES_MAP;
 };
