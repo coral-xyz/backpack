dbc80551f3b133916620aed7d7a84b41e8f11a56
Philipp Adrian <hello@philippadrian.com>
2023-05-30 00:05:15 -0400

    Setup SecureEvents and Transports

--- packages/secure-background/src/service.ts
+++
@@ -1,7 +0,0 @@
-import type { SECURE_SVM_EVENTS } from "./VMs/svm/events";
-import { SVMService } from "./VMs/svm/service";
-import type { ISecureEvent, Transport } from "./types";
-
-export default function startSecureService(transport: Transport<ISecureEvent>) {
-  new SVMService(transport as Transport<SECURE_SVM_EVENTS>);
-}
