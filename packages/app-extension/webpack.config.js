574a62d9ed07da568e2356398b1f12a580218ea0
wentokay <101902546+wentokay@users.noreply.github.com>
2023-06-02 19:55:23 +0100

    hide ResizeObserver error overlay in dev

--- packages/app-extension/webpack.config.js
+++ packages/app-extension/webpack.config.js
@@ -121,6 +121,12 @@ const {
           overlay: {
             errors: true,
             warnings: false,
+            runtimeErrors: (error) => {
+              if (error.message === "ResizeObserver loop limit exceeded") {
+                return false;
+              }
+              return true;
+            },
           },
         },
         devMiddleware: {
