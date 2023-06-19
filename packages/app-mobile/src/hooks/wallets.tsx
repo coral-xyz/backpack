ff8bdbd9d4edea6ff9965f1105b9a6a0f0c20588
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-06-19 17:29:36 -0500

    clean this up

--- packages/app-mobile/src/hooks/wallets.tsx
+++ packages/app-mobile/src/hooks/wallets.tsx
@@ -2,13 +2,31 @@ import { useAllWallets, useDehydratedWallets } from "@coral-xyz/recoil";
 
 import { Wallet } from "~types/types";
 
+type Result = {
+  coldWallets: Wallet[];
+  activeWallets: Wallet[];
+};
+
+function groupByColdWallet(wallets: Wallet[]): Result {
+  return wallets.reduce(
+    (result: Result, wallet) => {
+      if (wallet.isCold) {
+        result.coldWallets.push(wallet);
+      } else {
+        result.activeWallets.push(wallet);
+      }
+      return result;
+    },
+    { coldWallets: [], activeWallets: [] }
+  );
+}
+
 export function useWallets(): {
   allWallets: Wallet[];
   coldWallets: Wallet[];
 } {
   const wallets = useAllWallets();
-  const activeWallets = wallets.filter((w) => !w.isCold);
-  const coldWallets = wallets.filter((w) => w.isCold);
+  const { activeWallets, coldWallets } = groupByColdWallet(wallets);
 
   // Dehydrated public keys are keys that exist on the server but cannot be
   // used on the client as we don't have signing data, e.g. mnemonic, private
