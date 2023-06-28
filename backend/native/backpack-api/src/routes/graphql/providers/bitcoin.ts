af494faec52576e4b23acdd532222cca34a26046
Matthew Callens <callensmatt@gmail.com>
2023-06-28 11:49:29 -0400

    add ordinals to the bitcoin provider

--- backend/native/backpack-api/src/routes/graphql/providers/bitcoin.ts
+++ backend/native/backpack-api/src/routes/graphql/providers/bitcoin.ts
@@ -1,11 +1,19 @@
 import { BitcoinToken } from "@coral-xyz/common";
 import { ethers } from "ethers";
 
+import { MAGIC_EDEN_API_KEY } from "../../../config";
+import { BlockchainInfo, MagicEden } from "../clients";
+import type {
+  MagicEdenGetOrdinalCollectionResponse,
+  MagicEdenGetOrdinalsByOwnerResponse,
+} from "../clients/magiceden";
 import type { ApiContext } from "../context";
 import { NodeBuilder } from "../nodes";
 import type {
   BalanceFiltersInput,
   Balances,
+  Collection,
+  Nft,
   NftConnection,
   NftFiltersInput,
   TokenBalance,
@@ -30,9 +38,14 @@ export type BitcoinProviderSettings = {
  */
 export class Bitcoin implements BlockchainDataProvider {
   readonly #ctx?: ApiContext;
+  readonly #sdk: { btc: BlockchainInfo; ord: MagicEden };
 
   constructor({ context }: BitcoinProviderSettings) {
     this.#ctx = context;
+    this.#sdk = {
+      btc: new BlockchainInfo({}),
+      ord: new MagicEden({ apiKey: MAGIC_EDEN_API_KEY }),
+    };
   }
 
   /**
@@ -95,10 +108,7 @@ export class Bitcoin implements BlockchainDataProvider {
       throw new Error("API context object not available");
     }
 
-    const balance = await this.#ctx.dataSources.blockchainInfo.getBalance(
-      address
-    );
-
+    const balance = await this.#sdk.btc.getBalance(address);
     const prices = await this.#ctx.dataSources.coinGecko.getPrices(["bitcoin"]);
     const displayAmount = ethers.utils.formatUnits(
       balance.final_balance,
@@ -146,16 +156,67 @@ export class Bitcoin implements BlockchainDataProvider {
 
   /**
    * Get a list of NFT data for tokens owned by the argued address.
-   * @param {string} _address
+   * @param {string} address
    * @param {NftFiltersInput} [_filters]
    * @returns {Promise<NftConnection>}
    * @memberof Bitcoin
    */
   async getNftsForAddress(
-    _address: string,
+    address: string,
     _filters?: NftFiltersInput | undefined
   ): Promise<NftConnection> {
-    return createConnection([], false, false);
+    if (!this.#ctx) {
+      throw new Error("API context object not available");
+    }
+
+    const ordinals = await this.#sdk.ord.getOrdinalsByOwner(address);
+    const collectionSymbols = ordinals.items.reduce<Set<string>>(
+      (acc, curr) => {
+        const s = _parseCollectionSymbol(curr);
+        if (s) {
+          acc.add(s);
+        }
+        return acc;
+      },
+      new Set()
+    );
+
+    const collections = await this.#sdk.ord.getOrdinalCollections(
+      collectionSymbols
+    );
+
+    const nodes: Nft[] = ordinals.items.map((ord) =>
+      NodeBuilder.nft(this.id(), {
+        address: ord.inscriptionNumber.toString(),
+        attributes: ord.meta?.attributes?.map((attr) => ({
+          trait: attr.trait_type,
+          value: attr.value,
+        })),
+        collection: _getCollectionData(
+          this.id(),
+          collections,
+          _parseCollectionSymbol(ord)
+        ),
+        compressed: false,
+        image: ord.contentURI, // FIXME:
+        listing: ord.listed
+          ? NodeBuilder.nftListing(this.id(), "", {
+              amount: ethers.utils.formatUnits(
+                ord.listedPrice,
+                this.decimals()
+              ),
+              source: ord.listedAt,
+              url: this.#sdk.ord.getOrdinalListingUrl(""), // FIXME:
+            })
+          : undefined,
+        metadataUri: undefined, // FIXME:
+        name: ord.meta?.name,
+        owner: address,
+        token: ord.inscriptionNumber.toString(),
+      })
+    );
+
+    return createConnection(nodes, false, false);
   }
 
   /**
@@ -173,7 +234,7 @@ export class Bitcoin implements BlockchainDataProvider {
       throw new Error("API context object not available");
     }
 
-    const resp = await this.#ctx.dataSources.blockchainInfo.getRawAddressData(
+    const resp = await this.#sdk.btc.getRawAddressData(
       address,
       filters?.offset ?? undefined
     );
@@ -193,4 +254,41 @@ export class Bitcoin implements BlockchainDataProvider {
     const hasPrevious = filters?.offset ? filters.offset > 0 : false;
     return createConnection(nodes, hasNext, hasPrevious);
   }
+}
+
+/**
+ * Try to find and build the NFT collection node from the argued symbol.
+ * @param {ProviderId} providerId
+ * @param {MagicEdenGetOrdinalCollectionResponse} collections
+ * @param {string} [symbol]
+ * @returns {(Collection | undefined)}
+ */
+function _getCollectionData(
+  providerId: ProviderId,
+  collections: MagicEdenGetOrdinalCollectionResponse,
+  symbol?: string
+): Collection | undefined {
+  if (!symbol) {
+    return undefined;
+  }
+  const collectionData = symbol ? collections[symbol] : undefined;
+  return collectionData
+    ? NodeBuilder.nftCollection(providerId, {
+        address: "", // FIXME:
+        image: collectionData.imageURI,
+        name: collectionData.name,
+        verified: true,
+      })
+    : undefined;
+}
+
+/**
+ * Attempt to find and return the symbol for the ordinal collection
+ * @param {MagicEdenGetOrdinalsByOwnerResponse['items'][number]} ordinal
+ * @returns {(string | undefined)}
+ */
+function _parseCollectionSymbol(
+  ordinal: MagicEdenGetOrdinalsByOwnerResponse["items"][number]
+): string | undefined {
+  return ordinal.collectionSymbol || ordinal.collection?.symbol || undefined;
 }
